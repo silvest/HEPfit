dfcb2e872a9681329a0dc5f9b467b1749db68dc2
silvest <silvest@2e41be99-0402-4e17-b588-3552aed5b166>
2010-11-30 11:31:40 +0000

    First release
    git-svn-id: file:///afs/infn.it/roma1/project/susy/susy/svnroot/trunk/SusyFit@20 2e41be99-0402-4e17-b588-3552aed5b166

---
+++ MFVmodel/src/MFV.cpp
@@ -0,0 +1,25 @@
+/* 
+ * File:   MFV.cpp
+ * Author: silvest
+ * 
+ * Created on September 24, 2010, 10:53 AM
+ */
+
+#include "MFV.h"
+
+MFV::MFV(const double mQtilde, const double mUtilde, const double mDtilde,
+        const double Au, const double Ad, const double mLtilde,
+        const double mEtilde, const double mNtilde, const double Ae,
+        const double mhu, const double mhd, const double B, const double m1,
+        const double m2, const double m3, const double muH,
+        const gslpp::matrix<gslpp::complex>& Yu,
+        const gslpp::matrix<gslpp::complex>& Yd,
+        const gslpp::matrix<double>& Yl, const gslpp::matrix<double>& Ye) {
+}
+
+MFV::MFV(const MFV& orig) {
+}
+
+MFV::~MFV() {
+}
+
