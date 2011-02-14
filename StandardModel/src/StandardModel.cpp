5ef4774c7bc74690afdaf44e6825811672e57bec
mishima <mishima@2e41be99-0402-4e17-b588-3552aed5b166>
2011-02-14 22:40:22 +0000

    added sin2thw() and prototypes of other EWPO's
    git-svn-id: file:///afs/infn.it/roma1/project/susy/susy/svnroot/trunk/SusyFit@70 2e41be99-0402-4e17-b588-3552aed5b166

--- StandardModel/src/StandardModel.cpp
+++ StandardModel/src/StandardModel.cpp
@@ -25,13 +25,14 @@ StandardModel::StandardModel(const gslpp::matrix<gslpp::complex>& VCKM_i,
         double mb_i, const gslpp::matrix<gslpp::complex>& UPMNS_i, double me_i,
         double mmu_i, double mtau_i, double mnu1_i, double mnu2_i,
         double mnu3_i, double GF_i, double alsMz_i, double ale_i, double mZ_i,
-        double dAle5Mz_i) : UPMNS(UPMNS_i), VCKM(VCKM_i),
+        double dAle5Mz_i, double mHl_i) : UPMNS(UPMNS_i), VCKM(VCKM_i),
         Yd(3,3,0.), Yu(3,3,0.), Ye(3,3,0.), Yn(3,3,0.) {
     GF = GF_i;
     alsMz = alsMz_i;
     ale = ale_i;
     mZ = mZ_i;
     dAle5Mz = dAle5Mz_i;
+    mHl = mHl_i;
     mu = mu_i;
     md = md_i;
     mc = mc_i;
@@ -83,11 +84,12 @@ StandardModel::StandardModel(Parameters& Par): UPMNS(3,3,0.), VCKM(3,3,0.),
     Par.Get("VCKM",VCKM);
     Par.Get("UPMNS",UPMNS);
     Yu = Yu*VCKM;
-    Par.Get("GF",GF);
-    Par.Get("ale",ale);
-    Par.Get("alsmZ",alsMz);
-    Par.Get("dAle5Mz",dAle5Mz);
-    Par.Get("mZ",mZ);
+    Par.Get("GF", GF);
+    Par.Get("alsMz", alsMz);
+    Par.Get("ale", ale);
+    Par.Get("mZ", mZ);
+    Par.Get("dAle5Mz", dAle5Mz);
+    Par.Get("mHl", mHl);
 }
 
 
@@ -98,7 +100,7 @@ StandardModel::StandardModel(const StandardModel& orig) :
             orig.getMs(), orig.getMt(), orig.getMb(), orig.getUPMNS(),
             orig.getMe(), orig.getMmu(), orig.getMtau(), orig.getMnu1(),
             orig.getMnu2(), orig.getMnu3(), orig.getGF(), orig.getAlsMz(),
-            orig.getAle(), orig.getMZ(), orig.getDAle5Mz() );
+            orig.getAle(), orig.getMZ(), orig.getDAle5Mz(), orig.getMHl() );
 }
 
 StandardModel::~StandardModel() {
@@ -125,8 +127,8 @@ double StandardModel::mW() const {
     const double c10 = 0.0716;
     const double c11 = 115.0;
 
-    // mt, mZ, dAle5Mz and alsMz have to be varied within their
-    // combined 2 sigma region around their central values adopted below.
+    // mt, mZ, dAle5Mz and alsMz have to be varied within their combined 
+    // 2 sigma region around their central values (year 2003) adopted below.
     double dH = log(mHl/100.0);
     double dh = pow((mHl/100.0), 2.0);
     double dt = pow((mt/174.3), 2.0) - 1.0;
@@ -139,5 +141,100 @@ double StandardModel::mW() const {
                 - c8*dH*dt + c9*dh*dt - c10*dalphas + c11*dZ;
     return mw;
 }
+
+double StandardModel::sin2thw() const {
+    // Effective leptonic weak mixing angle
+    // hep-ph/0407317, hep-ph/0608099
+    // applicable for 10 GeV <= mHl <= 1 TeV
+
+    const double s0 = 0.2312527;
+    const double d1 = 4.729*0.0001;
+    const double d2 = 2.07*0.00001;
+    const double d3 = 3.85*0.000001;
+    const double d4 = -1.85*0.000001;
+    const double d5 = 0.0207;
+    const double d6 = -0.002851;
+    const double d7 = 1.82*0.0001;
+    const double d8 = -9.74*0.000001;
+    const double d9 = 3.98*0.0001;
+    const double d10 = -0.655;
+
+    // mt, mZ, dAle5Mz and alsMz have to be varied within their combined
+    // 2 sigma region around their central values (year 2003) adopted below.
+    double L_H = log(mHl/100.0);
+    double Delta_H = mHl/100.0;
+    double Delta_alphae = dAle5Mz/0.05907 - 1.0;
+    double Delta_t = pow((mt/178.0), 2.0) - 1.0;
+    double Delta_alphas = alsMz/0.117 - 1.0;
+    double Delta_Z = mZ/91.1876 - 1.0;
+
+    double sin2t = s0 + d1*L_H + d2*L_H*L_H + d3*L_H*L_H*L_H*L_H
+                 + d4*(Delta_H*Delta_H - 1.0) + d5*Delta_alphae + d6*Delta_t
+                 + d7*Delta_t*Delta_t + d8*Delta_t*(Delta_H - 1.0)
+                 + d9*Delta_alphas + d10*Delta_Z;
+    return sin2t;
+}
+
+double StandardModel::GammaW() const {
+    return 0.0;
+}
+
+double StandardModel::GammaZ() const {
+    return 0.0;
+}
+
+double StandardModel::sigma_had() const {
+    return 0.0;
+}
+
+double StandardModel::R_l() const {
+    return 0.0;
+}
+
+double StandardModel::R_c() const {
+    return 0.0;
+}
+
+double StandardModel::R_b() const {
+    return 0.0;
+}
+
+double StandardModel::AFB_l() const {
+    return 0.0;
+}
+
+double StandardModel::AFB_c() const {
+    return 0.0;
+}
+
+double StandardModel::AFB_b() const {
+    return 0.0;
+}
+
+double StandardModel::A_l() const {
+    return 0.0;
+}
+
+double StandardModel::A_c() const {
+    return 0.0;
+}
+
+double StandardModel::A_b() const {
+    return 0.0;
+}
+
+double StandardModel::S() const {
+    return 0.0;
+}
+
+double StandardModel::T() const {
+    return 0.0;
+}
+
+double StandardModel::U() const {
+    return 0.0;
+}
+
+
 
 
