e5c442a26b7901f9787997bca8db4fd0a725ea32
silvest <silvest@2e41be99-0402-4e17-b588-3552aed5b166>
2011-03-03 15:28:09 +0000

    added virtual method returning the complex Delta Bd=2 amplitude
    git-svn-id: file:///afs/infn.it/roma1/project/susy/susy/svnroot/trunk/SusyFit@100 2e41be99-0402-4e17-b588-3552aed5b166

--- StandardModel/src/Model.h
+++ StandardModel/src/Model.h
@@ -8,11 +8,19 @@
 #ifndef MODEL_H
 #define	MODEL_H
 
+#include "Particle.h"
+#include <gslpp_complex.h>
+
 class Model {
 public:
     Model();
     Model(const Model& orig);
     virtual ~Model();
+    /**
+     * get the @f$\Delta B=\Delta D=2@f$ amplitude
+     * @return @f$\langle \bar B_d \vert \mathcal{H}_\mathrm{eff}\vert B_d\rangle@f$ //CHECK!!
+     */
+    virtual gslpp::complex getDBD2Amplitude(const int LE) const;
 private:
 
 };
