4c56c33920cc1e5e2d93e36f6b84fd95f872bf72
silvest <silvest@2e41be99-0402-4e17-b588-3552aed5b166>
2011-03-28 10:18:02 +0000

    First release
    git-svn-id: file:///afs/infn.it/roma1/project/susy/susy/svnroot/trunk/SusyFit@115 2e41be99-0402-4e17-b588-3552aed5b166

--- Observables/src/Observable.cpp
+++ Observables/src/Observable.cpp
@@ -7,10 +7,9 @@
 
 #include "Observable.h"
 
-void Observable::Init(const std::string name_i, const bool tMCMC_i, const double min_i,
+void Observable::Init(const bool tMCMC_i, const double min_i,
     const double max_i, const std::string distr_i, const std::string filename_i,
         const double ave_i, const double errg_i, const double errf_i) {
-    name =  name_i;
     min = min_i;
     max = max_i;
     tMCMC = tMCMC_i;
@@ -21,40 +20,47 @@ void Observable::Init(const std::string name_i, const bool tMCMC_i, const double
     errf = errf_i;
 }
 
-Observable::Observable(const std::string name_i, const bool tMCMC_i, const double min_i,
-    const double max_i, const std::string distr_i) {
-    if((distr_i.compare("noweight")!=0) || tMCMC_i) {
-        std::cout << "Wrong Observable constructor called: Observable(" <<
-                name_i <<", " << tMCMC_i <<", " << min_i <<", " << max_i <<", " << distr_i << ")"<<std::endl;
-    }
-    Init(name_i, tMCMC_i, min_i, max_i, distr_i, "", 0., 0., 0.);
-}
-Observable::Observable(const std::string name_i, const bool tMCMC_i, const double min_i,
-    const double max_i, const std::string distr_i, const std::string filename_i) {
-    if(distr_i.compare("file")!=0) {
-        std::cout << "Wrong Observable constructor called: Observable(" << 
-                name_i <<", " << tMCMC_i <<", " << min_i <<", " << max_i <<", " << distr_i << ", " << filename_i << ")"<<std::endl;
-    }
-    Init(name_i, tMCMC_i, min_i, max_i, distr_i, filename_i, 0., 0., 0.);
+Observable::Observable (const std::string name_i) {
+    name = name_i;
 }
 
-Observable::Observable(const std::string name_i, const bool tMCMC_i, const double min_i,
-    const double max_i, const std::string distr_i, const double ave_i,
-    const double errg_i, const double errf_i) {
-    if(distr_i.compare("weight")!=0) {
-        std::cout << "Wrong Observable constructor called: Observable(" <<
-                name_i <<", " << tMCMC_i <<", " << min_i <<", " << max_i <<", " << distr_i
-                << ", " << ave_i <<", " << errg_i <<", " << errf_i <<", "  << ")"<<std::endl;
-    }
-    Init(name_i, tMCMC_i, min_i, max_i, distr_i, "", ave_i, errg_i, errf_i);
-}
 
 Observable::Observable(const Observable& orig) {
-    Init(orig.name, orig.tMCMC, orig.min, orig.max, orig.distr, orig.filename,
+	name = orig.name;
+    Init(orig.tMCMC, orig.min, orig.max, orig.distr, orig.filename,
             orig.ave, orig.errg, orig.errf);
 }
 
 Observable::~Observable() {
+}
+
+void Observable::Set(const bool tMCMC_i, const double min_i,
+    const double max_i, const std::string distr_i) {
+    if((distr_i.compare("noweight")!=0) || tMCMC_i) {
+        std::cout << "Wrong Observable set called: Set(" <<
+                 name << tMCMC_i <<", " << min_i <<", " << max_i <<", " << distr_i << ")"<<std::endl;
+    }
+    Init(tMCMC_i, min_i, max_i, distr_i, "", 0., 0., 0.);
+}
+
+void Observable::Set(const bool tMCMC_i, const double min_i,
+    const double max_i, const std::string distr_i, const std::string filename_i) {
+    if(distr_i.compare("file")!=0) {
+        std::cout << "Wrong Observable set called: Set(" << 
+                name <<", " << tMCMC_i <<", " << min_i <<", " << max_i <<", " << distr_i << ", " << filename_i << ")"<<std::endl;
+    }
+    Init(tMCMC_i, min_i, max_i, distr_i, filename_i, 0., 0., 0.);
+}
+
+void Observable::Set(const bool tMCMC_i, const double min_i,
+    const double max_i, const std::string distr_i, const double ave_i,
+    const double errg_i, const double errf_i) {
+    if(distr_i.compare("weight")!=0) {
+        std::cout << "Wrong Observable set called: Set(" <<
+                name <<", " << tMCMC_i <<", " << min_i <<", " << max_i <<", " << distr_i
+                << ", " << ave_i <<", " << errg_i <<", " << errf_i <<", "  << ")"<<std::endl;
+    }
+    Init(tMCMC_i, min_i, max_i, distr_i, "", ave_i, errg_i, errf_i);
 }
 
 std::ostream& operator<<(std::ostream& output, const Observable& o)
