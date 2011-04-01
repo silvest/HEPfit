fe2e744c04f7a86aa112027fbff023d7fc1211e8
silvest <silvest@2e41be99-0402-4e17-b588-3552aed5b166>
2011-04-01 15:12:46 +0000

    First release
    git-svn-id: file:///afs/infn.it/roma1/project/susy/susy/svnroot/trunk/SusyFit@130 2e41be99-0402-4e17-b588-3552aed5b166

---
+++ InputParser/tests/newsimpletest.cpp
@@ -0,0 +1,38 @@
+/* 
+ * File:   newsimpletest.cpp
+ * Author: silvest
+ *
+ * Created on Mar 28, 2011, 11:27:12 AM
+ */
+
+#include <stdlib.h>
+#include <iostream>
+#include "../src/InputParser.h"
+
+/*
+ * Simple C++ Test Suite
+ */
+
+void test1() {
+    std::cout << "InputParserTest test 1" << std::endl;
+    std::vector<Observable> Obs;
+    std::vector<ModelParameter> ModPars;
+    InputParser::ReadParameters("test.conf",ModPars,Obs);
+    for (std::vector<ModelParameter>::iterator it=ModPars.begin() ; it < ModPars.end(); it++ )
+        std::cout << *it;
+    for (std::vector<Observable>::iterator xt=Obs.begin() ; xt < Obs.end(); xt++ )
+        std::cout << *xt;
+}
+
+int main(int argc, char** argv) {
+    std::cout << "%SUITE_STARTING% InputParserTest" << std::endl;
+    std::cout << "%SUITE_STARTED%" << std::endl;
+
+    std::cout << "%TEST_STARTED% test1 (InputParserTest)" << std::endl;
+    test1();
+    std::cout << "%TEST_FINISHED% time=0 test1 (InputParserTest)" << std::endl;
+
+    std::cout << "%SUITE_FINISHED% time=0" << std::endl;
+
+    return (EXIT_SUCCESS);
+}
