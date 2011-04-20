b036644b9ee8a4cb614e3eec675e9a71c5c7e495
silvest <silvest@2e41be99-0402-4e17-b588-3552aed5b166>
2011-04-20 11:40:50 +0000

    Added observables to be computed but not used in the MCMC weight Added trees with observables and weights to the root file output Added histograms for observables Added deletion of new objects in destructor Added 2D Observables (only input or output 2D Histograms for the moment)
    git-svn-id: file:///afs/infn.it/roma1/project/susy/susy/svnroot/trunk/SusyFit@141 2e41be99-0402-4e17-b588-3552aed5b166

--- MonteCarlo/src/MonteCarlo.h
+++ MonteCarlo/src/MonteCarlo.h
@@ -8,16 +8,21 @@
 #ifndef __MONTECARLO__H
 #define __MONTECARLO__H
 
-#include <BCModel.h>
-#include <BCH1D.h>
-#include <TH1D.h>
-#include <TFile.h>
+#include <BAT/BCModel.h>
+#include <BAT/BCH1D.h>
+#include <BAT/BCH2D.h>
+#include <BAT/BCModelOutput.h>
+#include <root/TH1D.h>
+#include <root/TH2D.h>
+#include <root/TFile.h>
 #include <Observable.h>
+#include <Observable2D.h>
 #include <ModelParameter.h>
 #include <Model.h>
 #include <map>
 
 #define NBINS1D 100
+#define NBINS2D 100
 
 // This is a MonteCarlo header file.
 // Model source code is located in file MonteCarlo/MonteCarlo.cxx
@@ -27,22 +32,29 @@ class MonteCarlo : public BCModel {
 public:
 
     // Constructors and destructor
-    MonteCarlo(const char * name, Model * Mod, std::vector<ModelParameter> ModPars, std::vector<Observable> Obs);
+    MonteCarlo(const char * name, Model * Mod, std::vector<ModelParameter> ModPars, std::vector<Observable> Obs, std::vector<Observable2D> Obs2D);
     ~MonteCarlo();
 
     // Methods to overload, see file MonteCarlo.cxx
     void DefineParameters();
     double LogLikelihood(std::vector <double> parameters);
-    void PrintHistogram();
-    //void MCMCIterationInterface();
-    void MCMCCurrentPointInterface(std::vector <double> & point, int ichain, bool accepted);
+    void PrintHistogram(BCModelOutput& out);
+    void MCMCIterationInterface();
+    void AddChains();
+    double Weight(const Observable& obs, const double& th);
+    double Weight(const Observable2D& obs, const double& th1, const double& th2); 
+
 private:
     std::vector<ModelParameter> ModPars;
-    std::vector<Observable> Obs_MCMC, Obs_noMCMC;
+    std::vector<Observable> Obs_MCMC, Obs_ALL; 
+    std::vector<Observable2D> Obs2D_MCMC, Obs2D_ALL;
     Model * Mod;
     std::map<std::string, double> DPars;
     std::map<std::string, BCH1D * > Histo1D;
     std::map<std::string, TH1D * > InHisto1D;
+    std::map<std::string, BCH2D * > Histo2D;
+    std::map<std::string, TH2D * > InHisto2D;
+    double *obval, *obweight;
 };
 // ---------------------------------------------------------
 
