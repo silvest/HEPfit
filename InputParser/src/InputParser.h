fe89e12caca18c566a104ba8b879e7a26df995ae
silvest <silvest@2e41be99-0402-4e17-b588-3552aed5b166>
2011-04-20 11:37:16 +0000

    ThFactory moved to its own class Added deletion of new objects in destructor Added 2D Observables (only input or output 2D Histograms for the moment)
    git-svn-id: file:///afs/infn.it/roma1/project/susy/susy/svnroot/trunk/SusyFit@140 2e41be99-0402-4e17-b588-3552aed5b166

--- InputParser/src/InputParser.h
+++ InputParser/src/InputParser.h
@@ -8,7 +8,9 @@
 #ifndef INPUTPARSER_H
 #define	INPUTPARSER_H
 
+#include "ThFactory.h"
 #include <Observable.h>
+#include <Observable2D.h>
 #include <ThObservable.h>
 #include <ModelParameter.h>
 #include <StandardModel.h>
@@ -25,14 +27,14 @@ public:
     InputParser(const InputParser& orig);
     virtual ~InputParser();
     void ReadParameters(const char *, std::vector<ModelParameter>&,
-    std::vector<Observable>&);
+        std::vector<Observable>&,  std::vector<Observable2D>&);
     Model* GetMyModel() const {
         return myModel;
     }
 private:
     Model * myModel; 
     Flavour * myFlavour;
-    ThObservable * ThFactory(const std::string& name);
+    ThFactory * thf;
 };
 
 #endif	/* INPUTPARSER_H */
