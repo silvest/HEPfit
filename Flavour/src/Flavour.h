0b3f3e192895a1f932f32ada032fc342e915e8eb
silvest <silvest@2e41be99-0402-4e17-b588-3552aed5b166>
2011-05-25 08:54:04 +0000

    Now extends ThObsType and contains the Delta F=2 effective Hamiltonian
    git-svn-id: file:///afs/infn.it/roma1/project/susy/susy/svnroot/trunk/SusyFit@149 2e41be99-0402-4e17-b588-3552aed5b166

--- Flavour/src/Flavour.h
+++ Flavour/src/Flavour.h
@@ -8,23 +8,34 @@
 #ifndef FLAVOUR_H
 #define	FLAVOUR_H
 
-#include "Dmb.h"
-#include "Vud.h"
-#include "Vus.h"
-#include "Vcb.h"
-#include "Vub.h"
-#include "alpha.h"
-#include "alpha_2a.h"
-#include "gamma.h"
+#include <ThObsType.h>
+#include <StandardModel.h>
+#include <DmBd.h>
+#include <DmBd0.h>
+#include <Vud.h>
+#include <Vus.h>
+#include <Vcb.h>
+#include <Vub.h>
+#include <alpha.h>
+#include <alpha_2a.h>
+#include <gamma.h>
 
-class Flavour {
+using namespace gslpp;
+
+class Flavour : public ThObsType {
 public:
-    Flavour(StandardModel&);
-    Flavour(const Flavour& orig);
-    virtual ~Flavour();
+
+    Flavour(const StandardModel& SM_i) : ThObsType(SM_i), DF2(SM_i) {
+    };
+
+    Flavour(const Flavour& orig) : ThObsType(orig.SM), DF2(orig.SM) {
+     };
+
+    virtual ~Flavour() {
+    };
+
 private:
-    StandardModel * myModel;
+    HeffDF2 DF2;
 };
 
 #endif	/* FLAVOUR_H */
-
