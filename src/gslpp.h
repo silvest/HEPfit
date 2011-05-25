016511e50bc50fea2a3b72c9c006a5af535c86ca
silvest <silvest@2e41be99-0402-4e17-b588-3552aed5b166>
2011-05-25 09:13:53 +0000

    Utility include containing all gslpp headers
    git-svn-id: file:///afs/infn.it/roma1/project/susy/susy/svnroot/trunk/SusyFit@156 2e41be99-0402-4e17-b588-3552aed5b166

---
+++ src/gslpp.h
@@ -0,0 +1,18 @@
+/* 
+ * File:   gslpp.h
+ * Author: marco
+ *
+ * Created on May 4, 2011, 6:29 PM
+ */
+
+#ifndef GSLPP_H
+#define	GSLPP_H
+
+#include <gslpp_complex.h>
+#include <gslpp_vector_double.h>
+#include <gslpp_vector_complex.h>
+#include <gslpp_matrix_double.h>
+#include <gslpp_matrix_complex.h>
+
+#endif	/* GSLPP_H */
+
