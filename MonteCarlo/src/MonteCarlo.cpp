e1ce13e7f4e6fbd6afeb5cde0ae0c242377e9637
silvest <silvest@2e41be99-0402-4e17-b588-3552aed5b166>
2011-05-25 10:32:29 +0000

    Moved BCModel and BAT MonteCarlo stuff to MonteCarloEngine class Now contains an InputParser and a MonteCarloEngine object
    git-svn-id: file:///afs/infn.it/roma1/project/susy/susy/svnroot/trunk/SusyFit@162 2e41be99-0402-4e17-b588-3552aed5b166

--- MonteCarlo/src/MonteCarlo.cpp
+++ MonteCarlo/src/MonteCarlo.cpp
@@ -2,297 +2,111 @@
  * File:   MonteCarlo.cpp
  * Author: silvest
  * 
- * Created on March 8, 2011, 3:18 PM
+ * Created on April 28, 2011, 5:49 PM
  */
 
 #include "MonteCarlo.h"
-#include <BAT/BCMath.h>
-#include <root/TF1.h>
-#include <root/TMath.h>
-#include <root/TTree.h>
-#include <root/TROOT.h>
+#include <BAT/BCAux.h>
+#include <BAT/BCLog.h>
 
-MonteCarlo::MonteCarlo(const char * name, Model * Mod_i,
-        std::vector<ModelParameter> ModPars_i, std::vector<Observable> Obs_i,
-        std::vector<Observable2D> Obs2D_i) :
-BCModel(name) {
-    // default constructor
-    Mod = Mod_i;
-    ModPars = ModPars_i;
-    Obs_ALL = Obs_i;
-    Obs2D_ALL = Obs2D_i;
-    int k = 0, kweight = 0;
-    for (std::vector<Observable>::iterator it = Obs_i.begin();
-            it < Obs_i.end(); it++) {
-        if ((it->getDistr()).compare("file") == 0) {
-            TFile *lik = new TFile((it->getFilename() + ".root").c_str(), "read");
-            TH1D *htmp = (TH1D*) (lik->Get(it->getHistoname().c_str()));
-            if (htmp == NULL) {
-                std::cout << "nonexistent histogram called " + it->getHistoname()
-                        + " in " + it->getFilename() + ".root\n";
-                exit(EXIT_FAILURE);
-            }
-            TH1D *inhisto = (TH1D *) htmp->Clone((it->getFilename() + "_" + it->getHistoname()).c_str());
-            inhisto->SetDirectory(gROOT);
-            InHisto1D[it->getFilename() + it->getHistoname()] = inhisto;
-            std::cout << "added input histogram " << InHisto1D[it->getFilename() + it->getHistoname()]->GetName() << std::endl;
-            it->setMin(inhisto->GetXaxis()->GetXmin());
-            it->setMax(inhisto->GetXaxis()->GetXmax());
-            lik->Close();
-            delete lik;
-        }
-        if (it->isTMCMC()) {
-            Obs_MCMC.push_back(*it);
-        } else {
-            k++;
-            if (it->getDistr().compare("noweight") != 0)
-                kweight++;
-        }
-        if (Histo1D.find(it->getThname()) == Histo1D.end()) {
-            TH1D * histo = new TH1D(it->getThname().c_str(), it->getLabel().c_str(), NBINS1D,
-                    it->getMin(), it->getMax());
-            histo->GetXaxis()->SetTitle(it->getLabel().c_str());
-            BCH1D * bchisto = new BCH1D(histo);
-            Histo1D[it->getThname()] = bchisto;
-        }
-    }
-    obval = new double[fMCMCNChains * k];
-    obweight = new double[fMCMCNChains * kweight];
-    for (std::vector<Observable2D>::iterator it = Obs2D_ALL.begin();
-            it < Obs2D_ALL.end(); it++) {
-        if ((it->getDistr()).compare("file") == 0) {
-            TFile *lik2 = new TFile((it->getFilename() + ".root").c_str(), "read");
-            TH2D *htmp2 = (TH2D*) (lik2->Get(it->getHistoname().c_str()));
-            if (htmp2 == NULL) {
-                std::cout << "nonexistent histogram called " + it->getHistoname()
-                        + " in " + it->getFilename() + ".root\n";
-                exit(EXIT_FAILURE);
-            }
-            TH2D *inhisto2 = (TH2D *) htmp2->Clone((it->getFilename() + "_" + it->getHistoname()).c_str());
-            inhisto2->SetDirectory(gROOT);
-            InHisto2D[it->getFilename() + it->getHistoname()] = inhisto2;
-            std::cout << "added 2D input histogram " << InHisto2D[it->getFilename() + it->getHistoname()]->GetName() << std::endl;
-            it->setMin(inhisto2->GetXaxis()->GetXmin());
-            it->setMax(inhisto2->GetXaxis()->GetXmax());
-            it->setMin2(inhisto2->GetYaxis()->GetXmin());
-            it->setMax2(inhisto2->GetYaxis()->GetXmax());
-            lik2->Close();
-            delete lik2;
-            if (it->isTMCMC()) {
-                Obs2D_MCMC.push_back(*it);
-            } else {
-                std::cout << "cannot handle noMCMC for Observable2D file yet!\n";
-                exit(EXIT_FAILURE);
-            }
-        } 
-        else if (it->getDistr().compare("weight") == 0) {
-            std::cout << "do not use Observable2D for analytic 2D weights!\n";
+MonteCarlo::MonteCarlo(const std::string& ModelConf_i,
+        const std::string& MonteCarloConf_i, const std::string& OutFile_i) :
+myInputParser(), MCEngine(ModPars, Obs, Obs2D) {
+    ModelConf = ModelConf_i;
+    MCMCConf = MonteCarloConf_i;
+    OutFile = OutFile_i;
+}
+
+MonteCarlo::MonteCarlo(const MonteCarlo& orig) :
+myInputParser(), MCEngine(ModPars, Obs, Obs2D) {
+    ModelConf = orig.ModelConf;
+    MCMCConf = orig.MCMCConf;
+    OutFile = orig.OutFile;
+}
+
+MonteCarlo::~MonteCarlo() {
+}
+
+void MonteCarlo::Run() {
+    try {
+        MCEngine.SetName(myInputParser.ReadParameters(ModelConf, ModPars, Obs, Obs2D).c_str());
+        std::map<std::string, double> DP;
+        for (std::vector<ModelParameter>::iterator it = ModPars.begin(); it < ModPars.end(); it++)
+            DP[it->name] = it->ave;
+
+        if (!myInputParser.GetMyModel()->init(DP)) {
+            std::cout << "parameter(s) missing in model initialization" << std::endl;
             exit(EXIT_FAILURE);
         }
-        if (Histo2D.find(it->getThname() + "_vs_" + it->getThname2()) == Histo2D.end()) {
-            TH2D * histo2 = new TH2D((it->getThname() + "_vs_" + it->getThname2()).c_str(), (it->getLabel() + " vs " + it->getLabel2()).c_str(), NBINS2D,
-                    it->getMin(), it->getMax(), NBINS2D, it->getMin2(), it->getMax2());
-            histo2->GetXaxis()->SetTitle(it->getLabel().c_str());
-            histo2->GetYaxis()->SetTitle(it->getLabel2().c_str());
-            BCH2D * bchisto2 = new BCH2D(histo2);
-            Histo2D[it->getThname() + "_vs_" + it->getThname2()] = bchisto2;
-        }
-    }
-    DefineParameters();
-};
 
-// ---------------------------------------------------------
-
-MonteCarlo::~MonteCarlo()
-// default destructor
-{
-    delete obval, obweight;
-    for (std::map<std::string, BCH1D *>::iterator it = Histo1D.begin();
-            it != Histo1D.end(); it++)
-        delete it->second;
-    for (std::map<std::string, BCH2D *>::iterator it = Histo2D.begin();
-            it != Histo2D.end(); it++)
-        delete it->second;
-    for (std::map<std::string, TH1D *>::iterator it = InHisto1D.begin();
-            it != InHisto1D.end(); it++)
-        delete it->second;
-    for (std::map<std::string, TH2D *>::iterator it = InHisto2D.begin();
-            it != InHisto2D.end(); it++)
-        delete it->second;
-};
-
-// ---------------------------------------------------------
-
-void MonteCarlo::DefineParameters() {
-    // Add parameters to your model here.
-    // You can then use them in the methods below by calling the
-    // parameters.at(i) or parameters[i], where i is the index
-    // of the parameter. The indices increase from 0 according to the
-    // order of adding the parameters.
-    int k = 0;
-    for (std::vector<ModelParameter>::iterator it = ModPars.begin(); it < ModPars.end(); it++) {
-        if(it->errf == 0. && it->errg == 0.)
-            continue;
-        AddParameter(it->name.c_str(), it->min, it->max);
-        std::cout << it->name << " " << k << std::endl;
-        DPars[it->name]=0.; 
-        if (it->errf == 0.) SetPriorGauss(k, it->ave, it->errg);
-        else if (it->errg == 0.) SetPriorConstant(k);
-        else {
-            TF1 * combined = new TF1(it->name.c_str(),
-                    "(TMath::Erf((x-[0]+[2])/sqrt(2.)/[1])-TMath::Erf((x-[0]-[2])/sqrt(2.)/[1]))/4./[2]",
-                    it->min, it->max);
-            combined->SetParameter(0, it->ave);
-            combined->SetParameter(1, it->errg);
-            combined->SetParameter(2, it->errf);
-            SetPrior(k, combined);
-            delete combined;
-        }
-        k++;
-    }
-}
-
-// ---------------------------------------------------------
-
-double MonteCarlo::Weight(const Observable& obs, const double& th) {
-    double logprob;
-    if (obs.getDistr().compare("weight") == 0) {
-        if (obs.getErrf() == 0.)
-            logprob = BCMath::LogGaus(th, obs.getAve(), obs.getErrg());
-        else if (obs.getErrg() == 0.) {
-            std::cout << "cannot use purely flat error in MCMC " << obs.getName() << std::endl;
-            exit(EXIT_FAILURE);
-        } else
-            logprob = log(TMath::Erf((th - obs.getAve() + obs.getErrf()) / sqrt(2.) / obs.getErrg()) -
-                TMath::Erf((th - obs.getAve() - obs.getErrf()) / sqrt(2.) / obs.getErrg()));
-    } 
-    else if (obs.getDistr().compare("file") == 0) {
-        TH1D * h = InHisto1D[obs.getFilename()+obs.getHistoname()];
-       logprob = log(h->GetBinContent(h->FindBin(th)));
-    }
-    else {
-        std::cout << "Weight called without weight! " << obs.getName() << std::endl;
-        exit(EXIT_FAILURE);
-    }
-    return (logprob);
-}
-
-double MonteCarlo::Weight(const Observable2D& obs, const double& th1, const double& th2) {
-    double logprob;
-    if (obs.getDistr().compare("file") == 0) {
-        TH2D * h = InHisto2D[obs.getFilename()+obs.getHistoname()];
-        logprob = log(h->GetBinContent(h->GetXaxis()->FindBin(th1),h->GetYaxis()->FindBin(th2)));
-    }
-    else {
-        std::cout << "2D Weight called without file! " << obs.getName() << std::endl;
-        exit(EXIT_FAILURE);
-    }
-    return (logprob);
-}
-
-double MonteCarlo::LogLikelihood(std::vector <double> parameters) {
-    // This methods returns the logarithm of the conditional probability
-    // p(data|parameters). This is where you have to define your model.
-
-    double logprob = 0.;
-
-    for(int k=0; k<parameters.size(); k++) {
-//        std::string pippo = GetParameter(k)->GetName();
-//        double pluto = parameters[k];
-//        DPars[pippo]=pluto;
-        DPars[GetParameter(k)->GetName()]=parameters[k];
-    }
-
-    Mod->update(DPars);
-    // std::cout << "loglike" << parameters[0];
-    for (std::vector<Observable>::iterator it = Obs_MCMC.begin(); it < Obs_MCMC.end(); it++) {
-        double th = it->getTheoryValue();
-        logprob += Weight(*it, th);
-    }
-
-    for (std::vector<Observable2D>::iterator it = Obs2D_MCMC.begin(); it < Obs2D_MCMC.end(); it++) {
-        double th1 = it->getTheoryValue();
-        double th2 = it->getTheoryValue2();
-        logprob += Weight(*it, th1, th2);
-    }
-
-    return logprob;
-}
-
-void MonteCarlo::MCMCIterationInterface() {
-    // get number of chains
-    int nchains = MCMCGetNChains();
-
-    // get number of parameters
-    int npar = GetNParameters();
-
-    for (int i = 0; i < nchains; ++i) {
-        for (int k = 0; k < npar; k++) {
-            //        std::string pippo = GetParameter(k)->GetName();
-            //        double pluto = parameters[k];
-            //        DPars[pippo]=pluto;
-            DPars[GetParameter(k)->GetName()] = fMCMCx.at(i * npar + k);
-        }
-
-        Mod->update(DPars);
-
-        // fill the histograms for all observables
-        int k = 0, kweight = 0;
-        for (std::vector<Observable>::iterator it = Obs_ALL.begin();
-                it < Obs_ALL.end(); it++) {
-            double th = it->getTheoryValue();
-            Histo1D[it->getThname()]->GetHistogram()->Fill(th);
-            if (!it->isTMCMC()) {
-                obval[(i + 1)*(k + 1) - 1] = th;
-                k++;
-                if (it->getDistr().compare("noweight") != 0) {
-                    obweight[(i + 1)*(kweight + 1) - 1] = Weight(*it, th);
-                    kweight++;
+        std::cout << ModPars.size() << " parameters defined." << std::endl;
+        std::cout << Obs.size() << " observables defined." << std::endl;
+        MCEngine.Initialize(myInputParser.GetMyModel());
+        BCModelOutput out(&MCEngine, OutFile.c_str());
+        //MonteCarlo configuration parser
+        std::ifstream ifile(MCMCConf.c_str());
+        std::string line;
+        while (!getline(ifile, line).eof()) {
+            if (line.at(0) == '#') continue;
+            boost::char_separator<char> sep(" ");
+            boost::tokenizer<boost::char_separator<char> > tok(line, sep);
+            boost::tokenizer<boost::char_separator<char> >::iterator beg = tok.begin();
+            if (beg->compare("NChains") == 0) {
+                ++beg;
+                MCEngine.MCMCSetNChains(atoi((*beg).c_str()));
+            } else if (beg->compare("PrerunMaxIter") == 0) {
+                ++beg;
+                MCEngine.MCMCSetNIterationsMax(atoi((*beg).c_str()));
+            } else if (beg->compare("Seed") == 0) {
+                ++beg;
+                int seed = atoi((*beg).c_str());
+                if (seed != 0)
+                    MCEngine.MCMCGetTRandom3()->SetSeed(seed);
+            } else if (beg->compare("Iterations") == 0) {
+                ++beg;
+                MCEngine.MCMCSetNIterationsRun(atoi((*beg).c_str()));
+            } else if (beg->compare("WriteChain") == 0) {
+                ++beg;
+                if (beg->compare("true") == 0) {
+                    out.WriteMarkovChain(true);
+                    MCEngine.AddChains();
                 }
+            } else {
+                std::cout << "wrong keyword in MonteCarlo config file: " << *beg << std::endl;
+                exit(EXIT_FAILURE);
             }
         }
-        for (std::vector<Observable2D>::iterator it = Obs2D_ALL.begin();
-                it < Obs2D_ALL.end(); it++) {
-            double th1 = it->getTheoryValue();
-            double th2 = it->getTheoryValue2();
-            Histo2D[it->getThname() + "_vs_" + it->getThname2()]->GetHistogram()->Fill(th1,th2);
-        }
-    }
-}
 
+        // set nicer style for drawing than the ROOT default
+        BCAux::SetStyle();
 
-void MonteCarlo::PrintHistogram(BCModelOutput& out) {
-    //print the BAT histograms to an eps file
-    for (std::vector<Observable>::iterator it = Obs_ALL.begin(); it < Obs_ALL.end();
-            it++) {
-//        std::string fname = "Observables/" + it->name + ".pdf";
-//        Histo1D[it->name]->Print(fname.c_str());
-        out.Write(Histo1D[it->getThname()]->GetHistogram());
-    }
-    for (std::vector<Observable2D>::iterator it = Obs2D_ALL.begin(); it < Obs2D_ALL.end();
-            it++) {
-//        std::string fname = "Observables/" + it->name + ".pdf";
-//        Histo1D[it->name]->Print(fname.c_str());
-        out.Write(Histo2D[it->getThname() + "_vs_" + it->getThname2()]->GetHistogram());
-    }
-}
+        // open log file
+        BCLog::OpenLog("log.txt");
+        BCLog::SetLogLevel(BCLog::detail);
 
-void MonteCarlo::AddChains() {
-    fMCMCFlagWritePreRunToFile = false;
-    int k = 0, kweight = 0;
-    for (std::vector<Observable>::iterator it = Obs_ALL.begin();
-            it < Obs_ALL.end(); it++) {
-        if (!it->isTMCMC()) {
-            for (int i = 0; i < fMCMCNChains; ++i)
-                fMCMCTrees[i]->Branch(it->getName().c_str(), &obval[(i + 1)*(k + 1) - 1],
-                    (it->getName() + "/D").c_str());
-            k++;
-            if (it->getDistr().compare("noweight") == 0) {
-                for (int i = 0; i < fMCMCNChains; ++i)
-                    fMCMCTrees[i]->Branch((it->getName() + "_weight").c_str(),
-                        &obweight[(i + 1)*(kweight + 1) - 1], (it->getName() + "_weight/D").c_str());
-                kweight++;
-            }
-        }
+        // run the MCMC and marginalize w.r.t. to all parameters
+        MCEngine.MarginalizeAll();
+
+        // find mode using the best fit parameters as start values
+        //    MCEngine.FindMode(MCEngine.GetBestFitParameters());
+
+        // draw all marginalized distributions into a PostScript file
+        MCEngine.PrintAllMarginalized("MonteCarlo_plots.ps");
+
+        // print results of the analysis into a text file
+        MCEngine.PrintResults("MonteCarlo_results.txt");
+
+        // print ratio
+        MCEngine.PrintHistogram(out);
+
+        out.WriteMarginalizedDistributions();
+        out.FillAnalysisTree();
+        out.Close();
+
+        // close log file
+        BCLog::CloseLog();
+    } catch (std::string message) {
+        std::cerr << message << std::endl;
+        exit(EXIT_FAILURE);
     }
 }
--- MonteCarlo/src/MonteCarlo.h
+++ MonteCarlo/src/MonteCarlo.h
@@ -2,60 +2,29 @@
  * File:   MonteCarlo.h
  * Author: silvest
  *
- * Created on March 8, 2011, 3:18 PM
+ * Created on April 28, 2011, 5:49 PM
  */
 
-#ifndef __MONTECARLO__H
-#define __MONTECARLO__H
+#ifndef MONTECARLO_H
+#define	MONTECARLO_H
 
-#include <BAT/BCModel.h>
-#include <BAT/BCH1D.h>
-#include <BAT/BCH2D.h>
-#include <BAT/BCModelOutput.h>
-#include <root/TH1D.h>
-#include <root/TH2D.h>
-#include <root/TFile.h>
-#include <Observable.h>
-#include <Observable2D.h>
-#include <ModelParameter.h>
-#include <Model.h>
-#include <map>
+#include <InputParser.h>
+#include "MonteCarloEngine.h"
 
-#define NBINS1D 100
-#define NBINS2D 100
-
-// This is a MonteCarlo header file.
-// Model source code is located in file MonteCarlo/MonteCarlo.cxx
-
-// ---------------------------------------------------------
-class MonteCarlo : public BCModel {
+class MonteCarlo {
 public:
-
-    // Constructors and destructor
-    MonteCarlo(const char * name, Model * Mod, std::vector<ModelParameter> ModPars, std::vector<Observable> Obs, std::vector<Observable2D> Obs2D);
-    ~MonteCarlo();
-
-    // Methods to overload, see file MonteCarlo.cxx
-    void DefineParameters();
-    double LogLikelihood(std::vector <double> parameters);
-    void PrintHistogram(BCModelOutput& out);
-    void MCMCIterationInterface();
-    void AddChains();
-    double Weight(const Observable& obs, const double& th);
-    double Weight(const Observable2D& obs, const double& th1, const double& th2); 
-
+    MonteCarlo(const std::string& ModelConf_i, const std::string& 
+            MonteCarloConf_i, const std::string& OutFile_i);
+    MonteCarlo(const MonteCarlo& orig);
+    virtual ~MonteCarlo();
+    void Run();
 private:
+    MonteCarloEngine MCEngine;
+    InputParser myInputParser;
     std::vector<ModelParameter> ModPars;
-    std::vector<Observable> Obs_MCMC, Obs_ALL; 
-    std::vector<Observable2D> Obs2D_MCMC, Obs2D_ALL;
-    Model * Mod;
-    std::map<std::string, double> DPars;
-    std::map<std::string, BCH1D * > Histo1D;
-    std::map<std::string, TH1D * > InHisto1D;
-    std::map<std::string, BCH2D * > Histo2D;
-    std::map<std::string, TH2D * > InHisto2D;
-    double *obval, *obweight;
+    std::vector<Observable> Obs;
+    std::vector<Observable2D> Obs2D;   
+    std::string ModelConf, MCMCConf, OutFile;
 };
-// ---------------------------------------------------------
 
-#endif
+#endif	/* MONTECARLO_H */
