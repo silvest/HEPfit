044f970e5c4970f3f47bf9c0f1fdeaf8cec3b3e5
silvest <silvest@2e41be99-0402-4e17-b588-3552aed5b166>
2011-05-25 10:45:55 +0000

    Include containing orders and schemes enums
    git-svn-id: file:///afs/infn.it/roma1/project/susy/susy/svnroot/trunk/SusyFit@173 2e41be99-0402-4e17-b588-3552aed5b166

---
+++ StandardModel/src/OrderScheme.h
@@ -0,0 +1,17 @@
+/* 
+ * File:   orderscheme.h
+ * Author: enrico
+ *
+ * Created on May 12, 2011, 11:19 AM
+ */
+
+#ifndef ORDERSCHEME_H
+#define	ORDERSCHEME_H
+
+#define MAXORDER NNLO
+
+enum schemes {NDR, HV, LRI};
+enum orders {LO=0, NLO, NNLO, FULLNLO, FULLNNLO};
+
+#endif	/* ORDERSCHEME_H */
+
