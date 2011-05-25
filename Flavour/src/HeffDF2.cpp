6c7962c74e3815d3d56ed241609933980d68a186
silvest <silvest@2e41be99-0402-4e17-b588-3552aed5b166>
2011-05-25 09:04:46 +0000

    Class for the Delta F=2 Effective Hamiltonian - still in progress
    git-svn-id: file:///afs/infn.it/roma1/project/susy/susy/svnroot/trunk/SusyFit@151 2e41be99-0402-4e17-b588-3552aed5b166

---
+++ Flavour/src/HeffDF2.cpp
@@ -0,0 +1,53 @@
+/* 
+ * File:   HeffDF2.cpp
+ * Author: silvest
+ * 
+ * Created on April 28, 2011, 4:33 PM
+ */
+
+#include "HeffDF2.h"
+#include <sstream>
+#include <QCD.h>
+
+HeffDF2::HeffDF2(const StandardModel & SM) : model(SM), coeffDF2(8, NDR, NLO), 
+        uDF2(8, NDR, NLO, SM.getNc()), drNDRLRI(5, 5, 0) {
+    double Nc = model.getNc();
+    drNDRLRI(0,0) = -(((-1. + Nc)*(-7. + log(4096.)))/Nc);
+    drNDRLRI(1,1) = (-2.*(-1. + 6.*Nc*Nc - 8.*log(2.) + Nc*(-13. + log(1024.))))/(3.*Nc);
+    drNDRLRI(1,2) = (-2.*(13. - 10.*log(2.) + Nc*(-5. + log(256.))))/(3.*Nc);
+    drNDRLRI(2,1) = (-8. + 6.*Nc*Nc + 20.*log(2.) - 8.*Nc*(1. + log(4.)))/(3.*Nc);
+    drNDRLRI(2,2) = (2.*(4. + Nc - 10.*Nc*log(2.) + log(256.)))/(3.*Nc);
+    drNDRLRI(3,3) = (2. - 4.*Nc*Nc + log(4.))/Nc;
+    drNDRLRI(3,4) = 2. - log(4.);
+    drNDRLRI(4,3) = -2.*(1. + log(2.));
+    drNDRLRI(4,4) = (2. + log(4.))/Nc;
+}
+
+HeffDF2::~HeffDF2() {
+}
+
+vector<complex>** HeffDF2::Coeff(double mu, schemes scheme) {
+
+    const std::vector<WilsonCoefficient>& mc = model.CMdf2();
+
+    coeffDF2.setMu(mu); // also assign coeff to zero
+
+    orders ordDF2 = coeffDF2.getOrder();
+    for (int i = 0; i < mc.size(); i++)
+        for (int j = LO; j <= ordDF2; j++)
+            for (int k = LO; k <= j; k++)
+                coeffDF2.setCoeff(*coeffDF2.Coeff(orders(j)) +
+                    Df2Evol(mu, mc[i].getScale(), mc[i].getScheme(), orders(k)) *
+                    (*(mc[i].Coeff(orders(j - k)))), orders(j));
+
+    ChangeScheme(scheme, mc[0].getScheme(), ordDF2);
+
+    return coeffDF2.getCoeff();
+}
+
+void HeffDF2::ChangeScheme(schemes schout, schemes schin, orders order) {
+    if (schout == schin) return;
+    //...
+    coeffDF2.setScheme(schout);
+}
+
---
+++ Flavour/src/HeffDF2.h
@@ -0,0 +1,39 @@
+/* 
+ * File:   HeffDF2.h
+ * Author: silvest
+ *
+ * Created on April 28, 2011, 4:34 PM
+ */
+
+#ifndef HEFFDF2_H
+#define	HEFFDF2_H
+
+#include <StandardModel.h>
+#include <WilsonCoefficient.h>
+#include <RGEvolutor.h>
+
+using namespace gslpp;
+
+class HeffDF2 {
+public:
+    HeffDF2(const StandardModel & SM);
+    virtual ~HeffDF2();
+//    vector<complex>& getCoeff(double mu, schemes scheme = NDR, orders order = NLO); 
+    matrix<double>& Df2Evol(double mu, double M, schemes scheme);
+    void ChangeScheme(schemes schout, schemes schin, orders order);
+    vector<complex>** Coeff(double mu, schemes scheme);
+    matrix<double> AnomalousDimension(orders order, unsigned int nf = 0) const;
+
+private:
+    // Magic Number
+    // c and d are the coefficient of als(mu) e als(M)
+    // first index number of flavours
+    double b[5][5][5], c[3][5][5][5], d[3][5][5][5];
+    matrix<double> drNDRLRI;
+    const StandardModel& model;
+    std::vector<WilsonCoefficient> mcDF2;
+    WilsonCoefficient coeffDF2;
+    RGEvolutor uDF2;
+};
+
+#endif	/* HEFFDF2_H */
