492b2c5b3c1ffa9922f4ba6146cf55120e7f8e38
Mauro_87 <Mauro_87@2e41be99-0402-4e17-b588-3552aed5b166>
2012-03-19 11:36:34 +0000

    Calculation of DC=1 processes under development
    git-svn-id: file:///afs/infn.it/roma1/project/susy/susy/svnroot/trunk/SusyFit@280 2e41be99-0402-4e17-b588-3552aed5b166

---
+++ Flavour/src/HeffDC1.cpp
@@ -0,0 +1,169 @@
+/* 
+ * File:   HeffDF1bsg.cpp
+ * Author: stefano
+ *
+ * Created on 15 settembre 2011, 15.36
+ */
+    
+#include "HeffDC1.h"
+
+using namespace gslpp;
+
+HeffDC1::HeffDC1(const StandardModel & SM, StandardModelMatching & SM_Matching) :
+        model(SM), modelmatching(SM_Matching), coeffdc1(10, NDR, NLO), 
+        coeffdc1g(10, NDR, NLO), u(10, NDR, NLO, SM), ug(10, NDR, NLO, SM), 
+        ckm(3,0.), COEFF_pi(10,0.), COEFF_K(10,0.){
+  
+    double co = - 4. * model.getGF() / sqrt(2);
+    ckm = model.getVCKM();
+    complex lambdab_cu = - ckm(1,2)*ckm(0,2).conjugate(); // - V_cb * V_ub^{*}
+    complex lambdad_cu = ckm(1,0)*ckm(0,0).conjugate(); // V_cd * V_ud^{*}
+    complex lambdas_cu = ckm(1,1)*ckm(0,1).conjugate(); // V_cs * V_us^{*}
+    COEFF_pi.assign(0,0, co * lambdad_cu); COEFF_pi.assign(1,1, co * lambdad_cu);
+    COEFF_pi.assign(2,2, co * lambdab_cu); COEFF_pi.assign(3,3, co * lambdab_cu); 
+    COEFF_pi.assign(4,4, co * lambdab_cu); COEFF_pi.assign(5,5, co * lambdab_cu);
+    COEFF_pi.assign(6,6, co * lambdab_cu); COEFF_pi.assign(7,7, co * lambdab_cu);
+    COEFF_pi.assign(8,8, 1.); COEFF_pi.assign(9,9, 1.);
+    COEFF_K.assign(0,0, co * lambdas_cu);  COEFF_K.assign(1,1, co * lambdas_cu); 
+    COEFF_K.assign(2,2, co * lambdab_cu); COEFF_K.assign(3,3, co * lambdab_cu); 
+    COEFF_K.assign(4,4, co * lambdab_cu); COEFF_K.assign(5,5, co * lambdab_cu);
+    COEFF_K.assign(6,6, co * lambdab_cu); COEFF_K.assign(7,7, co * lambdab_cu);
+    COEFF_K.assign(8,8, 1.); COEFF_K.assign(9,9, 1.);
+    
+}
+
+HeffDC1::~HeffDC1() {
+}
+
+ /******************************************************************************
+ * evolution Wilson Coefficien D-> pi pi, K K                                  * 
+ * Misiak basis                                                                *
+ ******************************************************************************/
+vector<complex>** HeffDC1::ComputeCoeffDC1_pi(double mu, schemes scheme) {
+    
+    const std::vector<WilsonCoefficient>& mc = modelmatching.CMd1();
+    coeffdc1.setMu(mu); 
+    orders ordDF1 = coeffdc1.getOrder();
+    for (int i = 0; i < mc.size(); i++){
+        if(i != 1){
+        for (int j = LO; j <= ordDF1; j++){
+            for (int k = LO; k <= j; k++){
+                coeffdc1.setCoeff(*coeffdc1.getCoeff(orders(j)) +
+                    ug.DC1Evol(mu, mc[i].getMu(), orders(k), mc[i].getScheme()) *
+                    (*(mc[i].getCoeff(orders(j - k)))), orders(j));
+                }
+            }
+        }
+    } 
+    
+    coeffdc1.setScheme(scheme);
+    
+    return coeffdc1.getCoeff();
+}
+
+vector<complex>** HeffDC1::ComputeCoeffDC1_K(double mu, schemes scheme) {
+    
+    const std::vector<WilsonCoefficient>& mc = modelmatching.CMd1();
+    coeffdc1.setMu(mu); 
+    
+    orders ordDF1 = coeffdc1.getOrder();
+    for (int i = 0; i < mc.size(); i++){
+        std::cout << " SIZE i " << i << std::endl << std::endl;
+        for (int j = LO; j <= ordDF1; j++){
+            for (int k = LO; k <= j; k++){
+                coeffdc1.setCoeff(*coeffdc1.getCoeff(orders(j)) + 
+                    ug.DC1Evol(mu, mc[i].getMu(), orders(k), mc[i].getScheme()) *
+                    (*(mc[i].getCoeff(orders(j - k)))), orders(j));
+                }
+            }
+        if(i == 0){
+            for (int j = LO; j <= ordDF1; j++){
+            coeffdc1.setCoeff(COEFF_K * (*coeffdc1.getCoeff(orders(j))), orders(j));
+            }
+        }
+    }
+   
+    coeffdc1.setScheme(scheme);
+   
+    return coeffdc1.getCoeff();
+}
+
+/*  
+vector<complex>** HeffDC1::ComputeCoeffDC1g(double mu, schemes scheme) {
+    
+    const std::vector<WilsonCoefficient>& mcg = modelmatching.CMd1();
+    gslpp::vector<complex> vecdc1g(10,0.);
+    coeffdc1g.setMu(mu); 
+    
+    orders ordDF1 = coeffdc1g.getOrder();
+    for (int i = 0; i < mcg.size(); i++){
+        for (int j = LO; j <= ordDF1; j++){
+            for (int k = LO; k <= j; k++)  {
+                coeffdc1g.setCoeff(*coeffdc1g.getCoeff(orders(j)) +
+                    ug.DC1Evol(mu, mcg[i].getMu(), orders(k), mcg[i].getScheme()) *
+                    (*(mcg[i].getCoeff(orders(j - k)))), orders(j));
+            }
+            //STAMPA PROVE
+      vecdc1g = *coeffdc1g.getCoeff(orders(j));
+
+        std::cout<< std::endl <<" CHROMOMAGNETIC  "<< j <<" CHROMOMAGNETIC "<<std::endl; 
+        std::cout<<" ++++++++++ "<< "C_8g_eff" <<" --> "<< vecdc1g(7) <<" ++++++++++++ "<<std::endl; 
+        std::cout << std::endl << " \\\\\\\\\\\\\\\\\\\\  FINE STAMPE DI PROVA \\\\\\\\\\\\\\\\\\\\\\\\ " <<std::endl <<std::endl;
+        }
+    }
+       
+    coeffdc1g.setScheme(scheme);
+   
+    return coeffdc1g.getCoeff();
+}*/
+
+ /*   
+  
+  vector<complex> a(6,0.);  
+  
+  // a(0) = C_3 + C_4/3.;
+  // a(1) = C_4 + C_3/3.;
+  // a(2) = - 2./3./M_PI * C_8g;
+  // a(3) = C_5 + C_6/3.;
+  // a(4) = C_6 + C_5/3.;
+  // a(5) = - 2./9./M_PI * C_8g;
+
+  vector<double> delta1(10,0.),delta2(10,0.),delta3(10,0.),delta4(10,0.),
+                 delta5(10,0.),delta6(10,0.),delta7(10,0.),delta8(10,0.),
+                 delta9(10,0.),delta10(10,0.);
+   
+  delta1(0) = 1.; delta2(1) = 1.; delta3(2) = 1.; delta4(3) = 1.;
+  delta5(4) = 1.; delta6(5) = 1.; delta7(6) = 1.; delta8(7) = 1.;
+  delta9(8) = 1.; delta10(9) = 1.;
+  
+  vector<complex> ** allcoeff = myFlavour.ComputeCoeffDC1_K(
+            myFlavour.getModel().getBD().getMu(), 
+            myFlavour.getModel().getBD().getScheme());
+  
+  std::cout << " Wilson coefficients SM+SUSY " << *allcoeff[LO] << std::endl; 
+  std::cout << " Wilson coefficients SM+SUSY " << *allcoeff[NLO] << std::endl;
+  
+  a(0) = (*allcoeff[LO] + *allcoeff[NLO]) * delta3 +  (*allcoeff[LO] + *allcoeff[NLO]) 
+         * delta4/3.;
+  
+  a(1) = (*allcoeff[LO] + *allcoeff[NLO]) * delta4 +  (*allcoeff[LO] + *allcoeff[NLO]) 
+         * delta3/3.; 
+  
+  a(2) = - 2./3./M_PI * (*allcoeff[LO] + *allcoeff[NLO]) * delta8 ; 
+   
+  a(3) =  (*allcoeff[LO] + *allcoeff[NLO]) * delta5 + (*allcoeff[LO] + *allcoeff[NLO]) 
+          * delta6/3.;
+  
+  a(4) =  (*allcoeff[LO] + *allcoeff[NLO]) * delta6 + (*allcoeff[LO] + *allcoeff[NLO]) 
+          * delta5/3.; 
+  
+  a(5) =  - 2./9./M_PI *(*allcoeff[LO] + *allcoeff[NLO]) * delta8 ;
+  
+  std::cout << " a_3 -->" << a(0);
+  std::cout << " a_4 -->" << a(1);
+  std::cout << " a_4 cromo --> " << a(2);
+  std::cout << " a_5 --> " << a(3);
+  std::cout << " a_6 --> " << a(4);
+  std::cout << " a_6 cromo --> " << a(5); 
+   
+   */ 
---
+++ Flavour/src/HeffDC1.h
@@ -0,0 +1,102 @@
+/* 
+ * File:   HeffDC1.h
+ * Author: Mauro_87
+ *
+ * Created on 23 novembre 2011, 10.52
+ */
+
+#ifndef HEFFDC1_H
+#define	HEFFDC1_H
+
+#include <StandardModel.h>
+/* 
+ * File:   EvolDC1.h
+ * Author: Mauro_87
+ *
+ * Created on 22 novembre 2011, 16.44
+ */
+#include <StandardModelMatching.h>
+#include <SUSYMassInsertion.h>
+#include <SUSYMassInsertionMatching.h>
+#include <WilsonCoefficient.h>
+#include "EvolDC1.h"
+#include "EvolDC1Buras.h"
+#include <sstream>
+
+using namespace gslpp;
+/**
+ * @class HeffDC1
+ * @brief \f$ |\Delta C = 1 | \f$ Hamiltonian Class
+ * @details The Hamiltonian for \f$ |\Delta C = 1 | \f$ processes as \f$ D^{0} \, \rightarrow \, \pi^{+} \, \pi^{-} \f$
+ * and \f$ D^{0} \, \rightarrow \, K^{+} \, K^{-} \f$ 
+ */
+class HeffDC1 {
+public:
+/**
+ * @brief HeffDC1 constructor 
+ * @param SM an object of StandardModel class
+ * @param SM_Matching an object of StandardModelMatching class
+ */
+    HeffDC1(const StandardModel & SM, StandardModelMatching & SM_Matching);
+    /**
+     * @brief HeffDC1 destructor
+     */
+    virtual ~HeffDC1();
+    /**
+     * @brief a method returning the evolved Wilson related to \f$ D^{0} \, \rightarrow \, \pi^{+} \, \pi^{-} \f$ 
+     * @details it returns the Wilson coefficients of  the process \f$ D^{0} \, \rightarrow \, \pi^{+} \, \pi^{-} \f$
+     * evolved to the low scale \f$ \mu \f$ with the associated coupling constants and CKM factors
+     * @param mu a double for the low scale of the evolution 
+     * @param scheme an enum "schemes" for the regularization scheme of the evolutor
+     * @return a vector<complex> pointer to pointer method for the evolved Wilson coefficients 
+     */
+    gslpp::vector<complex>** ComputeCoeffDC1_pi(double mu, schemes scheme = NDR);
+    /**
+     * @brief a method returning the evolved Wilson related to \f$ D^{0} \, \rightarrow \, K^{+} \, K^{-} \f$ 
+     * @details it returns the Wilson coefficients of the process \f$ D^{0} \, \rightarrow \, K^{+} \, K^{-} \f$
+     * evolved down to the low scale \f$ \mu \f$ with the associated coupling constants and CKM factors
+     * @param mu a double for the low scale of the evolution 
+     * @param scheme an enum "schemes" for the regularization scheme of the evolutor
+     * @return a vector<complex> pointer to pointer method for the evolved Wilson coefficients 
+     */
+    gslpp::vector<complex>** ComputeCoeffDC1_K(double mu, schemes scheme = NDR);
+    /**
+     *  
+     * @return an object of WilsonCoefficient class
+     */
+    WilsonCoefficient getCoeffDC1() const {
+        return coeffdc1;
+    }
+    /**
+     *  
+     * @return an object of EvolDC1 class
+     */
+    EvolDC1 getUDC1() const {
+        return ug;
+    }
+    /**
+     * 
+     * @return an object of EvolDC1Buras class
+     */
+    EvolDC1Buras getUDC1Buras() const {
+        return u;
+    }
+    /**
+     * 
+     * @return an object of StandardModel class
+     */
+    const StandardModel& GetModel() const {
+        return model;
+    }
+    
+private :
+    const StandardModel& model;
+    ModelMatching& modelmatching;
+    WilsonCoefficient coeffdc1, coeffdc1g;
+    EvolDC1 ug;
+    EvolDC1Buras u;
+    matrix<complex> ckm, COEFF_pi, COEFF_K;
+};
+
+#endif	/* HEFFDC1_H */
+
