5faa39acb5caa8c893e2727ba0570aec9025a59e
silvest <silvest@2e41be99-0402-4e17-b588-3552aed5b166>
2012-04-03 14:36:33 +0000

    Added X matrices and methods to set the spectrum
    git-svn-id: file:///afs/infn.it/roma1/project/susy/susy/svnroot/trunk/SusyFit@298 2e41be99-0402-4e17-b588-3552aed5b166

--- MFV/src/MFV.h
+++ MFV/src/MFV.h
@@ -9,179 +9,30 @@
 #define	MFV_H
 
 #include <SUSY.h>
+#include "Xmatrices.h"
 
 /**
  * @class MFV
- * @brief Minimal Flavour Violating SUSY model
+ * @brief calculation of soft masses and trilinear couplings for squarks and sleptons in 
+ * Minimal Flavour Violating SUSY models as in arXiv:0807.0801v2
  */
 class MFV : public SUSY {
 public:
-    MFV(const gslpp::matrix<gslpp::complex>& VCKM_i,
-            double mu_i, double md_i, double mc_i, double ms_i, double mt_i,
-            double mb_i, const gslpp::matrix<gslpp::complex>& UPMNS_i,
-            double me_i, double mmu_i, double mtau_i, double mnu1_i,
-            double mnu2_i, double mnu3_i, double tanb_i, gslpp::complex muH_i);
+    static const int NMFVvars = 33;
+    static const std::string MFVvars[NMFVvars];
 
-     /**
-     * @brief MFV constructor
-     * @param mQtilde_i squark doublet universal soft DRbar mass @f$\tilde{m}_{Q}(\tilde{m}_{Q})@f$
-     * @param mUtilde_i right-handed up-type universal squark soft DRbar mass @f$\tilde{m}_{U}(\tilde{m}_{U})@f$
-     * @param mDtilde_i right-handed down-type universal squark soft DRbar mass @f$\tilde{m}_{D}(\tilde{m}_{D})@f$
-     * @param Au_i universal trilinear up-type squark coupling
-     * @param Ad_i universal trilinear down-type squark coupling
-     * @param mLtilde_i slepton doublet universal soft DRbar mass @f$\tilde{m}_{L}(\tilde{m}_{L})@f$
-     * @param mEtilde_i right-handed charged slepton universal soft DRbar mass @f$\tilde{m}_{E}(\tilde{m}_{E})@f$
-     * @param mNtilde_i right-handed sneutrino universal soft DRbar mass @f$\tilde{m}_{N}(\tilde{m}_{N})@f$
-     * @param Ae_i universal trilinear charged slepton coupling
-     * @param An_i universal trilinear sneutrino coupling
-     * @param m1_i bino soft DRbar mass @f$m_{1}(m_{1})@f$
-     * @param m2_i wino soft DRbar mass @f$m_{2}(m_{2})@f$
-     * @param m3_i gluino soft DRbar mass @f$m_{3}(m_{3})@f$
-     * @param muH_i superpotential @f$\mu@f$ term
-     * @param tanb_i @f$\tan \beta @f$
-     * @param mHp_i charged Higgs mass @f$m_{H^+}@f$
-     * @param SM_i reference to a StandardModel object
-     */
-   MFV(const SUSY& SUSY_i);
+    MFV();
 
-    /**
-     * @brief MFV copy contructor
-     * @param orig reference to a MFV object
-     */
-    MFV(const MFV& orig);
-
-    /**
-     * @brief MFV destructor
-     */
-    virtual ~MFV();
-
-    void setParameters(double a1, double a2, double a3, gslpp::complex a4,
-        gslpp::complex a5, double b1, double b2, gslpp::complex b3, double b5,
-        double b6, gslpp::complex b7, gslpp::complex b8, double c4,
-        gslpp::complex c7, gslpp::complex c10, gslpp::complex c11);
-    /**
-     * 
-     * @return universal trilinear down-type squark coupling
-     */
-    double getAd() const { return Ad; }
-
-    /**
-     * @brief set universal trilinear down-type squark coupling
-     * @param Ad universal trilinear down-type squark coupling
-     */
-    void setAd(double Ad) { this->Ad = Ad; }
-
-    /**
-     *
-     * @return universal trilinear charged slepton coupling
-     */
-    double getAe() const { return Ae; }
-
-    /**
-     * @brief set universal trilinear charged slepton coupling
-     * @param Ae universal trilinear charged slepton coupling
-     */
-    void setAe(double Ae) { this->Ae = Ae; }
-
-    /**
-     * 
-     * @return universal trilinear sneutrino coupling
-     */
-    double getAn() const { return An; }
-
-    /**
-     * @brief set universal trilinear sneutrino coupling
-     * @param An universal trilinear sneutrino coupling
-     */
-    void setAn(double An) { this->An = An; }
-
-    /**
-     *
-     * @return universal trilinear up-type squark coupling
-     */
-    double getAu() const { return Au; }
-
-    /**
-     * @brief set universal trilinear up-type squark coupling
-     * @param Au universal trilinear up-type squark coupling
-     */
-    void setAu(double Au) { this->Au = Au; }
-
-    /**
-     * 
-     * @return right-handed down-type universal squark soft DRbar mass @f$\tilde{m}_{D}(\tilde{m}_{D})@f$
-     */
-    double getMDtilde() const { return mDtilde; }
-
-    /**
-     * @brief set right-handed down-type universal squark soft DRbar mass @f$\tilde{m}_{D}(\tilde{m}_{D})@f$
-     * @param mDtilde right-handed down-type universal squark soft DRbar mass @f$\tilde{m}_{D}(\tilde{m}_{D})@f$
-     */
-    void setMDtilde(double mDtilde) { this->mDtilde = mDtilde; }
-
-    /**
-     * 
-     * @return right-handed charged slepton universal soft DRbar mass @f$\tilde{m}_{E}(\tilde{m}_{E})@f$
-     */
-    double getMEtilde() const { return mEtilde; }
-
-    /**
-     * @brief set right-handed charged slepton universal soft DRbar mass @f$\tilde{m}_{E}(\tilde{m}_{E})@f$
-     * @param mEtilde right-handed charged slepton universal soft DRbar mass @f$\tilde{m}_{E}(\tilde{m}_{E})@f$
-     */
-    void setMEtilde(double mEtilde) { this->mEtilde = mEtilde; }
-
-    /**
-     *
-     * @return slepton doublet universal soft DRbar mass @f$\tilde{m}_{L}(\tilde{m}_{L})@f$
-     */
-    double getMLtilde() const { return mLtilde; }
-
-    /**
-     * @brief set slepton doublet universal soft DRbar mass @f$\tilde{m}_{L}(\tilde{m}_{L})@f$
-     * @param mLtilde slepton doublet universal soft DRbar mass @f$\tilde{m}_{L}(\tilde{m}_{L})@f$
-     */
-    void setMLtilde(double mLtilde) { this->mLtilde = mLtilde; }
-
-    /**
-     *
-     * @return right-handed sneutrino universal soft DRbar mass @f$\tilde{m}_{N}(\tilde{m}_{N})@f$
-     */
-    double getMNtilde() const { return mNtilde; }
-
-    /**
-     * @brief set right-handed sneutrino universal soft DRbar mass @f$\tilde{m}_{N}(\tilde{m}_{N})@f$
-     * @param mNtilde right-handed sneutrino universal soft DRbar mass @f$\tilde{m}_{N}(\tilde{m}_{N})@f$
-     */
-    void setMNtilde(double mNtilde) { this->mNtilde = mNtilde; }
-
-    /**
-     * 
-     * @return squark doublet universal soft DRbar mass @f$\tilde{m}_{Q}(\tilde{m}_{Q})@f$
-     */
-    double getMQtilde() const { return mQtilde; }
-
-    /**
-     * @brief set squark doublet universal soft DRbar mass @f$\tilde{m}_{Q}(\tilde{m}_{Q})@f$
-     * @param mQtilde squark doublet universal soft DRbar mass @f$\tilde{m}_{Q}(\tilde{m}_{Q})@f$
-     */
-    void setMQtilde(double mQtilde) { this->mQtilde = mQtilde; }
-
-    /**
-     *
-     * @return mUtilde_i right-handed up-type universal squark soft DRbar mass @f$\tilde{m}_{U}(\tilde{m}_{U})@f$
-     */
-    double getMUtilde() const { return mUtilde; }
-
-    /**
-     * @brief set mUtilde_i right-handed up-type universal squark soft DRbar mass @f$\tilde{m}_{U}(\tilde{m}_{U})@f$
-     * @param mUtilde mUtilde_i right-handed up-type universal squark soft DRbar mass @f$\tilde{m}_{U}(\tilde{m}_{U})@f$
-     */
-    void setMUtilde(double mUtilde) { this->mUtilde = mUtilde; }
+    virtual void Update(const std::map<std::string, double>& DPars);
+    virtual bool Init(const std::map<std::string, double>& DPars);
+    virtual bool CheckParameters(const std::map<std::string, double>& DPars);
 
 private:
-    double mQtilde, mUtilde, mDtilde, Au, Ad, mLtilde, mEtilde, mNtilde, Ae, An;
-
+    void SetSoftTerms(void);
+    void SetParameter(const std::string, const double&);
+    double a1, a2, a3, a6, a7, x1, x2, y1, y3, y6, y7;
+    complex a4, a5, a8, y2, y4, y5, w1, w2, w3, w4, w5;
+    Xmatrices X;
 };
 
 #endif	/* MFV_H */
---
+++ MFV/src/Xmatrices.h
@@ -0,0 +1,60 @@
+/* 
+ * File:   Xmatrices.h
+ * Author: silvest
+ *
+ * Created on March 26, 2012, 2:02 PM
+ */
+
+#ifndef XMATRICES_H
+#define	XMATRICES_H
+
+#include <gslpp_matrix_complex.h>
+#include <CKM.h>
+
+using namespace gslpp;
+
+class Xmatrices {
+public:
+    Xmatrices();
+
+    void Update(const CKM &);
+    
+    matrix<complex> GetX1() const {
+        return X1;
+    }
+
+    matrix<complex> GetX13() const {
+        return X13;
+    }
+
+    matrix<complex> GetX2() const {
+        return X2;
+    }
+
+    matrix<complex> GetX3() const {
+        return X3;
+    }
+
+    matrix<complex> GetX4() const {
+        return X4;
+    }
+
+    matrix<complex> GetX5() const {
+        return X5;
+    }
+
+    matrix<complex> GetX6() const {
+        return X6;
+    }
+
+    matrix<complex> GetX9() const {
+        return X9;
+    }
+    
+private:
+    matrix<complex> X1, X2, X3, X4, X5, X6, X9, X13;
+    double myrho, myeta, mylambda, myA;
+};
+
+#endif	/* XMATRICES_H */
+
