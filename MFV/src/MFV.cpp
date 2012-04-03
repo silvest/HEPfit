97dd3faa0a7e91d98cae10f972903565b309276d
silvest <silvest@2e41be99-0402-4e17-b588-3552aed5b166>
2012-04-03 14:36:33 +0000

    Added X matrices and methods to set the spectrum
    git-svn-id: file:///afs/infn.it/roma1/project/susy/susy/svnroot/trunk/SusyFit@298 2e41be99-0402-4e17-b588-3552aed5b166

--- MFV/src/MFV.cpp
+++ MFV/src/MFV.cpp
@@ -42,89 +42,133 @@
      * @param mnu3_i hevier neutrino mass
      */
 
-const std::string MFV::MFVvars[NMFVvars] = {"mQtilde", "mUtilde", "mDtilde", 
-        "Au", "Ad", "mLtilde", "mEtilde", "mNtilde", "Ae", "An", "m1", "m2", 
-        "m3", "muH", "tanb", "mHp" };
+const std::string MFV::MFVvars[NMFVvars] = {"a1", "a2", "a3", "a4r", "a4i", 
+        "a5r", "a5i", "a6", "a7", "a8r", "a8i", "x1", "x2", "y1", "y2r", "y2i", 
+        "y3", "y4r", "y4i", "y5r", "y5i", "y6", "y7", "w1r", "w1i", "w2r", 
+        "w2i", "w3r", "w3i", "w4r", "w4i", "w5r", "w5i"};
 
-MFV::MFV(const gslpp::matrix<gslpp::complex>& VCKM_i, double mu_i, double md_i,
-        double mc_i, double ms_i, double mt_i, double mb_i,
-        const gslpp::matrix<gslpp::complex>& UPMNS_i, double me_i,
-        double mmu_i, double mtau_i, double mnu1_i, double mnu2_i,
-        double mnu3_i, double tanb_i, gslpp::complex muH_i) :
-        SUSY(VCKM_i, mu_i, md_i, mc_i, ms_i, mt_i, mb_i, UPMNS_i, me_i, mmu_i,
-        mtau_i, mnu1_i, mnu2_i, mnu3_i, tanb_i, muH_i)
+MFV::MFV() :
+        SUSY(), X()
 {
 }
 
-MFV::MFV(const SUSY& SUSY_i) : SUSY(SUSY_i)
-{
-
+void MFV::Update(const std::map<std::string, double>& DPars) {
+    for (std::map<std::string, double>::const_iterator it = DPars.begin(); it != DPars.end(); it++)
+        SetParameter(it->first, it->second);
+    SetSoftTerms();
+    SetFeynHiggsPars();
+    CalcHiggsSpectrum();
+    CalcHiggsCouplings();
+    CalcHiggsProd(7.);//cross sections at 7 TeV
+    CalcConstraints();
+    CalcFlavour();
+    CalcSpectrum();
 }
 
-MFV::MFV(const MFV& orig) : SUSY(orig.getVCKM(), orig.getMu(),
-        orig.getMd(), orig.getMc(), orig.getMs(), orig.getMt(), orig.getMb(),
-        orig.getUPMNS(), orig.getMe(), orig.getMmu(), orig.getMtau(),
-        orig.getMnu1(), orig.getMnu2(), orig.getMnu3(), orig.getTanb(),
-        orig.getMuH())
-{
+void MFV::SetParameter(const std::string name, const double& value) {
+    if(name.compare("a1") == 0)
+        a1 = value;
+    else if(name.compare("a2") == 0)
+        a2 = value;
+    else if(name.compare("a3") == 0)
+        a3 = value;
+    else if(name.compare("a4r") == 0)
+        a4.real() = value;
+    else if(name.compare("a4i") == 0)
+        a4.imag() = value;
+    else if(name.compare("a5r") == 0)
+        a5.real() = value;
+    else if(name.compare("a5i") == 0)
+        a5.imag() = value;
+    else if(name.compare("a6") == 0)
+        a6 = value;
+    else if(name.compare("a7") == 0)
+        a7 = value;
+    else if(name.compare("a8r") == 0)
+        a8.real() = value;
+    else if(name.compare("a8i") == 0)
+        a8.imag() = value;
+    else if(name.compare("x1") == 0)
+        x1 = value;
+    else if(name.compare("x2") == 0)
+        x2 = value;
+    else if(name.compare("y1") == 0)
+        y1 = value;
+    else if(name.compare("y2r") == 0)
+        y2.real() = value;
+    else if(name.compare("y2i") == 0)
+        y2.imag() = value;
+    else if(name.compare("y3") == 0)
+        y3 = value;
+    else if(name.compare("y4r") == 0)
+        y4.real() = value;
+    else if(name.compare("y4i") == 0)
+        y4.imag() = value;
+    else if(name.compare("y5r") == 0)
+        y5.real() = value;
+    else if(name.compare("y5i") == 0)
+        y5.imag() = value;
+    else if(name.compare("y6") == 0)
+        y6 = value;
+    else if(name.compare("y7") == 0)
+        y7 = value;
+    else if(name.compare("w1r") == 0)
+        w1.real() = value;
+    else if(name.compare("w1i") == 0)
+        w1.imag() = value;
+    else if(name.compare("w2r") == 0)
+        w2.real() = value;
+    else if(name.compare("w2i") == 0)
+        w2.imag() = value;
+    else if(name.compare("w3r") == 0)
+        w3.real() = value;
+    else if(name.compare("w3i") == 0)
+        w3.imag() = value;
+    else if(name.compare("w4r") == 0)
+        w4.real() = value;
+    else if(name.compare("w4i") == 0)
+        w4.imag() = value;
+    else if(name.compare("w5r") == 0)
+        w5.real() = value;
+    else if(name.compare("w5i") == 0)
+        w5.imag() = value;
+    else
+        SUSY::SetParameter(name, value);
 }
 
-MFV::~MFV() {
+bool MFV::CheckParameters(const std::map<std::string, double>& DPars) {
+    for (int i = 0; i < NMFVvars; i++) {
+        if (DPars.find(MFVvars[i]) == DPars.end()) {
+            std::cout << "missing mandatory MFV parameter " << MFVvars[i] << std::endl;
+            return false;
+        }
+    }
+    return(SUSY::CheckParameters(DPars));
 }
 
-void MFV::setParameters(double a1, double a2, double a3, gslpp::complex a4,
-        gslpp::complex a5, double b1, double b2, gslpp::complex b3, double b5,
-        double b6, gslpp::complex b7, gslpp::complex b8, double c4,
-        gslpp::complex c7, gslpp::complex c10, gslpp::complex c11) {
+bool MFV::Init(const std::map<std::string, double>& DPars) {
+    Update(DPars);
+    return (CheckParameters(DPars));
+}
 
-    gslpp::matrix<gslpp::complex> mQ2a(3,3,0.), mU2a(3,3,0.), mD2a(3,3,0.),
-            Aua(3,3,0.), Ada(3,3,0.);
-    gslpp::matrix<gslpp::complex> mL2a(3,3,0.), mn2a(3,3,0.), mcha(3,3,0.),
-            Ana(3,3,0.), Acha(3,3,0.);
-    gslpp::matrix<gslpp::complex> mU2(6,6,0.), mD2(6,6,0.), Yu2(3,3,0.),
-            Yd2(3,3,0.);
-    gslpp::matrix<double> mqd(3,3,0.), mqd2(3,3,0), mqu(3,3,0.), mqu2(3,3,0),
-            I=gslpp::matrix<double>::Id(3);
-    double cos2b;
-   
-    Yu2 = Yu.hconjugate()*Yu;
-    Yd2 = Yd.hconjugate()*Yd;
-
-    // Eq. (15) of Colangelo et al., 0807.0801 reduced according to Eq. (21)
-    // Notice that Yukawa couplings are LR for Bertolini but RL for Colangelo
-
-    mQ2a = a1*I+b1*Yu2;//+b2*Yd2+(b3*Yd2*Yu2+b3.conjugate()*Yu2*Yd2);
-    mU2a = a2*I+b5*Yu*Yu.hconjugate();
-    mD2a = a3*I+Yd*(b6+c4*Yu2)*Yd.hconjugate();
-    Aua = Yu*(a4*I+c7*Yu2+b7*Yd2);
-    Ada = Yd*(a5*I+b8*Yu2+c10*Yd2+c11*Yd2*Yu2);
-    mqd(0,0) = md;
-    mqd(1,1) = ms;
-    mqd(2,2) = mb;
-    mqd2=mqd*mqd;
-    mqu(0,0) = mu;
-    mqu(1,1) = mc;
-    mqu(2,2) = mt;
-    mqu2=mqu*mqu;
-    cos2b = (1.-tanb*tanb)/(1.+tanb*tanb);
-
-    //SUPER-CKM Basis
-
-    mD2.assign(0, 0, mQ2a+mqd2+I*(Mz*Mz*cos2b*(-0.5+1./3.*sin2tw())));
-    mD2.assign(0, 3, v1()/sqrt(2.)*Ada.hconjugate()-muH*mqd*tanb);
-    mD2.assign(3, 0, v1()/sqrt(2.)*Ada-muH.conjugate()*mqd*tanb);
-    mD2.assign(3, 3, mD2a+mqd2+I*(Mz*Mz*cos2b*(-1./3.*sin2tw())));
-
-    mD2.eigensystem(Rd, Msd2);
-    
-    mU2.assign(0, 0, VCKM*mQ2a*VCKM.hconjugate()+mqu2+I*
-    (Mz*Mz*cos2b*(0.5-2./3.*sin2tw())));
-    mU2.assign(0, 3, v2()/sqrt(2.)*VCKM*Aua.hconjugate()-muH*mqu/tanb);
-    mU2.assign(3, 0, v2()/sqrt(2.)*Aua*VCKM.hconjugate()-
-    muH.conjugate()*mqu/tanb);
-    mU2.assign(3, 3, mU2a+mqu2+I*(Mz*Mz*cos2b*(2./3.*sin2tw())));
-
-    std::cout<< mU2 << std::endl;
-
-    mU2.eigensystem(Ru, Msu2);
+void MFV::SetSoftTerms(void){
+    // Colangelo's expression in Colangelo's basis
+    X.Update(myCKM);
+    MsQ2 = matrix<complex>::Id(3) * a1 + X.GetX13() * x1 + X.GetX1() * y1 + 
+            X.GetX5() * y2 + X.GetX9() * y2.conjugate();
+    MsU2 = matrix<complex>::Id(3) * a2 + X.GetX1() * x2;
+    MsD2 = matrix<complex>::Id(3) * a3 + X.GetX1() * y3 + X.GetX3() * w1 + 
+            X.GetX4() * w1.conjugate();
+    TU = (X.GetX5() * a4 + X.GetX1() * y4 + 
+            X.GetX6() * w2).transpose();
+    TD = (X.GetX1() * a5 + X.GetX5() * y5 + 
+            X.GetX2() * w3 + X.GetX4() * w4).transpose();
+    MsL2 = matrix<complex>::Id(3) * a6 + X.GetX1() * y6;
+    MsE2 = matrix<complex>::Id(3) * a7 + X.GetX1() * y7;
+    TE = (X.GetX1() * a8 + X.GetX2() * w5).transpose();
+    //rotation to the SCKM basis according to Satoshi's notes
+    matrix<complex> ckm(3,3,0.);
+    myCKM.getCKM(ckm);
+    MsQ2 = ckm*MsQ2*ckm.transpose();
+    TU = ckm.hconjugate()*TU;
 }
