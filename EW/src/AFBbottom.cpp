25a20c0a86cf29410300e56f07f7b7ae3d7a61e7
mishima <mishima@2e41be99-0402-4e17-b588-3552aed5b166>
2012-05-17 15:12:45 +0000

    slight modifications in EW class in order to use S, T and U.
    git-svn-id: file:///afs/infn.it/roma1/project/susy/susy/svnroot/trunk/SusyFit@317 2e41be99-0402-4e17-b588-3552aed5b166

--- EW/src/AFBbottom.cpp
+++ EW/src/AFBbottom.cpp
@@ -7,18 +7,20 @@
 
 
 AFBbottom::AFBbottom(const EW& EW_i) : ThObservable(EW_i) {
+    double AFB_b_TMP = 3.0/4.0*EW_i.A_l(SM.ELECTRON)*EW_i.A_q(SM.BOTTOM);
+    
     std::string Model = EW_i.getSM().ModelName();
     if (Model=="StandardModel" || Model=="SUSY")
-        AFB_b = 3.0/4.0*EW_i.A_l(SM.ELECTRON)*EW_i.A_q(SM.BOTTOM);
+        AFB_b = AFB_b_TMP;
     else if (Model=="THDM") {
         double S = EW_i.getSM().obliqueS();
         double T = EW_i.getSM().obliqueT();        
         double U = EW_i.getSM().obliqueU();
         double AFB_b_SM = 0.0;
         // Write codes!
-        AFB_b = AFB_b_SM;
+        //AFB_b = AFB_b_TMP + ....;
     } else 
-        throw "Error";
+        throw "Error in AFBbottom::AFBbottom()";
 }
 
 double AFBbottom::getThValue() {   
