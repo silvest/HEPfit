3473d5e92e975eb448f8e219e13f5698791a74a3
mishima <mishima@2e41be99-0402-4e17-b588-3552aed5b166>
2012-06-08 05:03:56 +0000

    QCD::Lambda() has been modified.
    git-svn-id: file:///afs/infn.it/roma1/project/susy/susy/svnroot/trunk/SusyFit@348 2e41be99-0402-4e17-b588-3552aed5b166

--- StandardModel/src/QCD.cpp
+++ StandardModel/src/QCD.cpp
@@ -445,9 +445,10 @@ double QCD::Lambda(double muMatching, double mf, double nfNEW, double nfORG,
             break;
         case FULLNLO:
             log_mu2_mf2 = log(muMatching*muMatching/mf/mf);
-            C1 = 2./3.*log_mu2_mf2;
-            if (nfNEW > nfORG)
-                C1 *= - 1.; 
+            if (nfNEW < nfORG)
+                C1 = 2./3.*log_mu2_mf2;
+            else
+                C1 = -2./3.*log_mu2_mf2;
             
             LogLambda = 1./2./Beta0(nfNEW)
                         *( (Beta0(nfNEW) - Beta0(nfORG))*Lf 
@@ -458,11 +459,12 @@ double QCD::Lambda(double muMatching, double mf, double nfNEW, double nfORG,
         case FULLNNLO:
             log_mu2_mf2 = log(muMatching*muMatching/mf/mf);
             log_Lf = log(Lf);
-            C1 = 2./3.*log_mu2_mf2;
-            C2 = - 16.*(log_mu2_mf2*log_mu2_mf2/36. - 19./24.*log_mu2_mf2 + 11./72.);                
-            if (nfNEW > nfORG) {
-                C1 *= - 1.; 
-                C2 *= -16.*(log_mu2_mf2*log_mu2_mf2/36. + 19./24.*log_mu2_mf2 - 11./72.);
+            if (nfNEW < nfORG) {
+                C1 = 2./3.*log_mu2_mf2;
+                C2 = -16.*(log_mu2_mf2*log_mu2_mf2/36. - 19./24.*log_mu2_mf2 + 11./72.);                
+            } else {
+                C1 = -2./3.*log_mu2_mf2;
+                C2 = -16.*(log_mu2_mf2*log_mu2_mf2/36. + 19./24.*log_mu2_mf2 - 11./72.);
             }
             
             LogLambda = 1./2./Beta0(nfNEW)
