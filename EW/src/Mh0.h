a3971401bdaead1f0057ed0f01700a66bd6b1b09
girardi_mac <girardi_mac@2e41be99-0402-4e17-b588-3552aed5b166>
2012-08-16 13:09:27 +0000

    The light higgs mass which is  a Standard Model parameter is updated after higgs mass computation.
    git-svn-id: file:///afs/infn.it/roma1/project/susy/susy/svnroot/trunk/SusyFit@366 2e41be99-0402-4e17-b588-3552aed5b166

---
+++ EW/src/Mh0.h
@@ -0,0 +1,25 @@
+/* 
+ * File:   Mh0.h
+ * Author: girardi_mac
+ *
+ * Created on 16 agosto 2012, 14.24
+ */
+
+#ifndef MH0_H
+#define	MH0_H
+
+#include <ThObservable.h>
+#include "EW.h"
+
+class Mh0 : public ThObservable {
+public: 
+    Mh0(const EW& EW_i) : ThObservable(EW_i){ };
+    double getThValue(){
+        return SM.GetMHl();
+    }
+private:
+
+};
+
+#endif	/* MH0_H */
+
