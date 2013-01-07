48e3f8cfd45c5f91b282540d038b3d7c5c597ff4
SatoshiMishima <Satoshi.Mishima@roma1.infn.it>
2013-01-07 10:37:35 +0100

    Initial commit of Macros for drawing histograms

---
+++ Macros/main.cpp
@@ -0,0 +1,406 @@
+/* 
+ * Copyright (C) 2013 SusyFit Collaboration
+ * All rights reserved.
+ *
+ * For the licensing terms see doc/COPYING.
+ */
+
+#include <cstdlib>
+#include <iomanip>
+#include <iostream>
+#include <fstream>
+#include <streambuf>
+#include <TApplication.h>
+#include <TCanvas.h>
+#include <TFile.h>
+#include <TString.h>
+#include <TObject.h>
+#include "BaseMacros.h"
+#include "SFH1D.h"
+#include "Pull.h"
+#include "SFH2D.h"
+
+using namespace std;
+
+/**
+ *  @addtogroup Macros
+ *  A project for drawing histograms and outputting results. 
+ *  @{
+ */
+
+//-- set probability ranges --
+//const double prob68 = 0.6827;
+//const double prob95 = 0.9545;
+const double prob68 = 0.68;
+const double prob95 = 0.95;
+
+/**
+ * @brief 
+ * @author SusyFit Collaboration
+ * @copyright GNU General Public License
+ * @details 
+ */
+int main(int argc, char** argv) 
+{
+    
+    if (argc < 3) {
+        cout << "#################################################################" << endl;
+        cout << " Usage:                                                          " << endl;
+        cout << "   1-D histogram:                                                " << endl;
+        cout << "     > " << argv[0] << " rootfile plotname --oneDim <optional parameters>" << endl;
+        cout << "   Compatibility plot:                                           " << endl;
+        cout << "     > " << argv[0] << " rootfile plotname --compat <optional parameters>" << endl;
+        cout << "   2-D histogram:                                                " << endl;
+        cout << "     > " << argv[0] << " rootfile plotname --twoDim <optional parameters>" << endl;
+        cout << "       'rootfile' (with extension)                               " << endl;
+        cout << "       'plotname' (e.g. sin2b)                                   " << endl;
+        cout << "                                                                 " << endl;
+        cout << "   Check the contents of a rootfile:                             " << endl;
+        cout << "     > " << argv[0] << " rootfile info                           " << endl;
+        cout << "                                                                 " << endl;
+        cout << " Optional parameters for 1-D histograms:                         " << endl;
+        cout << "   --oneDim         -> 1-D histogram (mandatory)                 " << endl;
+        cout << "   --orig           -> superimpose the original histogram        " << endl;
+        cout << "   --outputTxt      -> output results to a text file             " << endl;
+        cout << "   -output=filename -> the base name of output eps and text files (without extension)" << endl;
+        cout << "                       (default: plotname)                       " << endl;
+        cout << "   -maxDigits=n     -> set max digits in the axis labels         " << endl;
+        cout << "                       (default: n=8)                            " << endl;
+        cout << "   -precision=n     -> precision of values in the std output     " << endl;
+        cout << "                       (default: n=6)                            " << endl;
+        cout << "   -xlab=namex      -> x label                                   " << endl;
+        cout << "   -ylab=namey      -> y label                                   " << endl;
+        cout << "   -smooth=ntime    -> iterative smoothing with TH1::Smooth()    " << endl;
+        cout << "                       (default: ntime=0)                        " << endl;
+        cout << "   -moreBins=n      -> increase the number of bins               " << endl;
+        cout << "                       (default: n=100)                          " << endl;
+        cout << "   -col68=index     -> color index of the 68% interval           " << endl;
+        cout << "                       (default: index=1393)                     " << endl;
+        cout << "   -col95=index     -> color index of the 95% interval           " << endl;
+        cout << "                       (default: index=1392)                     " << endl;
+        cout << "                                                                 " << endl;
+        cout << " Optional parameters for compatibility plots:                    " << endl;
+        cout << "   --compat           -> Compatibility plot (mandatory)          " << endl;
+        cout << "   --outputTxt      -> output results to a text file             " << endl;
+        cout << "   -output=filename -> the base name of output eps and text files (without extension)" << endl;
+        cout << "                       (default: plotname)                       " << endl;
+        cout << "   -maxDigits=n     -> set max digits in the axis labels         " << endl;
+        cout << "                       (default: n=8)                            " << endl;
+        cout << "   -precision=n     -> precision of values in the std output     " << endl;
+        cout << "                       (default: n=6)                            " << endl;
+        cout << "   -xlab=namex      -> x label                                   " << endl;
+        cout << "   -ylab=namey      -> y label                                   " << endl;
+        cout << "   -val=xval        -> xval is the measured point to be superimposed" << endl;
+        cout << "   -err=xerr        -> xerr is the error of xval                 " << endl;
+        cout << "   -range=\"[xmin,xmax]x[ymin,ymax]\" -> define the graph range  " << endl;
+        cout << "   -bins=\"[xbins]x[ybins]\"          -> define the graph binning" << endl;
+        cout << "                                         (default: 100x20)       " << endl;
+        cout << "                                                                 " << endl;
+        cout << " Optional parameters for 2-D histograms:                         " << endl;
+        cout << "   --twoDim         -> 2-D histogram (mandatory)                 " << endl;
+        cout << "   --drawlines      -> draw contour lines                        " << endl;
+        cout << "   --outputTxt      -> output results to a text file             " << endl;
+        cout << "   -output=filename -> the base name of output eps and text files (without extension)" << endl;
+        cout << "                       (default: plotname)                       " << endl;
+        cout << "   -maxDigits=n     -> set max digits in the axis labels         " << endl;
+        cout << "                       (default: n=8)                            " << endl;
+        cout << "   -precision=n     -> precision of values in the std output     " << endl;
+        cout << "                       (default: n=6)                            " << endl;
+        cout << "   -xlab=namex      -> x label                                   " << endl;
+        cout << "   -ylab=namey      -> y label                                   " << endl;
+        cout << "   -smooth=ntime    -> iterative smoothing with TH2::Smooth()    " << endl;
+        cout << "                       (default: ntime=0)                        " << endl;
+        cout << "   -col68=index     -> color index of the 68% interval           " << endl;
+        cout << "                       (default: index=1393)                     " << endl;
+        cout << "   -col95=index     -> color index of the 95% interval           " << endl;
+        cout << "                       (default: index=1392)                     " << endl;
+        cout << "   -plot2=name      -> plot to be superimposed                   " << endl;
+        cout << "   -rootfile2=filename -> rootfile name for plot2 (with extension)" << endl;
+        cout << "                          (default: same as rootfile)            " << endl;
+        cout << "   -smooth2=ntime   -> iterative smoothing for plot2             " << endl;
+        cout << "                       (default: ntime=0)                        " << endl;
+        cout << "   -col682=index    -> color index of the 68% interval for plot2 " << endl;
+        cout << "                       (default: index=2)                        " << endl;
+        cout << "   -col952=index    -> color index of the 95% interval for plot2 " << endl;
+        cout << "                       (default: index=5)                        " << endl;
+        cout << "   -Xval=xval2      -> xval2 is the  point to be superimposed    " << endl;
+        cout << "   -Xerr=xerr2      -> xerr2 is the error of xval2               " << endl;
+        cout << "   -Yval=yval2      -> yval2 is the  point to be superimposed    " << endl;
+        cout << "   -Yerr=yerr2      -> yerr2 is the error of yval2               " << endl;
+        cout << "#################################################################" << endl;
+        return 0;
+    }
+
+    // parameters which can be changed by command-line arguments
+    bool bOneDim = false, bCompat = false, bTwoDim = false;
+    bool bOrig = false, bOutputTxt = false, bMoreBins = false;
+    bool bContLines = false, bSuperImpose = false;
+    int maxDig = 8, prec = 6, smooth = 0, newNbins = 100, col68 = 1393, col95 = 1392;
+    int nx = 100, ny = 20, smooth2 = 0, col682 = 2, col952 = 5;
+    double xval = -999.0, xerr = 0.0, x_low = 0.0, x_up = 0.0, y_low = 0.0, y_up = 0.0;
+    double xval2 = -999.0, xerr2 = 0.0, yval2 = -999.0, yerr2 = 0.0;
+    string plotname2, filename2;
+    TString xlab = "", ylab = "";
+
+    // root file
+    string filename = argv[1];
+    TFile *datafile = new TFile(filename.c_str());
+    if (argc == 3 && strncmp(argv[2], "info", 4) == 0) {
+        datafile->ls();
+        return 0;
+    }
+        
+    // plot name
+    string plotname = argv[2];
+    TObject* tobj;
+    tobj = datafile->Get(plotname.c_str());
+    if (tobj == NULL) {
+        cout << "Error: plot \"" << plotname << "\" does not exist..." << endl;
+        return 1;
+    }
+    
+    // options
+    string outputFileName = plotname;
+    filename2 = filename;
+    for (int i = 3; i < argc; i++) {
+        char str[100];
+        if (strncmp(argv[i], "--oneDim", 8) == 0) bOneDim = true;
+        if (strncmp(argv[i], "--compat", 8) == 0) bCompat = true;
+        if (strncmp(argv[i], "--twoDim", 8) == 0) bTwoDim = true;    
+        if (strncmp(argv[i], "--orig", 6) == 0) bOrig = true;
+        if (strncmp(argv[i], "--outputTxt", 11) == 0) bOutputTxt = true;        
+        if (strncmp(argv[i], "--drawlines", 11) == 0) bContLines = true;        
+        
+        if (strncmp(argv[i], "-output", 7) == 0) {
+            sscanf(argv[i], "-output=%s", str);
+            outputFileName = str;
+        }
+
+        if (strncmp(argv[i], "-xlab", 5) == 0) {
+            sscanf(argv[i], "-xlab=%s", str);
+            xlab = str;
+        }
+        
+        if (strncmp(argv[i], "-ylab", 5) == 0) {
+            sscanf(argv[i], "-ylab=%s", str);
+            ylab = str;
+        }        
+        
+        if (strncmp(argv[i], "-maxDigits", 10) == 0) 
+            sscanf(argv[i], "-maxDigits=%d", &maxDig);
+
+        if (strncmp(argv[i], "-precision", 10) == 0) 
+            sscanf(argv[i], "-precision=%d", &prec);
+
+        if (strncmp(argv[i], "-smooth", 7) == 0) 
+            sscanf(argv[i], "-smooth=%d", &smooth);
+
+        if (strncmp(argv[i], "-moreBins", 9) == 0) {
+            sscanf(argv[i], "-moreBins=%d", &newNbins);
+            bMoreBins = true;
+        }
+        
+        if (strncmp(argv[i], "-col68", 6) == 0) 
+            sscanf(argv[i], "-col68=%d", &col68);
+        
+        if (strncmp(argv[i], "-col95", 6) == 0) 
+            sscanf(argv[i], "-col95=%d", &col95);
+        
+        if (strncmp(argv[i], "-val", 4) == 0) 
+            sscanf(argv[i], "-val=%lf", &xval);
+
+        if (strncmp(argv[i], "-err", 4) == 0) 
+            sscanf(argv[i], "-err=%lf", &xerr);
+
+        if (strncmp(argv[i], "-range", 6) == 0) {
+            TString stmp(argv[i] + 7);
+            sscanf(stmp.Data(), "[%lf,%lf]x[%lf,%lf]", &x_low, &x_up, &y_low, &y_up);
+        }
+
+        if (strncmp(argv[i], "-bins", 5) == 0) {
+            TString stmp(argv[i] + 6);
+            sscanf(stmp.Data(), "[%d]x[%d]", &nx, &ny);
+        }
+        
+        // second plot
+        if (strncmp(argv[i], "-plot2", 6) == 0) {
+            sscanf(argv[i], "-plot2=%s", str);
+            plotname2 = str;
+            bSuperImpose = true;
+        }
+
+        // root file for the second plot
+        if (strncmp(argv[i], "-rootfile2", 10) == 0) {
+            sscanf(argv[i], "-rootfile2=%s", str);
+            filename2 = str;
+        }        
+        
+        if (strncmp(argv[i], "-smooth2", 8) == 0) 
+            sscanf(argv[i], "-smooth2=%d", &smooth2);
+
+        if (strncmp(argv[i], "-col682", 7) == 0) 
+            sscanf(argv[i], "-col682=%d", &col682);
+        
+        if (strncmp(argv[i], "-col952", 7) == 0) 
+            sscanf(argv[i], "-col952=%d", &col952);
+        
+        if (strncmp(argv[i], "-Xval", 5) == 0) 
+            sscanf(argv[i], "-Xval=%lf", &xval2);
+
+        if (strncmp(argv[i], "-Xerr", 5) == 0) 
+            sscanf(argv[i], "-Xerr=%lf", &xerr2);
+
+        if (strncmp(argv[i], "-Yval", 5) == 0) 
+            sscanf(argv[i], "-Yval=%lf", &yval2);
+
+        if (strncmp(argv[i], "-Yerr", 5) == 0) 
+            sscanf(argv[i], "-Yerr=%lf", &yerr2);
+    }        
+
+    if (!(bOneDim | bCompat | bTwoDim)) {
+        cout << "Error: A mandatory option --oneDim, --compat or --twoDim is missing!" << endl;
+        return 1;
+    }
+    if ( (bOneDim&bCompat) | (bOneDim&bTwoDim) | (bTwoDim&bCompat) ) {
+        cout << "Error: Use only one of --oneDim, --compat and --twoDim!" << endl;
+        return 1;
+    }
+
+    string epsFileName = outputFileName + ".eps";
+    string txtFileName = outputFileName + ".txt";
+
+    // second plot
+    TFile *datafile2 = new TFile(filename2.c_str());
+    TObject* tobj2;
+    tobj2 = datafile2->Get(plotname2.c_str());
+    if (tobj2 == NULL && bSuperImpose) {
+        cout << "Error: plot \"" << plotname2 << "\" does not exist..." << endl;
+        return 1;
+    }
+    
+    //----------------------------------------------------------------------
+
+    ofstream* fout;
+    streambuf* buf;
+    if (bOutputTxt) {
+        fout = new ofstream();
+        fout->open(txtFileName.c_str(), ios::out);
+        if (!fout->is_open())
+            cout << "cannot open " << txtFileName << endl;
+        buf = fout->rdbuf();        
+    } else
+        buf = cout.rdbuf();
+    ostream os(buf);
+    os.precision(prec);
+
+    BaseMacros::DefineNewColours();
+    
+    TCanvas TC("TC", "2D histogram", 3);
+    TC.SetLeftMargin(0.20);
+    // TC.SetRightMargin(0.20);
+    TC.SetBottomMargin(0.15);
+    TC.SetFillColor(0);
+    TC.SetBorderMode(0);
+    TC.SetFrameFillColor(0);
+    TC.SetHighLightColor(0);
+
+    gStyle->SetOptStat(0);
+    gStyle->SetOptTitle(0);
+    //gStyle->SetStripDecimals(false);   
+                
+    if (bOneDim) {
+        // 1-D histogram
+        TH1D* hist = (TH1D*) tobj->Clone();   
+        os << hist->GetXaxis()->GetTitle() << " in " << plotname << endl;
+
+        // output the 1-D histogram
+        SFH1D SFHisto1D(*hist, prob68, prob95);
+        SFHisto1D.smoothHist(smooth);
+        SFHisto1D.increaseNbins(newNbins);
+        SFHisto1D.Draw(xlab, ylab, col68, col95, maxDig, bOrig);    
+    
+        // output results
+        //   Note: after Draw(), use Integral("width"). 
+        os << "  Num of bins: " << SFHisto1D.getNewHist()->GetNbinsX() 
+           << "   smooth: " << smooth << " time(s)" << endl
+           << "  Local mode: " << SFHisto1D.getLocalMode()
+           << " + " << SFHisto1D.getXmax68() - SFHisto1D.getLocalMode() 
+           << " - " << SFHisto1D.getLocalMode() - SFHisto1D.getXmin68() << endl
+           << "  Center of " << SFHisto1D.getNewHist68()->Integral("width")*100.0 
+           << "% interval: " << (SFHisto1D.getXmin68() + SFHisto1D.getXmax68())/2.0
+           << " +- " << (SFHisto1D.getXmax68() - SFHisto1D.getXmin68())/2.0 << endl
+           << "  at " << SFHisto1D.getNewHist68()->Integral("width")*100.0 << " % (>~"
+           << prob68*100.0 << "%)"
+           << " [" << SFHisto1D.getXmin68() << ", " << SFHisto1D.getXmax68() << "]" 
+           << endl
+           << "  at " << SFHisto1D.getNewHist95()->Integral("width")*100.0 << " % (>~"
+           << prob95*100.0 << "%)"
+           << " [" << SFHisto1D.getXmin95() << ", " << SFHisto1D.getXmax95() << "]" 
+           << endl;        
+        SFH1D orig1D(*hist, prob68, prob95);
+        os << endl << "[Original histogram]" << endl
+           << "  Num of bins: " << orig1D.getNewHist()->GetNbinsX() 
+           << "   smooth: 0 time" << endl
+           << "  Local mode: " << orig1D.getLocalMode()
+           << " + " << orig1D.getXmax68() - orig1D.getLocalMode() 
+           << " - " << orig1D.getLocalMode() - orig1D.getXmin68() << endl
+           << "  Center of " << orig1D.getNewHist68()->Integral("")*100.0 
+           << "% interval: " << (orig1D.getXmin68() + orig1D.getXmax68())/2.0
+           << " +- " << (orig1D.getXmax68() - orig1D.getXmin68())/2.0 << endl
+           << "  at " << orig1D.getNewHist68()->Integral("")*100.0 << " % (>~"
+           << prob68*100.0 << "%)"
+           << " [" << orig1D.getXmin68() << ", " << orig1D.getXmax68() << "]" 
+           << endl
+           << "  at " << orig1D.getNewHist95()->Integral("")*100.0 << " % (>~"
+           << prob95*100.0 << "%)"
+           << " [" << orig1D.getXmin95() << ", " << orig1D.getXmax95() << "]" 
+           << endl;          
+    } else if (bCompat) {
+        // 1-D histogram
+        TH1D* hist = (TH1D*) tobj->Clone();
+        os << hist->GetXaxis()->GetTitle() << " in " << plotname << endl;
+        os << "  Num of bins: " << nx << " x " << ny << endl;
+        
+        // output the compatibility plot
+        Pull CompatPlot(*hist, nx, ny, x_low, x_up, y_low, y_up);
+        CompatPlot.Draw(xlab, ylab, xval, xerr, maxDig);
+        
+    } else if (bTwoDim) {    
+        // 2-D histogram
+        TH2D* hist = (TH2D*) tobj->Clone();
+        os << hist->GetXaxis()->GetTitle() << " vs " << hist->GetYaxis()->GetTitle() 
+           << " in " << plotname << endl;
+        os << "  smooth: " << smooth << " time(s)" << endl;
+        
+        // output the 2-D histogram
+        SFH2D SFHisto2D(*hist, os, prob68, prob95);
+        SFHisto2D.smoothHist(smooth);
+        SFHisto2D.draw(xlab, ylab, col68, col95, maxDig, 
+                       xval2, xerr2, yval2, yerr2, bContLines, false);
+
+        // superimpose the 2nd histogram
+        if (bSuperImpose) {
+            TH2D* hist2 = (TH2D*) tobj2->Clone();
+            SFH2D SFHisto2D2(*hist2, os, prob68, prob95);
+            SFHisto2D2.smoothHist(smooth2);
+            SFHisto2D2.draw(xlab, ylab, col682, col952, maxDig, 
+                            xval2, xerr2, yval2, yerr2, bContLines, true);
+        }
+    } 
+
+    gPad->RedrawAxis();
+    TC.Print(epsFileName.c_str());
+    
+    if (bOutputTxt) {
+        fout->close();          
+        cout << txtFileName << " has been created." << endl;
+    }
+
+    return 0;
+}
+
+/** 
+ * @}
+ */
+
+
---
+++ Macros/src/SFH1D.cpp
@@ -0,0 +1,211 @@
+/* 
+ * Copyright (C) 2013 SusyFit Collaboration
+ * All rights reserved.
+ *
+ * For the licensing terms see doc/COPYING.
+ */
+
+#include <iostream>
+#include <vector>
+#include <numeric>
+#include <TString.h>
+#include <TGaxis.h>
+#include <BAT/BCH1D.h>
+#include "SFH1D.h"
+
+SFH1D::SFH1D(TH1D& hist, const double prob68_in, const double prob95_in) 
+: prob68(prob68_in), prob95(prob95_in), origHist(hist), origName(hist.GetName())
+{
+    std::string NewName = origName + "_new";
+    newHist = (TH1D*) hist.Clone(NewName.c_str()); 
+    double sum = newHist->Integral();
+    newHist->Scale(1.0/sum);
+
+    newHist68 = (TH1D*) newHist->Clone("68"); 
+    newHist95 = (TH1D*) newHist->Clone("95");     
+    
+    computeIntervals();
+}
+
+
+void SFH1D::smoothHist(const int smooth)
+{
+    if (smooth!=0) { 
+        newHist->Smooth(smooth);
+        double sum = newHist->Integral();
+        newHist->Scale(1.0/sum);
+        computeIntervals();
+    }
+}
+
+
+void SFH1D::increaseNbins(const int newNbin)
+{
+    int nbin = newHist->GetNbinsX();
+    double xmin = newHist->GetXaxis()->GetXmin();
+    double xmax = newHist->GetXaxis()->GetXmax();
+    
+    if (newNbin > nbin) {
+        // compute new weights for a histogram having more bins
+        std::vector<double> NewEntries;
+        for (int i = 0; i < newNbin; i++) {    
+            double x = xmin + (xmax - xmin)/(double)newNbin*((double)i + 0.5);
+            
+            int index = newHist->FindBin(x);
+            if (index==1) index += 1; // the first bin
+            if (index==nbin) index -= 1; // the last bin
+            
+            double xx[3], yy[3];
+            for (int j=0; j<3; j++){
+                xx[j] = newHist->GetBinCenter(index + j);
+                yy[j] = newHist->GetBinContent(index + j);
+            }
+
+            double val = quadra(x, xx, yy);
+            if (val < 0.0) val = 0.0;
+            NewEntries.push_back(val);        
+        }
+        
+        double sum = std::accumulate(NewEntries.begin(), NewEntries.end(), 0.0);
+        
+        newHist->Reset();
+        newHist->SetBins(newNbin, xmin, xmax);
+        for (int n = 1; n <= newNbin; n++)
+            newHist->SetBinContent(n, NewEntries.at(n-1)/sum);
+
+        computeIntervals();
+    }
+}
+
+
+void SFH1D::Draw(const TString xlab, const TString ylab, 
+                 const int col68, const int col95, 
+                 const int maxDigits, const bool bOrigHist) 
+{
+    newHist->SetTitle("");
+    newHist->GetXaxis()->SetTitleSize(0.06);
+    newHist->GetYaxis()->SetTitleSize(0.06);
+    newHist->GetXaxis()->SetTitleOffset(1.1);
+    newHist->GetYaxis()->SetTitleOffset(1.5);
+    newHist->GetXaxis()->SetNdivisions(505);
+    newHist->GetYaxis()->SetNdivisions(505);
+    newHist->GetXaxis()->SetLabelOffset(0.013);
+    newHist->GetXaxis()->SetLabelSize(0.043);
+    newHist->GetYaxis()->SetLabelOffset(0.013);
+    newHist->GetYaxis()->SetLabelSize(0.043);
+    newHist->SetLabelFont(42,"X");
+    newHist->SetLabelFont(42,"Y");
+    newHist->SetTitleFont(42,"X");
+    newHist->SetTitleFont(42,"Y");
+    //newHist->SetLabelFont(62,"X");
+    //newHist->SetLabelFont(62,"Y");
+    //newHist->SetTitleFont(62,"X");
+    //newHist->SetTitleFont(62,"Y");
+    newHist->SetMinimum(0.0);
+    newHist->SetLineStyle(1);
+    ((TGaxis*) newHist->GetXaxis())->SetMaxDigits(maxDigits);
+    ((TGaxis*) newHist->GetYaxis())->SetMaxDigits(maxDigits);
+
+    // Titles of the axes 
+    if (xlab.CompareTo("")==0) {
+        TString Xtitle = ConvertTitle(newHist->GetXaxis()->GetTitle());
+        newHist->GetXaxis()->SetTitle(Xtitle);
+    } else
+        newHist->GetXaxis()->SetTitle(xlab);
+    if (ylab.CompareTo("")==0)
+        newHist->GetYaxis()->SetTitle("Probability density");
+    else 
+        newHist->GetYaxis()->SetTitle(ylab);
+    
+    // This is necessary to set the same styles and titles as newHist 
+    // to newHist68 and newHist95. 
+    computeIntervals();
+
+    // 68% and 95% intervals
+    newHist68->SetFillColor(col68);
+    newHist95->SetFillColor(col95);
+    newHist68->SetLineStyle(3);
+    newHist95->SetLineStyle(3);
+
+    // normalize the histograms
+    newHist->Scale(1.0/newHist->GetBinWidth(1));
+    newHist68->Scale(1.0/newHist68->GetBinWidth(1));
+    newHist95->Scale(1.0/newHist95->GetBinWidth(1));
+    
+    // draw the histograms
+    newHist95->Draw();
+    newHist68->Draw("SAME");
+    newHist->Draw("SAME");
+
+    // draw the original histogram
+    if (bOrigHist) {
+        origHist.SetLineColor(kRed);
+        origHist.SetLineWidth(2);
+        origHist.Draw("SAME");
+    }
+}
+
+
+//////////////////////////////////////////////////////////////////////// 
+
+double SFH1D::quadra(const double x, const double xx[3], const double yy[3]) const 
+{
+    double a = ( (yy[2]-yy[0])/(xx[2]-xx[0])*(xx[1]-xx[0]) - (yy[1]-yy[0]) )
+               /( (xx[2]*xx[2]-xx[0]*xx[0])/(xx[2]-xx[0])*(xx[1]-xx[0]) 
+                  - (xx[1]*xx[1]-xx[0]*xx[0]) );
+    double b = ( yy[1] - yy[0] - a*(xx[1]*xx[1]-xx[0]*xx[0]) )/(xx[1]-xx[0]);
+    double c = yy[0] - b*xx[0] - a*xx[0]*xx[0];
+    return a*x*x + b*x + c;
+}
+
+
+TH1D* SFH1D::HistInterval(double &min, double &max, const double level) const
+{
+    BCH1D *myBCH1D = new BCH1D(newHist);  
+    TH1D* tmpHist = myBCH1D->GetSmallestIntervalHistogram(level);
+    for (int n = 1; n <= tmpHist->GetNbinsX(); n++) {
+        if (tmpHist->GetBinContent(n)!=0.0) 
+            tmpHist->SetBinContent(n, newHist->GetBinContent(n));
+    }    
+    
+    // get the minimum of the interval
+    bool flag = false;
+    for (int n = 1; n <= tmpHist->GetNbinsX(); n++) {
+        if (tmpHist->GetBinContent(n)!=0.0 && !flag) {
+            min = tmpHist->GetBinLowEdge(n);
+            flag = true;
+        }
+    }
+
+    // get the maximum of the interval
+    flag = false;
+    for (int n = tmpHist->GetNbinsX(); n >= 1; n--) {
+        if (tmpHist->GetBinContent(n)!=0.0 && !flag) {
+            max = tmpHist->GetBinLowEdge(n) + tmpHist->GetBinWidth(1);
+            flag = true;
+        }
+    }
+    
+    return tmpHist;
+}
+
+
+void SFH1D::computeIntervals()
+{
+    double min, max;
+    if(newHist68!=NULL) delete newHist68;
+    newHist68 = HistInterval(min, max, prob68);
+    xmin68 = min;
+    xmax68 = max;
+    if(newHist95!=NULL) delete newHist95;
+    newHist95 = HistInterval(min, max, prob95);
+    xmin95 = min;
+    xmax95 = max;
+    
+    localMode = newHist->GetBinCenter(newHist->GetMaximumBin());
+}
+
+
+
+
+
