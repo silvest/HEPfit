486d372fcaea22b654e782939827d2ecf55feb1d
stefano <stefano@stefano>
2013-03-15 17:25:18 +0100

    mothod computing Epsilon'/epsilon parameter

---
+++ Flavour/src/EpsilonP_O_Epsilon.h
@@ -0,0 +1,36 @@
+/* 
+ * Copyright (C) 2012 SUSYfit Collaboration
+ * All rights reserved.
+ *
+ * For the licensing terms see doc/COPYING.
+ */
+
+#ifndef EPSILONP_O_EPSILON_H
+#define	EPSILONP_O_EPSILON_H
+
+#include <ThObservable.h>
+#include "Flavour.h"
+#include "AmpDS1.h"
+
+using namespace gslpp;
+
+class EpsilonP_O_Epsilon : public ThObservable, AmpDS1 {
+public:   
+    /**
+     * constructor
+     * @param Flavour
+     */
+    EpsilonP_O_Epsilon(Flavour& Flavour): ThObservable(Flavour), AmpDS1(Flavour) {};
+    
+    /**
+     * 
+     * @return theoretical value of |\f$ \epsilon ' / \epsilon \f$| 
+     */
+    double getThValue();
+    
+private:
+    double om;
+    
+};
+
+#endif	/* EPSILONP_O_EPSILON_H */
