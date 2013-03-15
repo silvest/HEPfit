af0e874b5414cece79e3460a4309b166467235f1
stefano <stefano@stefano>
2013-03-15 17:31:39 +0100

    adding of the epsilon'/epsilon observable

---
+++ Flavour/src/EpsilonP_O_Epsilon.cpp
@@ -0,0 +1,18 @@
+/* 
+ * Copyright (C) 2012 SUSYfit Collaboration
+ * All rights reserved.
+ *
+ * For the licensing terms see doc/COPYING.
+ */
+
+#include "EpsilonP_O_Epsilon.h"
+
+double EpsilonP_O_Epsilon::getThValue(){
+    
+    om = SM.getReA2_kd() / SM.getReA0_kd();
+    
+    double a = 0.707106781*(om/SM.getReA0_kd())*((1/om)*AmpDS1pp2(NLO).imag()-
+               (1-SM.getOmega_eta_etap())*AmpDS1pp0(NLO).imag());
+    
+    return a;
+}
