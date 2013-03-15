74ffdf72f008179d0eb3bb99d8c8af37217dd85b
stefano <stefano@stefano>
2013-03-15 17:20:13 +0100

    amplitude to compute DS=1 processes. Only the amplitude for the decay K_L in two pions is implemented in the *.cpp file

---
+++ Flavour/src/AmpDS1.h
@@ -0,0 +1,46 @@
+/* 
+ * Copyright (C) 2012 SUSYfit Collaboration
+ * All rights reserved.
+ *
+ * For the licensing terms see doc/COPYING.
+ */
+
+#ifndef AMPDS1_H
+#define	AMPDS1_H
+
+#include <gslpp_complex.h>
+#include "Flavour.h"
+
+using namespace gslpp;
+
+class AmpDS1 {
+public:
+    /**
+     * 
+     * @brief comupte the amplitude for K_L decay in 2 pion
+     * @param Flavour
+     */
+    AmpDS1(Flavour& Flavour);
+
+protected:
+    /**
+     * 
+     * @param order
+     * @return the amplitude for K_L decay in 2 pion with 0 isospin change
+     */
+    complex AmpDS1pp0(orders order);
+    
+    /**
+     * 
+     * @param order
+     * @return the amplitude for K_L decay in 2 pion with double isospin change
+     */
+    complex AmpDS1pp2(orders order);
+    
+private:
+    Flavour& myFlavour;
+
+};
+
+
+#endif	/* AMPDS1_H */
