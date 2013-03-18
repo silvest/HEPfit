3433c61d02b0bcaf3690dccece4606059fb1a4b5
stefano <stefano@stefano>
2013-03-18 15:16:14 +0100

    Matching contribution to rare decay added -K_L->P0nunu and K+->P+nunu at NLO in QCD (top contribution) and to loop EW correction (leading order in sin(theta_weak expansion)) -B(d,s)->X(d,s)nunu and B(d,s)->mumu at NLO in QCD

--- StandardModel/src/StandardModelMatching.cpp
+++ StandardModel/src/StandardModelMatching.cpp
@@ -10,6 +10,7 @@
 #include "QCD.h"
 #include <gsl/gsl_sf_dilog.h>
 #include <gsl/gsl_sf_zeta.h>
+#include <gsl/gsl_sf_clausen.h>
 #include <stdexcept>
 
 
@@ -20,8 +21,10 @@ StandardModelMatching::StandardModelMatching(const StandardModel & SM_i)
         mck(10, NDR, NLO), mckcc(10, NDR, NLO), 
         mcbsg(10, NDR, NLO), mcbnlep(10, NDR, NLO, NLO_ew), 
         mcbnlepCC(10, NDR, NLO), mcd1(10, NDR, NLO), 
-        mcd1Buras(10, NDR, NLO) 
-{   
+        mcd1Buras(10, NDR, NLO) , mckpnn(1, NDR, NLO, NLO_ew), mckppnn(1, NDR, NLO, NLO_ew),
+        mckmm(1, NDR, NLO), mcbsnn(1, NDR, NLO), mcbdnn(1, NDR, NLO), 
+        mcbsmm(1, NDR, NLO), mcbdmm(1, NDR, NLO) {   
+    
     swa = 0.;  swb = 0.; swc = 0.;
     xcachea = 0.; xcacheb = 0.; xcachec = 0.;
     
@@ -187,6 +190,204 @@ double StandardModelMatching::E0b(double x) const
 {
     return ( -2./3. *log(x) + (x*(18. -11.*x -x*x))/(12.*(-x*x*x +3.*x*x -3.*x +1.)) +
             (x*x*(15. -16.*x +4.*x*x))/(6.*pow(1.-x,4.)) *log(x) );
+}
+
+/******************************************************************************/
+/* loop functions for rare K and B decays, K-> pi nu nu & B-> Xs nu nu        */
+/******************************************************************************/
+
+double StandardModelMatching::X0t(double x) const{
+    return ( (x/8.)*( (x+2.)/(x-1.) - (6.-3.*x)/(1.-x)/(1.-x)*log(x) ) );
+}
+
+double StandardModelMatching::X1t(double x) const{
+    return ( 
+            -(29.*x -x*x -4.*x*x*x)/(3.*(1.-x)*(1.-x))
+            -log(x)*(x +9.*x*x -x*x*x -x*x*x*x)/((1.-x)*(1.-x)*(1.-x)) 
+            +log(x)*log(x)*(8.*x +4.*x*x +x*x*x -x*x*x*x)/(2.*(1.-x)*(1.-x)*(1.-x))
+            -gsl_sf_dilog(1.-x)*(4.*x -x*x*x)/((1.-x)*(1.-x))
+            +8.*x* (1/8.)*( (x+2.)/(x-1.) + (3.*x-6.)/(x-1.)/(x-1.)*log(x) )
+            *log(SM.getMut()*SM.getMut()/SM.getMuw()/SM.getMuw())
+            +8.*x* (x/8.)*( 1./(x-1.) -(x+2.)/(x*x -2.*x +1.) + (3.*x -6.)/(x*(x*x -2.*x +1.))
+            +3.*log(x)/(x*x -2.*x +1.) -2.*log(x)*(3.*x-6.)/(x-1.)/(x-1.)/(x-1.) ) );
+}
+
+double StandardModelMatching::Xewt(double x, double a, double mu) const{
+    double b = 0.;
+    
+    double swsq = (M_PI * SM.getAle() )/( sqrt(2) * SM.getGF() * SM.Mw_tree() * SM.Mw_tree());
+    
+    double A[16], C[16];
+    
+    A[0]=(16. - 48.*a)*M_PI*M_PI + (288.*a - (32. - 88.*a)*M_PI*M_PI )*x 
+        + (2003.*a + 4.*(4. - 6.*a - a*a )*M_PI*M_PI )*x*x
+        + (9.*a*(93. + 28.*a) - 4.*a*(3. - 2.*a + 8.*a*a )*M_PI*M_PI )*x*x*x
+        + (3.*a*(172. - 49.*a - 32.*a*a ) + 4.*a*(20. - a + 16.*a*a )*M_PI*M_PI )*x*x*x*x
+        - (3.*a*(168. + 11.*a - 24.*a*a ) + 4.*a*(45. + 8.*a*a )*M_PI*M_PI)*x*x*x*x*x
+        + 96.*a*M_PI*M_PI*x*x*x*x*x*x ;
+    
+    A[1]=- 768.*x - (525. - 867.*a)*x*x + (303. + 318.*a)*x*x*x - 195.*a*x*x*x*x;
+    
+    A[2]=- 8.*(95. - 67.*a + 11.*a*a )*x*x + 2.*(662. - 78.*a - 177.*a*a + 40.*a*a*a )*x*x*x
+        - (608. + 476.*a - 595.*a*a + 114.*a*a*a )*x*x*x*x 
+        + (44. + 188.*a - 321.*a*a + 103.*a*a*a - 8.*a*a*a*a )*x*x*x*x*x
+        - a*(28. - 72.*a + 33.*a*a - 4.*a*a*a )*x*x*x*x*x*x;
+    
+    A[3]=+ 48. - 10.*(57. + 4.*a)*x + 51.*(29. + 10.*a)*x*x - 
+        (841. + 1265.*a)*x*x*x + (308. + 347.*a)*x*x*x*x
+        - (28. - 40.*a )*x*x*x*x*x + 12.*a*x*x*x*x*x*x ;
+    
+    A[4]= + 768. + (816. - 768.*a)*x + (1240. - 1232.*a)*x*x 
+        - 4.*(415. + 2.*a)*x*x*x + (311. + 722.*a)*x*x*x*x
+        + (145. - 267.*a)*x*x*x*x*x - (36. + 51.*a)*x*x*x*x*x*x + 20.*a*x*x*x*x*x*x*x ;
+    
+    A[5]=+ 328.*x - (536. + 900.*a)*x*x + (208. + 1584.*a + 670.*a*a )*x*x*x 
+        - a*(668. + 1161.*a + 225.*a*a )*x*x*x*x
+        + a*a*(479. + 362.*a + 28.*a*a )*x*x*x*x*x 
+        - a*a*a*(143. + 42.*a)*x*x*x*x*x*x + 16.*a*a*a*a*x*x*x*x*x*x*x;
+    
+    A[6] =+ 32. - 4.*(44. - 9.*a)*x + (384. - 322.*a - 400.*a*a )*x*x 
+        - (400. - 869.*a - 1126.*a*a - 696.*a*a*a )*x*x*x
+        + 2.*(80. - 488.*a - 517.*a*a - 631.*a*a*a - 264.*a*a*a*a )*x*x*x*x
+        + (48. + 394.*a + 269.*a*a + 190.*a*a*a + 882.*a*a*a*a + 196.*a*a*a*a*a )*x*x*x*x*x
+        - (64. - 58.*a - 89.*a*a - 95.*a*a*a + 34.*a*a*a*a + 296.*a*a*a*a*a + 32.*a*a*a*a*a*a )*x*x*x*x*x*x
+        + (16. - 59.*a - 79.*a*a + 256.*a*a*a - 239.*a*a*a*a 
+        + 57.*a*a*a*a*a + 48.*a*a*a*a*a*a )*x*x*x*x*x*x*x
+        + (1. - a)*(1. - a)*(1. - a)*a*a*(29. + 16.*a)*x*x*x*x*x*x*x*x ;
+    
+    A[7]= + 28.*a*a*x*x - 32.*a*a*a*x*x*x;
+    
+    A[8]=- 288. + 36.*(1. + 8.*a)*x + 6.*(647. + 87.*a)*x*x + 5.*(55. - 927.*a - 132.*a*a )*x*x*x
+        - (1233. + 98.*a - 879.*a*a - 192.*a*a*a )*x*x*x*x 
+        + (360. + 1371.*a - 315.*a*a - 264.*a*a*a )*x*x*x*x*x
+        - 24.*a*(17. - 4.*a*a)*x*x*x*x*x*x;
+    
+    A[9]=+ 32. + 4.*(-44. + 29.*a)*x - 12.*(-32. + 77.*a + 31.*a*a )*x*x
+        + 2.*(-200. + 837.*a + 767.*a*a + 182.*a*a*a )*x*x*x 
+        - 2.*(-80. + 625.*a + 905.*a*a + 520.*a*a*a + 82.*a*a*a*a )*x*x*x*x
+        + (48. + 1079.*a + 590.*a*a + 1002.*a*a*a + 462.*a*a*a*a + 32.*a*a*a*a*a )*x*x*x*x*x
+        + (-64. - 1160.*a - 501.*a*a - 364.*a*a*a - 486.*a*a*a*a - 72.*a*a*a*a*a )*x*x*x*x*x*x
+        + (16. + 729.*a + 1038.*a*a + 38.*a*a*a + 238.*a*a*a*a + 52.*a*a*a*a*a )*x*x*x*x*x*x*x
+        - a*(192. + 743.*a + 50.*a*a*a + 12.*a*a*a*a )*x*x*x*x*x*x*x*x + 192.*a*a*x*x*x*x*x*x*x*x*x;
+    
+    A[10]=+ 16.*x + 324.*x*x - 36.*x*x*x*x;
+    
+    A[11]=+ 216.*x - 672.*x*x + 152.*x*x*x;
+    
+    A[12]=- 16.*x + (16. - 42.*a)*x*x + (16. + 21.*a + 60.*a*a )*x*x*x
+        - (16 - 21.*a + 45.*a*a + 32.*a*a*a )*x*x*x*x - a*a*(7. - 24.*a)*x*x*x*x*x; 
+    
+    A[13]=- 32. + (144. - 68.*a)*x + (-240. + 334.*a + 332.*a*a )*x*x 
+        + (160. - 551.*a - 660.*a*a - 364.*a*a*a )*x*x*x
+        + a*(329. + 451.*a + 650.*a*a + 164.*a*a*a )*x*x*x*x 
+        + (-48. - a - 59.*a*a - 523.*a*a*a - 316.*a*a*a*a - 32.*a*a*a*a*a )*x*x*x*x*x
+        + (16. - 43.*a - 93.*a*a + 255.*a*a*a + 287.*a*a*a*a + 32.*a*a*a*a*a )*x*x*x*x*x*x 
+        - a*a*(-29. + 42.*a + 103.*a*a + 8.*a*a*a )*x*x*x*x*x*x*x;
+
+    A[14]=- 144.*(1. - a)*(1. - a)*x*x + 144.*(1. - a)*(1. - a)*x*x*x - 36.*(1. - a)*(1. - a)*x*x*x*x;
+    
+    A[15]=- 32. + 96.*a + (48. - 32.*a)*x - 176.*a*x*x - (16. - 74.*a)*x*x*x + 212.*a*x*x*x*x;
+    
+    A[16]=- 32. + (64. - 100.*a)*x - 8.*(4. - 34.*a - 29.*a*a )*x*x 
+        - 4.*a*(34. + 170.*a + 33.*a*a )*x*x*x
+        + 8.*a*a*(47. + 51.*a + 4.*a*a)*x*x*x*x - 16.*a*a*a*(15. + 4.*a)*x*x*x*x*x 
+        + 32.*a*a*a*a*x*x*x*x*x*x;
+    
+    ////////////////////////////////////////////////////////////////////////////
+    
+    C[0] = 1./(3.*a*(x-1.)*(x-1.)*x);
+    
+    C[1] = phi1(0.25)/((x-1.)*(x-1.)*(x-1.)*(a*x-1.));
+    
+    C[2] = phi1(0.25*a)/(2.*(x-1.)*(x-1.)*(x-1.)*(a*x-1.));
+    
+    C[3] = phi1(1./4./x)/(2.*(x-1.)*(x-1.)*(x-1.)*(a*x-1.));
+    
+    C[4] = phi1(0.25*x)/(2.*(x-1.)*(x-1.)*(x-1.)*(a*x-1.));
+    
+    C[5] = phi1(a*x*0.25)/((x-1.)*(x-1.)*(x-1.)*(a*x-1.));
+    
+    C[6] = phi2(1/a/x, 1/a)/(2.*a*a*x*x*(x-1.)*(x-1.)*(x-1.)*(a*x-1.));
+    
+    C[7] = log(a)*log(a)/(a*x-1.);
+    
+    C[8] = log(x)/((x-1.)*(x-1.)*(x-1.)*(a*x-1.)*3.);
+    
+    C[9] = log(x)*log(x)/((x-1.)*(x-1.)*(x-1.)*(x-1.)*(a*x-1.)*2.*a*x);
+    
+    C[10] = 2.*log(mu/SM.Mw_tree())/((x-1.)*(x-1.));
+    
+    C[11] = log(x)*2.*log(mu/SM.Mw_tree())/((x-1.)*(x-1.)*(x-1.));
+    
+    C[12] = log(a)/((x-1.)*(x-1.)*(a*x-1.));
+    
+    C[13] = log(x)*log(a)/(2.*a*(x-1.)*(x-1.)*(x-1.)*x*(a*x-1.));
+    
+    C[14] = gsl_sf_dilog(1.-a)/(x-1.)/(x-1.);
+    
+    C[15] = gsl_sf_dilog(1.-x)/a*x;
+    
+    C[16] = gsl_sf_dilog(1.-a*x)/(a*x*(x-1.)*(x-1.));
+    
+    for (int i=0; i<10; i++){
+        b += C[i]*A[i];
+    }
+    
+    return (b/128./swsq);
+}
+
+double StandardModelMatching::phi1(double z) const{
+    if (z >= 0.) {
+        if (z < 1){
+                return(4.*sqrt(z/(1.-z))*gsl_sf_clausen(2.*asin(sqrt(z))));
+        }
+        else{
+            return((1./sqrt(1.-1./z))*(2.*log(0.5*sqrt(1.-1./z))*log(0.5*sqrt(1.-1./z))-
+                    4.*gsl_sf_dilog(0.5*(1.-sqrt(z/(1.-z)))) - log(4.*z)*log(4.*z)
+                    + M_PI*M_PI/3.));
+        }
+    }
+    else{
+        std::stringstream out;
+        out << z;
+        throw std::runtime_error("StandardModelMatching::phi1(double z)" + out.str() + " <0");
+    }
+    return(0.);
+}
+
+double StandardModelMatching::phi2(double x, double y) const{
+    double l = sqrt((1.-x-y)*(1.-x-y)-4.*x*y);
+    
+    if ((l*l) >= 0. || (sqrt(x) + sqrt(y)) <= 1.){
+        return( 1./l*(M_PI*M_PI/3. +2.*log(0.5*(1.+x-y-l))*log(0.5*(1.-x+y-l)) -log(x)*log(y)
+                -2.*gsl_sf_dilog(0.5*(1.+x-y-l)) -2.*gsl_sf_dilog(0.5*(1.-x+y-l))));        
+    }
+    else if((l*l) < 0. || (sqrt(x) + sqrt(y)) > 1.){
+        return(2./(-l*l)*(gsl_sf_clausen(2.*acos((-1.+x+y)/(2.*sqrt(x*y))))
+                +gsl_sf_clausen(2.*acos((1.+x-y)/(2.*sqrt(x))))
+                +gsl_sf_clausen(2.*acos((1.-x+y)/(2.*sqrt(y))))));
+    }
+    else{
+        std::stringstream out;
+        out << x;
+        throw std::runtime_error("StandardModelMatching::phi2(double x, double y) wrong" + out.str());
+    }
+    return(0.);
+}
+
+double StandardModelMatching::Y0t(double x) const{
+    return( x/8.*((4.-x)/(1.-x) + 3.*x*log(x)*(1.-x)*(1.-x)) );
+}
+
+double StandardModelMatching::Y1t(double x) const{
+    return( (4.*x+16.*x*x+4.*x*x*x)/(3.*(1.-x)*(1.-x))
+           -(4.*x-10.*x*x+x*x*x-x*x*x*x)*log(x)/(1.-x)/(1.-x)/(1.-x)
+           +(2.*x-14.*x*x+x*x*x-x*x*x*x)*log(x)*log(x)/2./(1.-x)/(1.-x)
+           +(2.*x+x*x*x)/(1.-x)/(1.-x)*gsl_sf_dilog(1.-x) 
+           +x*((4.-x)/(1.-x) + 3.*x*log(x)*(1.-x)*(1.-x))*0.//log(mu^2/Muw^2)
+           +x*x*(-1./(1.-x) + (4.-x)/(1.-x)/(1.-x) + 3.*log(x)/(1.-x)/(1.-x)
+                 +6.*x*log(x)/(1.-x)/(1.-x)/(1.-x) +3./(1.-x)/(1.-x))*0.////log(mu^2/Muw^2)
+            );
 }
 
 /******************************************************************************/
@@ -791,6 +992,237 @@ const std::vector<WilsonCoefficient>& StandardModelMatching::CMbnlepCC(const int
     vmcbnlepCC.push_back(mcbnlepCC);
     return(vmcbnlepCC);
 }
+
+const std::vector<WilsonCoefficient>& StandardModelMatching::CMkp0nn() {
+    
+    //PROBLEMI: mu e sin(theta_weak) e la scala di als
+    
+    double xt = pow(SM.Mrun(SM.getMuw(), SM.getQuarks(QCD::TOP).getMass(), SM.getQuarks(QCD::TOP).getMass(), 5.)
+            / SM.Mw_tree(), 2.); // always FULLNLO
+    double a = pow(SM.Mrun(SM.getMuw(), SM.getQuarks(QCD::TOP).getMass(), SM.getQuarks(QCD::TOP).getMass(), 5.)
+            / SM.getMHl(), 2.);
+    
+    vmckpnn.clear();
+    
+    mckpnn.setMu(SM.getMut());
+ 
+    switch (mckpnn.getOrder()) {
+        case NNLO:
+        case NLO:
+            mckpnn.setCoeff(0, SM.Als(SM.getMuw())/4./M_PI*SM.getlamt().imag()*X1t(xt)/SM.GetLambda(), NLO);
+        case LO:
+            mckpnn.setCoeff(0, SM.getlamt().imag()*X0t(xt)/SM.GetLambda(), LO);
+            break;
+        default:
+            std::stringstream out;
+            out << mckpnn.getOrder();
+            throw std::runtime_error("StandardModelMatching::CMkp0nn(): order " + out.str() + "not implemented"); 
+    }
+    
+    switch (mckpnn.getOrder_ew()) {
+        case NLO_ew:
+            mckpnn.setCoeff(0, SM.getAle()/4./M_PI*SM.getlamt().imag()*Xewt(xt, a, SM.getMuw())/SM.GetLambda(), NLO_ew);
+        case LO_ew:
+            break; 
+        default:
+            std::stringstream out;
+            out << mckpnn.getOrder();
+            throw std::runtime_error("StandardModelMatching::CMkp0nn(): order " + out.str() + "not implemented"); 
+    }
+
+    vmckpnn.push_back(mckpnn);
+    return(vmckpnn);
+    
+}
+
+const std::vector<WilsonCoefficient>& StandardModelMatching::CMkppnn() {
+    
+    //PROBLEMI: mu e sin(theta_weak) e la scala di als
+    
+    double xt = pow(SM.Mrun(SM.getMuw(), SM.getQuarks(QCD::TOP).getMass(), SM.getQuarks(QCD::TOP).getMass(), 5.)
+            / SM.Mw_tree(), 2.); // always FULLNLO
+    double a = pow(SM.Mrun(SM.getMuw(), SM.getQuarks(QCD::TOP).getMass(), SM.getQuarks(QCD::TOP).getMass(), 5.)
+            / SM.getMHl(), 2.);
+    
+    vmckppnn.clear();
+    
+    mckppnn.setMu(SM.getMut());
+ 
+    switch (mckppnn.getOrder()) {
+        case NNLO:
+        case NLO:
+            mckppnn.setCoeff(0, SM.Als(SM.getMuw())/4./M_PI*SM.getlamt().imag()*X1t(xt)/SM.GetLambda(), NLO);
+        case LO:
+            mckppnn.setCoeff(0, SM.getlamt().imag()*X0t(xt)/SM.GetLambda(), LO);
+            break;
+        default:
+            std::stringstream out;
+            out << mckppnn.getOrder();
+            throw std::runtime_error("StandardModelMatching::CMkppnn(): order " + out.str() + "not implemented"); 
+    }
+    
+    switch (mckppnn.getOrder_ew()) {
+        case NLO_ew:
+            mckppnn.setCoeff(0, SM.getAle()/4./M_PI*SM.getlamt().imag()*Xewt(xt, a, SM.getMuw())/SM.GetLambda(), NLO_ew);
+        case LO_ew:
+            break; 
+        default:
+            std::stringstream out;
+            out << mckppnn.getOrder();
+            throw std::runtime_error("StandardModelMatching::CMkppnn(): order " + out.str() + "not implemented"); 
+    }
+
+    vmckppnn.push_back(mckppnn);
+    return(vmckppnn);
+    
+}
+
+const std::vector<WilsonCoefficient>& StandardModelMatching::CMkmm() {
+    
+    //PROBLEMI: mu e sin(theta_weak) e la scala di als
+    
+    double xt = pow(SM.Mrun(SM.getMuw(), SM.getQuarks(QCD::TOP).getMass(), SM.getQuarks(QCD::TOP).getMass(), 5.)
+            / SM.Mw_tree(), 2.); // always FULLNLO
+    
+    
+    vmckmm.clear();
+    
+    mckmm.setMu(SM.getMut());
+ 
+    switch (mckmm.getOrder()) {
+        case NNLO:
+        case NLO:
+            mckmm.setCoeff(0, SM.Als(SM.getMuw())/4./M_PI*SM.getlamt().real()*Y1t(xt)/SM.GetLambda(), NLO);
+        case LO:
+            mckmm.setCoeff(0, SM.getlamt().real()*Y0t(xt)/SM.GetLambda(), LO);
+            break;
+        default:
+            std::stringstream out;
+            out << mckmm.getOrder();
+            throw std::runtime_error("StandardModelMatching::CMkmm(): order " + out.str() + "not implemented"); 
+    }
+
+    vmckmm.push_back(mckmm);
+    return(vmckmm);
+    
+}
+
+const std::vector<WilsonCoefficient>& StandardModelMatching::CMBXsnn() {
+    
+    double xt = pow(SM.Mrun(SM.getMuw(), SM.getQuarks(QCD::TOP).getMass(), SM.getQuarks(QCD::TOP).getMass(), 5.)
+            / SM.Mw_tree(), 2.); // always FULLNLO
+    
+    vmcbsnn.clear();
+    
+    mcbsnn.setMu(SM.getMut());
+ 
+    switch (mcbsnn.getOrder()) {
+        case NNLO:
+        case NLO:
+            mcbsnn.setCoeff( 0, (SM.getVCKM()(2,1).abs() / SM.getVCKM()(1,2).abs())*
+                                SM.Als(SM.getMuw())/4./M_PI*X1t(xt), NLO);
+        case LO:
+            mcbsnn.setCoeff(0,  (SM.getVCKM()(2,1).abs() / SM.getVCKM()(1,2).abs())*
+                                X0t(xt), LO);
+            break;
+        default:
+            std::stringstream out;
+            out << mcbsnn.getOrder();
+            throw std::runtime_error("StandardModelMatching::CMXsnn(): order " + out.str() + "not implemented"); 
+    }
+
+    vmcbsnn.push_back(mcbsnn);
+    return(vmcbsnn);
+    
+}
+
+const std::vector<WilsonCoefficient>& StandardModelMatching::CMBXdnn() {
+    
+    double xt = pow(SM.Mrun(SM.getMuw(), SM.getQuarks(QCD::TOP).getMass(), SM.getQuarks(QCD::TOP).getMass(), 5.)
+            / SM.Mw_tree(), 2.); 
+    
+    vmcbdnn.clear();
+    
+    mcbdnn.setMu(SM.getMut());
+ 
+    switch (mcbdnn.getOrder()) {
+        case NNLO:
+        case NLO:
+            mcbsnn.setCoeff( 0, (SM.getVCKM()(2,2).abs() / SM.getVCKM()(1,2).abs())*
+                                SM.Als(SM.getMuw())/4./M_PI*X1t(xt), NLO);
+        case LO:
+            mcbsnn.setCoeff(0,  (SM.getVCKM()(2,2).abs() / SM.getVCKM()(1,2).abs())*
+                                X0t(xt), LO);
+            break;
+        default:
+            std::stringstream out;
+            out << mcbdnn.getOrder();
+            throw std::runtime_error("StandardModelMatching::CXdnn(): order " + out.str() + "not implemented"); 
+    }
+    
+    vmcbdnn.push_back(mcbdnn);
+    return(vmcbdnn);
+    
+}
+
+const std::vector<WilsonCoefficient>& StandardModelMatching::CMbsmm() {
+    
+    double xt = pow(SM.Mrun(SM.getMuw(), SM.getQuarks(QCD::TOP).getMass(), SM.getQuarks(QCD::TOP).getMass(), 5.)
+            / SM.Mw_tree(), 2.); 
+    
+    vmcbsmm.clear();
+    
+    mcbsmm.setMu(SM.getMut());
+ 
+    switch (mcbsmm.getOrder()) {
+        case NNLO:
+        case NLO:
+            mcbsnn.setCoeff( 0, (SM.getVCKM()(2,2).conjugate() * SM.getVCKM()(2,1)).abs()*
+                                SM.Als(SM.getMuw())/4./M_PI*Y1t(xt), NLO);
+        case LO:
+            mcbsnn.setCoeff(0,  (SM.getVCKM()(2,2).conjugate() * SM.getVCKM()(2,1)).abs()*
+                                Y0t(xt), LO);
+            break;
+        default:
+            std::stringstream out;
+            out << mcbsmm.getOrder();
+            throw std::runtime_error("StandardModelMatching::CXdnn(): order " + out.str() + "not implemented"); 
+    }
+    
+    vmcbsmm.push_back(mcbsmm);
+    return(vmcbsmm);
+    
+}
+
+const std::vector<WilsonCoefficient>& StandardModelMatching::CMbdmm() {
+    
+    double xt = pow(SM.Mrun(SM.getMuw(), SM.getQuarks(QCD::TOP).getMass(), SM.getQuarks(QCD::TOP).getMass(), 5.)
+            / SM.Mw_tree(), 2.); 
+    
+    vmcbdmm.clear();
+    
+    mcbdmm.setMu(SM.getMut());
+ 
+    switch (mcbdmm.getOrder()) {
+        case NNLO:
+        case NLO:
+            mcbsnn.setCoeff( 0, (SM.getVCKM()(2,2).conjugate() * SM.getVCKM()(2,0)).abs()*
+                                SM.Als(SM.getMuw())/4./M_PI*Y1t(xt), NLO);
+        case LO:
+            mcbsnn.setCoeff(0,  (SM.getVCKM()(2,2).conjugate() * SM.getVCKM()(2,0)).abs()*
+                                Y0t(xt), LO);
+            break;
+        default:
+            std::stringstream out;
+            out << mcbdmm.getOrder();
+            throw std::runtime_error("StandardModelMatching::CXdnn(): order " + out.str() + "not implemented"); 
+    }
+    
+    vmcbdmm.push_back(mcbdmm);
+    return(vmcbdmm);
+    
+}
+
 
 /*******************************************************************************
  * Wilson coefficients calculus, Buras base for nonlep. b decays               *  
