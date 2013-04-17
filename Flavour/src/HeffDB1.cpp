8229f3c4d79c7f8c6b392ae0c9465e892f2c1bc5
stefano <stefano@stefano>
2013-04-17 11:01:06 +0200

    Effective hamiltonian method for the rare Be decays

---
+++ Flavour/src/HeffDB1.cpp
@@ -0,0 +1,336 @@
+/* 
+ * Copyright (C) 2012 SusyFit Collaboration
+ * All rights reserved.
+ *
+ * For the licensing terms see doc/COPYING.
+ */
+    
+#include "HeffDB1.h"
+#include "gslpp_complex.h"
+
+using namespace gslpp;
+
+HeffDB1::HeffDB1(const StandardModel & SM) :
+        model(SM), u(10, NDR, NLO, NLO_ew, SM), 
+        coeffnlep00 (12, NDR, NLO, NLO_ew), coeffnlep00qcd (10, NDR, NLO, NLO_ew), coeffnlep00CC(10, NDR, NLO),
+        coeffnlep10 (12, NDR, NLO, NLO_ew), coeffnlep10qcd (10, NDR, NLO, NLO_ew), coeffnlep10CC(10, NDR, NLO),
+        coeffnlep01 (10, NDR, NLO), coeffnlep01A(10, NDR, NLO), coeffnlep01B(4, NDR, NLO),
+        coeffnlep11 (10, NDR, NLO), coeffnlep11A(10, NDR, NLO), coeffnlep11B(4, NDR, NLO),
+        coeffsmumu (1, NDR, NLO), coeffdmumu (1, NDR, NLO),
+        coeffsnunu (1, NDR, NLO), coeffdnunu (1, NDR, NLO),
+        nlep (12, 0.), nlep2(10, 0.), nlepCC(4, 0.){
+}
+
+HeffDB1::~HeffDB1() {
+}
+
+/*******************************************************************************
+ * evolution Wilson Coefficienys b-> nonlep.                                   * 
+ * Buras base                                                                  *
+ * deltaB=1   deltaC=0   deltaS=0                                              *
+ ******************************************************************************/
+vector<complex>** HeffDB1::ComputeCoeffBnlep00(double mu, schemes scheme) {
+    
+    const std::vector<WilsonCoefficient>& mcb = model.GetMyMatching()->CMbnlep( 0);
+    const std::vector<WilsonCoefficient>& mcbCC = model.GetMyMatching()->CMbnlepCC( 0);
+    
+    coeffnlep00qcd.setMu(mu); //inizializes to zero the coefficients
+    coeffnlep00CC.setMu(mu);
+    coeffnlep00.setMu(mu);
+    
+    orders_ew ordDF1ew = coeffnlep00.getOrder_ew();
+    orders ordDF1 =  coeffnlep00.getOrder();
+    
+    for (int i = 0; i < mcb.size(); i++){
+        for (int j = LO; j <= ordDF1; j++){
+            for (int k = LO; k <= j; k++) {        
+                
+                //Evolves the LO terms and the ones proportional to alpha_s 
+                coeffnlep00qcd.setCoeff(*coeffnlep00qcd.getCoeff(orders(j)) +
+                    u.Df1Evolnlep(mu, mcb[i].getMu(), orders(k), NULL_ew, mcb[i].getScheme())*
+                    (*(mcb[i].getCoeff(orders(j - k)))), orders(j));
+                
+                
+                //Evolves terms proportional to alpha_e and alpha_e/aplha_s
+                coeffnlep00qcd.setCoeff(*coeffnlep00qcd.getCoeff(orders_ew(j+4)) +
+                    u.Df1Evolnlep(mu, mcb[i].getMu(), NNLO, orders_ew(k+4), mcb[i].getScheme()) *
+                    (*(mcb[i].getCoeff(orders(j - k)))), orders_ew(j+4));
+                
+                
+                //Evolves the current*current part of the hamiltonian (the one non-proportional to lambda_t) 
+                coeffnlep00CC.setCoeff(*coeffnlep00CC.getCoeff(orders(j)) +
+                    u.Df1Evolnlep(mu, mcbCC[i].getMu(), orders(k), NULL_ew, mcbCC[i].getScheme()) *
+                    (*(mcbCC[i].getCoeff(orders(j - k)))), orders(j)); 
+   
+            }
+        }
+        
+                coeffnlep00qcd.setCoeff(*coeffnlep00qcd.getCoeff(orders_ew(NLO_ew)) +
+                    u.Df1Evolnlep(mu, mcb[i].getMu(), orders(LO), NULL_ew,  mcb[i].getScheme()) *
+                    (*(mcb[i].getCoeff(orders_ew(NLO_ew)))), orders_ew(NLO_ew));       
+        
+    }
+        
+    coeffnlep00qcd.setScheme(scheme);
+    coeffnlep00CC.setScheme(scheme);
+    
+    //Puts all together in a wilson coefficient object of 12 component: 
+    //the first 10 terms are the ones proportional to lambda_t
+    //the last two are the remainder current*current operators 
+    for (int j=LO; j <= ordDF1; j++) {
+        nlep2 = *coeffnlep00qcd.getCoeff(orders(j));
+        for (int i = 0; i < 10; i++){
+            nlep.assign(i, nlep2(i));
+        }
+        nlep2 = *coeffnlep00CC.getCoeff(orders(j));
+        for (int i = 10; i < 12; i++){
+            nlep.assign(i, nlep2(i-10));
+        }        
+    coeffnlep00.setCoeff(nlep, orders(j));
+    }
+    for (int k=LO_ew; k <= ordDF1ew; k++) {
+        nlep2 = *coeffnlep00qcd.getCoeff(orders_ew(k));
+        for (int l = 0; l < 10; l++){
+            nlep.assign(l, nlep2(l));;
+        }       
+    coeffnlep00.setCoeff(nlep, orders_ew(k));
+    }
+    
+    return coeffnlep00.getCoeff();
+}
+
+/*******************************************************************************
+ * evolution Wilson Coefficienys b-> nonlep.                                   * 
+ * Buras base                                                                  *
+ * deltaB=1   deltaC=0   deltaS=1                                              *
+ ******************************************************************************/
+vector<complex>** HeffDB1::ComputeCoeffBnlep10(double mu, schemes scheme) {
+    
+    const std::vector<WilsonCoefficient>& mcb = model.GetMyMatching()->CMbnlep( 1);
+    const std::vector<WilsonCoefficient>& mcbCC = model.GetMyMatching()->CMbnlepCC( 1);
+    
+    coeffnlep10qcd.setMu(mu);
+    coeffnlep10CC.setMu(mu);
+    coeffnlep10.setMu(mu);
+    
+    orders_ew ordDF1ew = coeffnlep10.getOrder_ew();
+    orders ordDF1 =  coeffnlep10.getOrder();
+    
+    for (int i = 0; i < mcb.size(); i++){
+        for (int j = LO; j <= ordDF1; j++){
+            for (int k = LO; k <= j; k++)   {
+                
+                //Evolves the LO terms and the ones proportional to alpha_s 
+                coeffnlep10qcd.setCoeff(*coeffnlep10qcd.getCoeff(orders(j)) +
+                    u.Df1Evolnlep(mu, mcb[i].getMu(), orders(k), NULL_ew,  mcb[i].getScheme()) *
+                    (*(mcb[i].getCoeff(orders(j - k)))), orders(j));
+                
+                //Evolves terms proportional to alpha_e and alpha_e/aplha_s
+                coeffnlep10qcd.setCoeff(*coeffnlep10qcd.getCoeff(orders_ew(j+4)) +
+                    u.Df1Evolnlep(mu, mcb[i].getMu(), NNLO, orders_ew(k+4), mcb[i].getScheme()) *
+                    (*(mcb[i].getCoeff(orders(j - k)))), orders_ew(j+4));
+                
+                //Evolves the current*current part of the hamiltonian (the one non-proportional to lambda_t)
+                coeffnlep10CC.setCoeff(*coeffnlep10CC.getCoeff(orders(j)) +
+                    u.Df1Evolnlep(mu, mcbCC[i].getMu(), orders(k), NULL_ew, mcbCC[i].getScheme()) *
+                    (*(mcbCC[i].getCoeff(orders(j - k)))), orders(j)); 
+        
+            }
+        }
+            
+        coeffnlep10qcd.setCoeff(*coeffnlep10qcd.getCoeff(orders_ew(NLO_ew)) +
+                    u.Df1Evolnlep(mu, mcb[i].getMu(), orders(LO), NULL_ew, mcb[i].getScheme()) *
+                    (*(mcb[i].getCoeff(orders(LO_ew)))), orders_ew(NLO_ew));
+    }        
+    
+    coeffnlep10qcd.setScheme(scheme);
+    coeffnlep10CC.setScheme(scheme);
+    
+    //Puts all together in a wilson coefficient object of 12 component: 
+    //the first 10 terms are the one proportional to lambda_t
+    //the last two are the remainder current*current operators 
+    
+    for (int j=LO; j <= ordDF1; j++) {
+        nlep2 = *coeffnlep10qcd.getCoeff(orders(j));
+        for (int i = 0; i < 10; i++){
+            nlep.assign(i, nlep2(i));
+        }
+        nlep2 = *coeffnlep10CC.getCoeff(orders(j));
+        for (int i = 10; i < 12; i++){
+            nlep.assign(i, nlep2(i-10));
+        }        
+    coeffnlep10.setCoeff(nlep, orders(j));
+    }
+    for (int k=LO_ew; k <= ordDF1ew; k++) {
+        nlep2 = *coeffnlep10qcd.getCoeff(orders_ew(k));
+        for (int l = 0; l < 10; l++){
+            nlep.assign(l, nlep2(l));;
+        }       
+    coeffnlep10.setCoeff(nlep, orders_ew(k));
+    }
+    
+    return coeffnlep10.getCoeff();
+}
+
+/*******************************************************************************
+ * evolution Wilson Coefficienys b-> nonlep.                                   * 
+ * Buras base                                                                  *
+ * deltaB=1   deltaC=1   deltaS=0                                              *
+ ******************************************************************************/
+vector<complex>** HeffDB1::ComputeCoeffBnlep01(double mu, schemes scheme) {
+    
+    const std::vector<WilsonCoefficient>& mcbCC1 =model.GetMyMatching()->CMbnlepCC( 2);
+    const std::vector<WilsonCoefficient>& mcbCC2 = model.GetMyMatching()->CMbnlepCC( 3);
+    
+    coeffnlep01.setMu(mu);
+    coeffnlep01A.setMu(mu);
+    coeffnlep01B.setMu(mu);
+    
+    orders ordDF1 = coeffnlep01A.getOrder();
+    
+    //evolution of the current*current terms
+    for (int i = 0; i < mcbCC1.size(); i++)
+        for (int j = LO; j <= ordDF1; j++)
+            for (int k = LO; k <= j; k++){
+                
+                coeffnlep01A.setCoeff(*coeffnlep01A.getCoeff(orders(j)) +
+                    u.Df1Evolnlep(mu, mcbCC1[i].getMu(), orders(k), NULL_ew, mcbCC1[i].getScheme()) *
+                    (*(mcbCC1[i].getCoeff(orders(j - k)))), orders(j)); 
+                
+                coeffnlep01B.setCoeff(*coeffnlep01B.getCoeff(orders(j)) +
+                    u.Df1Evolnlep(mu, mcbCC2[i].getMu(), orders(k), NULL_ew, mcbCC2[i].getScheme()) *
+                    (*(mcbCC2[i].getCoeff(orders(j - k)))), orders(j)); 
+            }
+        
+    coeffnlep01A.setScheme(scheme);
+    coeffnlep01B.setScheme(scheme);
+    coeffnlep01.setScheme(scheme);
+    
+    //Puts all together in a wilson coefficient object of 4 components
+    for (int j=LO; j <= ordDF1; j++) {
+        nlep2 = *coeffnlep01A.getCoeff(orders(j));
+        for (int i = 0; i < 2; i++){
+            nlep.assign(i, nlep2(i));
+        }
+        nlep2 = *coeffnlep01A.getCoeff(orders(j));
+        for (int i = 2; i < 4; i++){
+            nlep.assign(i, nlep2(i-2));
+        }        
+    coeffnlep01.setCoeff(nlep, orders(j));
+    }    
+    return coeffnlep01.getCoeff();   
+    
+}
+
+/*******************************************************************************
+ * evolution Wilson Coefficienys b-> nonlep.                                   * 
+ * Buras base                                                                  *
+ * deltaB=1   deltaC=1   deltaS=1                                              *
+ ******************************************************************************/
+vector<complex>** HeffDB1::ComputeCoeffBnlep11(double mu, schemes scheme) {
+    
+    const std::vector<WilsonCoefficient>& mcbCC1 = model.GetMyMatching()->CMbnlepCC( 2);
+    const std::vector<WilsonCoefficient>& mcbCC2 = model.GetMyMatching()->CMbnlepCC( 3);
+    
+    coeffnlep11.setMu(mu);
+    coeffnlep11A.setMu(mu);
+    coeffnlep11B.setMu(mu);
+    
+    orders ordDF1 = coeffnlep11A.getOrder();
+    
+    for (int i = 0; i < mcbCC1.size(); i++)
+        for (int j = LO; j <= ordDF1; j++)
+            for (int k = LO; k <= j; k++){
+                
+                coeffnlep11A.setCoeff(*coeffnlep11A.getCoeff(orders(j)) +
+                    u.Df1Evolnlep(mu, mcbCC1[i].getMu(), orders(k), NULL_ew, mcbCC1[i].getScheme()) *
+                    (*(mcbCC1[i].getCoeff(orders(j - k)))), orders(j)); 
+        
+                coeffnlep11B.setCoeff(*coeffnlep11B.getCoeff(orders(j)) +
+                    u.Df1Evolnlep(mu, mcbCC2[i].getMu(), orders(k), NULL_ew, mcbCC2[i].getScheme()) *
+                    (*(mcbCC2[i].getCoeff(orders(j - k)))), orders(j)); 
+            }
+        
+    coeffnlep11A.setScheme(scheme);
+    coeffnlep11B.setScheme(scheme);
+    coeffnlep11.setScheme(scheme);
+    
+   for (int j=LO; j <= ordDF1; j++) {
+        nlep2 = *coeffnlep11A.getCoeff(orders(j));
+        for (int i = 0; i < 2; i++){
+            nlep.assign(i, nlep2(i));
+        }
+        nlep2 = *coeffnlep11A.getCoeff(orders(j));
+        for (int i = 2; i < 4; i++){
+            nlep.assign(i, nlep2(i-2));
+        }        
+    coeffnlep11.setCoeff(nlep, orders(j));
+    }    
+    return coeffnlep11.getCoeff();   
+    
+}
+
+vector<complex>** HeffDB1::ComputeCoeffsmumu() {
+    
+    const std::vector<WilsonCoefficient>& mcb = model.GetMyMatching() -> CMbsmm();
+    
+    orders ordDF1 = coeffsmumu.getOrder();
+    
+    for (int i = 0; i < mcb.size(); i++){
+        for (int j = LO; j <= ordDF1; j++){
+            coeffsmumu.setCoeff(*coeffsmumu.getCoeff(orders(j))
+                                    + *mcb[i].getCoeff(orders(j)), orders(j));
+        }  
+    }
+    
+    return coeffsmumu.getCoeff(); 
+}
+
+vector<complex>** HeffDB1::ComputeCoeffdmumu() {
+    
+    const std::vector<WilsonCoefficient>& mcb = model.GetMyMatching() -> CMbsmm();
+    
+    orders ordDF1 = coeffdmumu.getOrder();
+    
+    for (int i = 0; i < mcb.size(); i++){
+        for (int j = LO; j <= ordDF1; j++){
+            coeffdmumu.setCoeff(*coeffdmumu.getCoeff(orders(j))
+                                    + *mcb[i].getCoeff(orders(j)), orders(j));
+        }
+    }
+    return coeffdmumu.getCoeff(); 
+} 
+
+vector<complex>** HeffDB1::ComputeCoeffsnunu() {
+    
+    const std::vector<WilsonCoefficient>& mcb = model.GetMyMatching() -> CMbsmm();
+    
+    orders ordDF1 = coeffsnunu.getOrder();
+    
+    for (int i = 0; i < mcb.size(); i++){
+        for (int j = LO; j <= ordDF1; j++){
+            coeffsnunu.setCoeff(*coeffsnunu.getCoeff(orders(j))
+                                    + *mcb[i].getCoeff(orders(j)), orders(j));
+        }
+    }
+     
+    return coeffsnunu.getCoeff(); 
+} 
+
+vector<complex>** HeffDB1::ComputeCoeffdnunu() {
+    
+    const std::vector<WilsonCoefficient>& mcb = model.GetMyMatching() -> CMbsmm();
+    
+    orders ordDF1 = coeffdnunu.getOrder();
+    
+    for (int i = 0; i < mcb.size(); i++){
+        for (int j = LO; j <= ordDF1; j++){
+            coeffdnunu.setCoeff(*coeffdnunu.getCoeff(orders(j))
+                                    + *mcb[i].getCoeff(orders(j)), orders(j));
+        }
+    }
+    
+    return coeffdnunu.getCoeff(); 
+} 
+
+
---
+++ Flavour/src/HeffDB1.h
@@ -0,0 +1,149 @@
+/* 
+ * Copyright (C) 2012 SusyFit Collaboration
+ * All rights reserved.
+ *
+ * For the licensing terms see doc/COPYING.
+ */
+
+#ifndef HEFFDB1_H
+#define	HEFFDB1_H
+
+#include <StandardModel.h>
+#include <StandardModelMatching.h>
+#include <WilsonCoefficient.h>
+#include "EvolDF1nlep.h"
+#include <sstream>
+
+using namespace gslpp;
+
+class HeffDB1 {
+public:
+    /**
+     * @brief constructor
+     * @param SM
+     * @param modelmatching
+     */
+    HeffDB1(const StandardModel & SM);
+    
+    /**
+     * 
+     * @brief destructor
+     */
+    virtual ~HeffDB1();
+    
+    /**
+     * 
+     * @param mu is the low energy scale
+     * @param scheme indicates the reonrmalization scheme
+     * @return the effective hamiltonian at the scale mu for B decays, \f$ |\Delta C = 0 | \f$, \f$ |\Delta S = 0 | \f$
+     */
+    vector<complex>** ComputeCoeffBnlep00(double mu, schemes scheme = NDR);
+    
+    /**
+     * 
+     * @param mu is the low energy scale
+     * @param scheme indicates the renormalization scheme
+     * @return the effective hamiltonian at the scale mu for B decays, \f$ |\Delta C = 1 | \f$, \f$ |\Delta S = 0 | \f$
+     */
+    vector<complex>** ComputeCoeffBnlep10(double mu, schemes scheme = NDR);
+    
+    /**
+     * 
+     * @param mu is the low energy scale
+     * @param scheme indicates the renormalization scheme
+     * @return the effective hamiltonian at the scale mu for B decays, \f$ |\Delta C = 0 | \f$, \f$ |\Delta S = 1 | \f$
+     */
+    vector<complex>** ComputeCoeffBnlep01(double mu, schemes scheme = NDR);
+    
+    /**
+     * 
+     * @param mu is the low energy scale
+     * @param scheme indicates the renormalization scheme
+     * @return the effective hamiltonian at the scale mu for B decays, \f$ |\Delta C = 1 | \f$, \f$ |\Delta S = 1 | \f$
+     */
+    vector<complex>** ComputeCoeffBnlep11(double mu, schemes scheme = NDR);
+    
+    /**
+     * 
+     * @param scheme
+     * @return short distance contribution to the rare decay \f$ B_{s} \rightarrow \mu \bar{\mu} \f$
+     */
+    vector<complex>** ComputeCoeffsmumu();
+    
+    /**
+     * 
+     * @param scheme
+     * @return short distance contribution to the rare decay \f$ B_{d} \rightarrow \mu \bar{\mu} \f$
+     */
+    vector<complex>** ComputeCoeffdmumu();
+    
+    /**
+     * 
+     * @param scheme
+     * @return short distance contribution to the rare decay \f$ B_{s} \rightarrow \nu \bar{\nu} \f$
+     */
+    vector<complex>** ComputeCoeffsnunu();
+    
+    /**
+     * 
+     * @param scheme
+     * @return short distance contribution to the rare decay \f$ B_{d} \rightarrow \nu \bar{\nu} \f$
+     */
+    vector<complex>** ComputeCoeffdnunu();
+    
+    
+    WilsonCoefficient getCoeffnlep00() const {
+        return coeffnlep00;
+    }
+    
+    WilsonCoefficient getCoeffnlep10() const {
+        return coeffnlep01;
+    }
+    
+    WilsonCoefficient getCoeffnlep01() const {
+        return coeffnlep10;
+    }
+    
+    WilsonCoefficient getCoeffnlep11() const {
+        return coeffnlep11;
+    }
+    
+    WilsonCoefficient getCoeffsmumu() const {
+        return coeffsmumu;
+    }
+    
+    WilsonCoefficient getCoeffdmumu() const {
+        return coeffdmumu;
+    }
+    
+    WilsonCoefficient getCoeffsnunu() const {
+        return coeffsnunu;
+    }
+    
+    WilsonCoefficient getCoeffdnunu() const {
+        return coeffdnunu;
+    }
+    
+    EvolDF1nlep getUDF1() const {
+        return u;
+    }
+
+    const StandardModel& GetModel() const {
+        return model;
+    }
+    
+private :
+    const StandardModel& model;
+    
+    WilsonCoefficient coeffnlep00qcd, coeffnlep00;
+    WilsonCoefficient coeffnlep10qcd, coeffnlep10;
+    WilsonCoefficient coeffnlep01, coeffnlep01A, coeffnlep01B, coeffnlep00CC;
+    WilsonCoefficient coeffnlep11, coeffnlep11A, coeffnlep11B, coeffnlep10CC;
+    WilsonCoefficient coeffsmumu, coeffdmumu;
+    WilsonCoefficient coeffsnunu, coeffdnunu;
+    EvolDF1nlep u;
+    
+    gslpp::vector<complex> nlep, nlep2, nlepCC;
+};
+
+#endif	/* HEFFDB1_H */
