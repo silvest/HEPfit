30c29938e7b450c5dad578da6310a8cae25bb1e0
vallima <Mauro_87@127.0.0.1>
2013-09-05 16:11:07 +0200

    Minor modifications on BR_Kp0nunu.cpp

--- Flavour/src/BR_Kp0nunu.cpp
+++ Flavour/src/BR_Kp0nunu.cpp
@@ -17,35 +17,35 @@ double BR_Kp0nunu::getThValue(){
 }
 
 complex BR_Kp0nunu::BRKp0nunu(orders order, orders_ew order_ew){
-    if (myFlavour.getHDS1().getCoeffDS1p0nunu().getOrder() < order){
+    if (myFlavour.getHDS1().getCoeffDS1pnunu().getOrder() < order){
         std::stringstream out;
         out << order;
         throw std::runtime_error("BRKp0nunu::getThValue(): requires cofficient of "
                                  "order" + out.str() + "not computed");
     }
     
-    vector<complex> ** allcoeff = myFlavour.ComputeCoeffDS1p0nunu();
+    vector<complex> ** allcoeff = myFlavour.ComputeCoeffDS1pnunu();
     
     switch(order_ew) {
         case NLO_ew:
             return((*(allcoeff[LO]) + *(allcoeff[NLO]) + *(allcoeff[NLO_ew])) *
                    (*(allcoeff[LO]) + *(allcoeff[NLO]) + *(allcoeff[NLO_ew])));
-        default:
+        case LO_ew:
+            switch(order) {
+                case NLO:
+                    return((*(allcoeff[LO]) + *(allcoeff[NLO])) * 
+                           (*(allcoeff[LO]) + *(allcoeff[NLO])));
+                case LO:
+                    return((*(allcoeff[LO])) * (*(allcoeff[LO])));
+                default:
+                    std::stringstream out;
+                    out << order;
+                    throw std::runtime_error("BRKp0nunu::BRKp0nunu(): order " + out.str() + "not implemented");
+            }
+         default:
             std::stringstream out;
-            out << order;
-            throw std::runtime_error("BRKp0nunu::BRKp0nunu(): order " + out.str() + "not implemented");;
-    }
-    
-    switch(order) {
-        case NLO:
-            return((*(allcoeff[LO]) + *(allcoeff[NLO])) * 
-                   (*(allcoeff[LO]) + *(allcoeff[NLO])));
-        case LO:
-            return((*(allcoeff[LO])) * (*(allcoeff[LO])));
-        default:
-            std::stringstream out;
-            out << order;
-            throw std::runtime_error("BRKp0nunu::BRKp0nunu(): order " + out.str() + "not implemented");;
+            out << order_ew;
+            throw std::runtime_error("BRKp0nunu::BRKp0nunu(): order_ew " + out.str() + "not implemented");
     }
 }
 
