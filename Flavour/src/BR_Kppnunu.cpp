6995d422d6c772a235857da41a3ec9f5c80ea1bb
vallima <Mauro_87@127.0.0.1>
2013-09-05 16:12:51 +0200

    Modifications on BR_Kppnunu.cpp in virtue of the new class Charm_Kpnunu implemented.

--- Flavour/src/BR_Kppnunu.cpp
+++ Flavour/src/BR_Kppnunu.cpp
@@ -16,49 +16,39 @@ double BR_Kppnunu::getThValue(){
 }
 
 complex BR_Kppnunu::BRKppnunu(orders order, orders_ew order_ew){
-    if (myFlavour.getHDS1().getCoeffDS1ppnunu().getOrder() < order){
+    if (myFlavour.getHDS1().getCoeffDS1pnunu().getOrder() < order){
         std::stringstream out;
         out << order;
         throw std::runtime_error("BRKppnunu::getThValue(): requires cofficient of "
                                  "order" + out.str() + "not computed");
     }
     
-    vector<complex> ** allcoeff = myFlavour.ComputeCoeffDS1ppnunu();
+    vector<complex> ** allcoeff = myFlavour.ComputeCoeffDS1pnunu();
     
-    switch(order) {
-        case NLO:
-            switch(order_ew) {
-                case NLO_ew:
-                    return((*(allcoeff[LO]) + *(allcoeff[NLO]) + *(allcoeff[NLO_ew])) *
-                           (*(allcoeff[LO]) + *(allcoeff[NLO]) + *(allcoeff[NLO_ew]))
-                           + CPB.CT_tot(NNLO, NLO_ew) * CPB.CT_tot(NNLO, NLO_ew) );
-                case LO_ew:
+    switch(order_ew) {
+        case NLO_ew:
+            return((*(allcoeff[LO]) + *(allcoeff[NLO]) + *(allcoeff[NLO_ew])) *
+                   (*(allcoeff[LO]) + *(allcoeff[NLO]) + *(allcoeff[NLO_ew])) +
+                   CKpnunu.C_TOT(NNLO,NLO_ew)*CKpnunu.C_TOT(NNLO,NLO_ew));
+        case LO_ew:
+            switch(order) {
+                case NLO:
                     return((*(allcoeff[LO]) + *(allcoeff[NLO])) *
-                           (*(allcoeff[LO]) + *(allcoeff[NLO]))
-                           + CPB.CT_tot(NNLO, LO_ew) * CPB.CT_tot(NNLO, LO_ew));
+                           (*(allcoeff[LO]) + *(allcoeff[NLO])) +
+                           CKpnunu.C_TOT(NLO,LO_ew)*CKpnunu.C_TOT(NLO,LO_ew));
+                case LO:
+                    return((*(allcoeff[LO])) * (*(allcoeff[LO]) ) +
+                           CKpnunu.C_TOT(LO,LO_ew)*CKpnunu.C_TOT(LO,LO_ew));
                 default:
                     std::stringstream out;
-                    out << order_ew;
-                    throw std::runtime_error("BRKppnunu::BRKppnunu(): order_ew " + out.str() + "not implemented");;
-            }
-        case LO:
-            switch(order_ew) {
-                case NLO_ew:
-                    return((*(allcoeff[LO]) + *(allcoeff[NLO_ew])) *
-                           (*(allcoeff[LO]) + *(allcoeff[NLO_ew]))
-                           + CPB.CT_tot(NNLO, NLO_ew) *CPB.CT_tot(NNLO, NLO_ew));
-                case LO_ew:
-                    return((*(allcoeff[LO]) * (*(allcoeff[LO])) 
-                           + CPB.CT_tot(NNLO, LO_ew)) * CPB.CT_tot(NNLO, LO_ew));
-                default:
-                    std::stringstream out;
-                    out << order_ew;
-                    throw std::runtime_error("BRKppnunu::BRKppnunu(): order_ew " + out.str() + "not implemented");;
+                    out << order;
+                    throw std::runtime_error("BRKppnunu::BRKppnunu(): order " + out.str() + "not implemented");
             }
         default:
             std::stringstream out;
-            out << order;
-            throw std::runtime_error("BRKppnunu::BRKppnunu(): order " + out.str() + "not implemented");;
+            out << order_ew;
+            throw std::runtime_error("BRKppnunu::BRKppnunu(): order_ew " + out.str() + "not implemented");
     }
 }
+
 
