3e6bdec82b032255872524762a47a7cbfde69687
SatoshiMishima <Satoshi.Mishima@roma1.infn.it>
2013-11-04 03:49:04 +0100

    added documentations to StandardModelParams.h.

--- StandardModel/src/StandardModelParams.h
+++ StandardModel/src/StandardModelParams.h
@@ -15,7 +15,7 @@
 /**
  * @class StandardModelParams
  * @ingroup StandardModel
- * @brief A class for posteriors of model parameters in StandardModel.
+ * @brief A class for retrieving parameters in StandardModel.
  * @author SusyFit Collaboration
  * @copyright GNU General Public License
  * @details
@@ -23,11 +23,22 @@
 class StandardModelParams : public ThObservable {
 public:
 
+    /**
+     * @brief StandardModelParams constructor. 
+     * @param[in] ObsType An object of ModelObservable class.
+     * @param[in] name_i The name of a parameter: "AlsMz", "dAle5Mz", "Mz", "mtop",
+     * "mHl", "delMw", "delSin2th_l", "delGammaZ", "delRhoZ_nu", "delRhoZ_e",
+     * "delRhoZ_u", "delRhoZ_d", "delRhoZ_b".
+     */
     StandardModelParams(const ThObsType& ObsType, const std::string name_i)
     : ThObservable(ObsType), name(name_i)
     {
     };
 
+    /**
+     * @brief Retrieves the value of the parameter specified in the constructor. 
+     * @return The value of the parameter. 
+     */
     double computeThValue();
 
 private:
