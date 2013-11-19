6f32bff71727297b71d4ae7a5242ae87a09afc01
Ayan Paul <apaul2@alumni.nd.edu>
2013-11-19 02:06:22 +0100

    MonteCarlo::generateEvent added. Generates observables with random parameters using BAT libraries. Can be made independent of BAT later. No chains initiated. For debugging and test runs only. Private member checkmode added to make sure event generator and MC modes are not called sequentially. Single event mode commented out from MonteCarlo:Run

--- MonteCarlo/src/MonteCarlo.cpp
+++ MonteCarlo/src/MonteCarlo.cpp
@@ -18,16 +18,14 @@
 
 MonteCarlo::MonteCarlo(const std::string& ModelConf_i,
                        const std::string& MonteCarloConf_i,
-                       const std::string& OutFile_i, const std::string& JobTag_i,
+                       const std::string& OutFile_i,
+                       const std::string& JobTag_i,
                        const bool checkTheoryRange_i)
 : myInputParser(), MCEngine(ModPars, Obs, Obs2D, CGO, ParaObs, checkTheoryRange_i)
 {
     ModelConf = ModelConf_i;
     MCMCConf = MonteCarloConf_i;
     JobTag = JobTag_i;
-    noMC = false;
-    if(OutFile_i.compare("")==0)
-        noMC = true;
     OutFile = OutFile_i + JobTag + ".root";
     ObsDirName = "Observables" + JobTag;
     FindModeWithMinuit = false;
@@ -35,14 +33,74 @@ MonteCarlo::MonteCarlo(const std::string& ModelConf_i,
     PrintCorrelationMatrix = false;
     PrintKnowledgeUpdatePlots = false;
     PrintParameterPlot = false;
+    checkMode = 0;
 }
 
 MonteCarlo::~MonteCarlo() 
 {
 }
 
+void MonteCarlo::generateEvent(const int rank, int unsigned nIteration, bool noMC, int seed)
+{
+    if(!noMC)
+        throw std::runtime_error("\nMonteCarlo::generateEvent():\n--noMC was not specified as an argument.\nPlease do so for running in Generate Event mode.\n");
+    if (checkMode == 1)
+        throw std::runtime_error("\nMonteCarlo::generateEvent():\nMonte Carlo Mode requested first. Cannot run in Generate Event mode");
+    checkMode = 1;
+    try {
+        /* set model parameters */
+        std::string ModelName = myInputParser.ReadParameters(ModelConf, ModPars, Obs, Obs2D, CGO, ParaObs);
+        MCEngine.SetName(ModelName.c_str());
+        MCEngine.MCMCSetRandomSeed(seed);
+        int npar = 0;
+        std::map<std::string, double> DP;
+        std::vector<double> INI;
+        for (std::vector<ModelParameter>::iterator it = ModPars.begin(); it < ModPars.end(); it++) {
+            DP[it->name] = it->ave;
+            if (it->errg > 0. || it->errf > 0.){
+                INI.push_back(it->ave);
+                npar++;}
+        }
+        int buffsize = npar++;
+        if (!myInputParser.getMyModel()->Init(DP)) {
+            throw std::runtime_error("parameter(s) missing in model initialization");
+        }
+        MCEngine.DefineParameters();
+        if (nIteration == 0) {
+            std::cout.precision(10);
+            std::cout << std::endl << "Running in Single Event mode...\n" << std::endl;
+        }
+        for (int unsigned i = 0; i < nIteration + 1; i++) {
+            MCEngine.generateRandomEvent(myInputParser.getMyModel(),i);
+            for (std::vector<Observable>::iterator it = Obs.begin();
+                 it < Obs.end(); it++) {
+                double th = it->computeTheoryValue();
+                std::cout << it->getName() << " = " << th << std::endl;
+            }
+            std::cout << std::endl;
+            for (std::vector<CorrelatedGaussianObservables>::iterator it = CGO.begin();
+                 it < CGO.end(); it++) {
+                std::vector<Observable> ObsInCGO = it->getObs();
+                for (std::vector<Observable>::iterator it2 = ObsInCGO.begin();
+                     it2 < ObsInCGO.end(); it2++) {
+                    double th = it2->computeTheoryValue();
+                    std::cout << it2->getName() << " = " << th << std::endl;
+                }
+                std::cout << std::endl;
+            }
+        }
+        return;
+        } catch (std::string message) {
+        std::cerr << message << std::endl;
+        exit(EXIT_FAILURE);
+        }
+}
+
 void MonteCarlo::Run(const int rank) 
 {
+        if (checkMode == 1)
+                throw std::runtime_error("\nMonteCarlo::Run():\nGenerate Event Mode requested first. Cannot run in Monte Carlo mode");
+        checkMode = 1;
     try {
 
         /* set model parameters */
@@ -59,7 +117,7 @@ void MonteCarlo::Run(const int rank)
             throw std::runtime_error("parameter(s) missing in model initialization");
         }
         
-        if(noMC) {
+        /*if(noMC) {
             std::cout.precision(10);
             std::cout << std::endl << "Running in Single Event mode..." << std::endl;
             for (std::vector<Observable>::iterator it = Obs.begin();
@@ -77,7 +135,7 @@ void MonteCarlo::Run(const int rank)
                 }
             }
             return;
-        }
+        }*/
 
         std::cout << std::endl << "Running in MonteCarlo mode..." << std::endl;
 
--- MonteCarlo/src/MonteCarlo.h
+++ MonteCarlo/src/MonteCarlo.h
@@ -13,7 +13,8 @@
 
 /**
  * @addtogroup MonteCarlo
- * @brief A project for Monte Carlo. 
+ * @brief A project for Markov Chain Monte Carlo based on
+ * the <a href="https://www.mppmu.mpg.de/bat/" target=blank>Bayesian Analysis Tool (BAT)</a>
  * @{
  */
 /**
@@ -21,15 +22,30 @@
  * @brief A class for Monte Carlo. 
  * @author SusyFit Collaboration
  * @copyright GNU General Public License
- * @details 
+ * @details This class is responsible for the MCMC runs using the
+ * MCMCEngine class together with input parameters from the InputParser
+ * class. It allows or two modes of runs.
+ * @li <b> Event Generator Mode </b>: If --noMC is specified as a run-time flag in the
+ * argument, the run will be conducted as a randomly generated events as no Markov
+ * Chain runs will be initiated. The observables that are specified in the
+ * SomeModel.conf file will be claculated with the central values of the parameters
+ * specified in the SomeModel.conf file as the first set of valuses. After which
+ * observables will be generated with random sets of parameters. This can be run with
+ * or without a seed. This mode is primarily to be used for debugging purposes
+ * or for test runs.
+ * @li <b> Monte Carlo Mode </b>: This is the default run mode if the --noMC flag is
+ * not specified.
  */
 class MonteCarlo {
 public:
-    MonteCarlo(const std::string& ModelConf_i, const std::string& 
-            MonteCarloConf_i, const std::string& OutFile_i, 
-            const std::string& JobTag_i, const bool checkTheoryRange_i=false);
+    MonteCarlo(const std::string& ModelConf_i,
+               const std::string& MonteCarloConf_i,
+               const std::string& OutFile_i,
+               const std::string& JobTag_i,
+               const bool checkTheoryRange_i=false);
     virtual ~MonteCarlo();
     void Run(const int rank);
+    void generateEvent(const int rank, int unsigned nIteration, bool noMC, int i = 0);
 private:
     InputParser myInputParser;
     MonteCarloEngine MCEngine;
@@ -42,6 +58,7 @@ private:
     bool noMC;
     bool FindModeWithMinuit, PrintAllMarginalized;
     bool PrintCorrelationMatrix, PrintKnowledgeUpdatePlots, PrintParameterPlot;
+    int checkMode;
 };
 
 /** 
