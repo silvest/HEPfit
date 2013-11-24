abb0452b0243960f395e2e6d3f1cf955a4198b21
Ayan Paul <apaul2@alumni.nd.edu>
2013-11-24 16:37:29 +0100

    Event Generation separated from Monte Carlo. Dependence on BAT to be removed.

---
+++ MonteCarlo/src/GenerateEvent.cpp
@@ -0,0 +1,85 @@
+/* 
+ * File:   GenerateEvent.cpp
+ * Author: ayanpaul
+ * 
+ * Created on November 24, 2013, 2:44 PM
+ */
+
+#include "GenerateEvent.h"
+#include <TSystem.h>
+#ifdef _MPI
+#include <mpi.h>
+#endif
+
+GenerateEvent::GenerateEvent(const std::string& ModelConf_i,
+                             const std::string& OutDirName_i,
+                             const std::string& JobTag_i,
+                             const bool noMC_i,
+                             const bool checkTheoryRange_i)
+: myInputParser(), MCEngine(ModPars, Obs, Obs2D, CGO, ParaObs, checkTheoryRange_i)
+{
+    ModelConf = ModelConf_i;
+    JobTag = JobTag_i;
+    OutDirName = OutDirName_i + JobTag;
+    ObsDirName = OutDirName + JobTag + "/Observables";
+    ParsDirName = OutDirName + JobTag + "/Parameters";
+    noMC = noMC_i;
+}
+
+GenerateEvent::~GenerateEvent() 
+{
+}
+
+void GenerateEvent::generate(const int rank, int unsigned nIteration, int seed)
+{
+        if(!noMC)
+        throw std::runtime_error("\nMonteCarlo::generateEvent():\n--noMC was not specified as an argument.\nPlease do so for running in Generate Event mode.\n");
+    try {
+        /* set model parameters */
+        std::string ModelName = myInputParser.ReadParameters(ModelConf, ModPars, Obs, Obs2D, CGO, ParaObs);
+        MCEngine.SetName(ModelName.c_str());
+        MCEngine.MCMCSetRandomSeed(seed);
+        int npar = 0;
+        std::map<std::string, double> DP;
+        std::vector<double> INI;
+        for (std::vector<ModelParameter>::iterator it = ModPars.begin(); it < ModPars.end(); it++) {
+            DP[it->name] = it->ave;
+            if (it->errg > 0. || it->errf > 0.){
+                INI.push_back(it->ave);
+                npar++;}
+        }
+        int buffsize = npar++;
+        if (!myInputParser.getMyModel()->Init(DP)) {
+            throw std::runtime_error("parameter(s) missing in model initialization");
+        }
+        MCEngine.DefineParameters();
+        if (nIteration == 0) {
+            std::cout.precision(10);
+            std::cout << std::endl << "Running in Single Event mode...\n" << std::endl;
+        }
+        for (int unsigned i = 0; i < nIteration + 1; i++) {
+            MCEngine.generateRandomEvent(myInputParser.getMyModel(),i);
+            for (std::vector<Observable>::iterator it = Obs.begin();
+                 it < Obs.end(); it++) {
+                double th = it->computeTheoryValue();
+                std::cout << it->getName() << " = " << th << std::endl;
+            }
+            std::cout << std::endl;
+            for (std::vector<CorrelatedGaussianObservables>::iterator it = CGO.begin();
+                 it < CGO.end(); it++) {
+                std::vector<Observable> ObsInCGO = it->getObs();
+                for (std::vector<Observable>::iterator it2 = ObsInCGO.begin();
+                     it2 < ObsInCGO.end(); it2++) {
+                    double th = it2->computeTheoryValue();
+                    std::cout << it2->getName() << " = " << th << std::endl;
+                }
+                std::cout << std::endl;
+            }
+        }
+        return;
+        } catch (std::string message) {
+        std::cerr << message << std::endl;
+        exit(EXIT_FAILURE);
+        }
+}
+
---
+++ MonteCarlo/src/GenerateEvent.h
@@ -0,0 +1,98 @@
+/* 
+ * File:   GenerateEvent.h
+ * Author: ayanpaul
+ *
+ * Created on November 24, 2013, 2:44 PM
+ */
+
+#ifndef GENERATEEVENT_H
+#define	GENERATEEVENT_H
+
+#include <InputParser.h>
+#include "MonteCarloEngine.h"
+
+/**
+ * @addtogroup MonteCarlo
+ * @brief A project for Markov Chain Monte Carlo based on
+ * the <a href="https://www.mppmu.mpg.de/bat/" target=blank>Bayesian Analysis Tool (BAT)</a>
+ * @{
+ */
+/**
+ * @class MonteCarlo
+ * @brief A class for Monte Carlo. 
+ * @author SusyFit Collaboration
+ * @copyright GNU General Public License
+ * @details This class is responsible for the MCMC runs using the
+ * MCMCEngine class together with input parameters from the InputParser
+ * class. It allows or two modes of runs.
+ * @li <b> Event Generator Mode </b>: If --noMC is specified as a run-time flag in the
+ * argument, the run will be conducted as a randomly generated events as no Markov
+ * Chain runs will be initiated. The observables that are specified in the
+ * SomeModel.conf file will be claculated with the central values of the parameters
+ * specified in the SomeModel.conf file as the first set of valuses. After which
+ * observables will be generated with random sets of parameters. This can be run with
+ * or without a seed. This mode is primarily to be used for debugging purposes
+ * or for test runs.
+ * @li <b> Monte Carlo Mode </b>: This is the default run mode if the --noMC flag is
+ * not specified.
+ */
+class GenerateEvent {
+public:
+    /**
+     * @brief The default constructor.
+     * @details The default constructor sets the names of the configuration files
+     * and the names of the output root file and specific output directory if a job ID is
+     * specified as an argument to the executable. The constructor also sets the default 
+     * values of some flags related to the Monte Carlo run. The boolean flags (FindModeWithMinuit,
+     * PrintAllMarginalized, PrintCorrelationMatrix, PrintKnowledgeUpdatePlots, PrintParameterPlot)
+     * attain their run time values after the MonteCarlo.conf file is parsed by 
+     * MonteCarlo::Run().
+     * @param[in] ModelConf_i the name of the input configuration file for the model name,
+     * the model parameters and observables to be calculated (Observables, Observables2D,
+     * Model Parameters vs. Observables and Correlated Gaussian Observables)
+     * @param[in] MonteCarloConf_i the name of the Monte Carlo configuration file that
+     * specifies the parameters of the monte carlo run like no. of chains, no. of pre run 
+     * iterations etc.
+     * @param[in] OutFile_i the name of the root output file to be given without the .root
+     * extention
+     * @param[in] JobTag_i optional job tag that might be specified
+     * @param[in] checkTheoryRange_i (default = false)
+     */
+    GenerateEvent(const std::string& ModelConf_i,
+                  const std::string& OutDirName_i,
+                  const std::string& JobTag_i,
+                  const bool noMC_i,
+                  const bool checkTheoryRange_i=false);
+    
+    /**
+     * @brief The default destructor.
+     */
+    virtual ~GenerateEvent();
+    
+    /**
+     * @brief
+     * @param[in]
+     * @param[in]
+     * @return
+     */
+    void generate(const int rank, int unsigned nIteration, int seed = 0);
+    
+private:
+    InputParser myInputParser; /**< an oject of the InputParser() class */
+    MonteCarloEngine MCEngine; /**< an object of the MonteCarloEngine() class*/
+    std::vector<ModelParameter> ModPars; /**< vector for the model parameters defined in SomeModel.conf*/
+    std::vector<Observable> Obs; /**< vector for the observables defined in SomeModel.conf*/
+    std::vector<Observable2D> Obs2D; /**< vector for the Observables2D defined in SomeModel.conf*/
+    std::vector<CorrelatedGaussianObservables> CGO; /**< vector for the Correlated Gaussian Observables defined in SomeModel.conf*/
+    std::vector<ModelParaVsObs> ParaObs; /**< vector for the ModelParaVsObs defined in SomeModel.conf*/
+    std::string ModelConf; /**< string for the name of the SomeModel.conf file*/
+    std::string MCMCConf; /**< string for the name of the MonteCarlo.conf file*/
+    std::string OutDirName; /**< string for the name of the output root file without the .root extension*/
+    std::string ObsDirName; /**< string for the name of the output root file without the .root extension*/
+    std::string ParsDirName; /**< string for the name of the output root file without the .root extension*/
+    std::string JobTag; /**< string for the optional JobTag argument to be passes to the executable*/
+    bool noMC;
+};
+
+#endif	/* GENERATEEVENT_H */
+
