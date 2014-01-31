dc59fc5e2ec5bc7ec9e130e3329814eb7e1b11bc
SatoshiMishima <Satoshi.Mishima@roma1.infn.it>
2014-01-31 08:08:33 +0100

    added documentation to OrderScheme.h.

--- StandardModel/src/OrderScheme.h
+++ StandardModel/src/OrderScheme.h
@@ -1,5 +1,5 @@
 /* 
- * Copyright (C) 2012-2013 SusyFit Collaboration
+ * Copyright (C) 2012-2014 SusyFit Collaboration
  * All rights reserved.
  *
  * For the licensing terms see doc/COPYING.
@@ -14,22 +14,39 @@
 /**
  * @enum schemes
  * @ingroup StandardModel
- * @brief Available schemes are NDR, HV and LRI.
+ * @brief An enum type for regularization schemes.
  */
-enum schemes {NDR=0, HV, LRI};
+enum schemes
+{
+    NDR=0, /**< Naive dimensional regularization (NDR) scheme */
+    HV, /**< 't Hooft-Veltman (HV) scheme */
+    LRI /**< Regularization-Independent (RI) renormalization schemes with the Landau gauge */
+};
 
 /**
  * @enum orders
  * @ingroup StandardModel
- * @brief Available orders in QCD are LO, NLO, NNLO, FULLNLO = LO + NLO , FULLNNLO = LO + NLO + NNLO.
+ * @brief An enum type for orders in %QCD.
  */
-enum orders {LO=0, NLO, NNLO, FULLNLO, FULLNNLO};
+enum orders
+{
+    LO=0, /**< Leading order */
+    NLO,  /**< Next-to-leading order */
+    NNLO,  /**< Next-to-next-to-leading order */
+    FULLNLO, /**< Full NLO = LO + NLO */
+    FULLNNLO /**< Full NNLO = LO + NLO + NNLO */
+};
 
 /**
  * @enum orders_ew
  * @ingroup StandardModel
- * @brief Available orders in Electroweak are NULL_ew, LO_ew and NLO_ew.
+ * @brief An enum type for orders in electroweak.
  */
-enum orders_ew {NULL_ew = orders(MAXORDER+1), LO_ew, NLO_ew};
+enum orders_ew
+{
+    NULL_ew = orders(MAXORDER+1), /**< An auxiliary enumerator */
+    LO_ew, /**< Leading order */
+    NLO_ew  /**< Next-to-leading order */
+};
 
 #endif	/* ORDERSCHEME_H */
