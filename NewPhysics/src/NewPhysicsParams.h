aa77b24722ce5788db5b6c3a9bcd71c1cc934a02
SatoshiMishima <Satoshi.Mishima@roma1.infn.it>
2014-01-31 09:06:21 +0100

    added documentation to StandardModelParams and NewPhysicsParams classes.

--- NewPhysics/src/NewPhysicsParams.h
+++ NewPhysics/src/NewPhysicsParams.h
@@ -14,19 +14,47 @@
 
 /**
  * @class NewPhysicsParams
- * @brief A class for retrieving parameters associated with NewPhysics project.
+ * @brief A class for retrieving parameters associated with the models in
+ * NewPhysics module.
  * @ingroup NewPhysics
  * @author SusyFit Collaboration
  * @copyright GNU General Public License
- * @details
+ * @details This class is responsible for retrieving the values of parameters
+ * in NPEffective1, NPEffective2, NPEpsilons, NPEpsilons_pureNP, NPSTU,
+ * NPSTUVWXY and NPZbbbar classes.
+ * The available parameters are listed in the description of the constructor
+ * NewPhysicsParams().
  */
 class NewPhysicsParams : public ThObservable {
 public:
 
     /**
      * @brief Constructor. 
-     * @param[in] ObsType
-     * @param[in] name_i the name of the parameter to be retrieved
+     * @param[in] ObsType a reference to an object of type ThObsType
+     * @param[in] name_i the name of the parameter to be retrieved: 
+     * @li "epsilon1", "epsilon2", "epsilon3", "epsilonb":&nbsp;
+     * the epsilon parameters @f$\varepsilon_1@f$, @f$\varepsilon_2@f$,
+     * @f$\varepsilon_3@f$ and @f$\varepsilon_b@f$,
+     * @li "deltaGVb", "deltaGAb":&nbsp;
+     * the new physics corrections to the vector and axial-vector @f$Zb\bar{b}@f$
+     * couplings, denoted by @f$\delta g_V^b@f$ and @f$\delta g_A^b@f$,
+     * @li "deltaGRb", "deltaGLb":&nbsp;
+     * the new physics corrections to the right-handed and left-handed @f$Zb\bar{b}@f$
+     * couplings, denoted by @f$\delta g_R^b@f$ and @f$g\delta _L^b@f$,
+     * @li "deltaRhoZb", "deltaKappaZb":&nbsp;
+     * the new physics corrections to the @f$Zb\bar{b}@f$ couplings, 
+     * denoted by @f$\delta\rho_Z^b@f$ and @f$\delta\kappa_Z^b@f$,
+     * @li "cHLp_NP", "cHQp_NP", "cHL_NP", "cHQ_NP", "cHU2_NP", "cHD3_NP":&nbsp;
+     * the coefficients of the dimension six operators, 
+     * @f$C'_{HL}@f$, @f$C'_{HQ}@f$, @f$C_{HL}@f$, @f$C_{HQ}@f$, @f$C_{HU_2}@f$
+     * and @f$C_{HD_3}@f$,
+     * @li "cHQ1pPLUScHQ2p_NP", "cHQ2pMINUScHQ2_NP", "cHQ3pPLUScHQ3_NP":&nbsp;
+     * @f$C'_{HQ_1} + C'_{HQ_2}@f$, @f$C'_{HQ_2} - C_{HQ_2}@f$ and 
+     * @f$C'_{HQ_3} + C_{HQ_3}@f$
+     * @li "c_Ae_NP":&nbsp;
+     * @f$C[\mathcal{A}_\ell]@f$ defined in @cite Ciuchini:2013pca,
+     * @li "c_GammaZ_uds_NP":&nbsp;
+     * @f$C[\Gamma_{uds}]@f$ defined in @cite Ciuchini:2013pca.
      */
     NewPhysicsParams(const ThObsType& ObsType, const std::string name_i)
     : ThObservable(ObsType), name(name_i)
@@ -34,13 +62,14 @@ public:
     };
 
     /**
-     * @brief A method to retrieve the value of the desired new physics parameter.
-     * @return the value of the new physics parameter
+     * @brief A method to retrieve the value of the desired new physics parameter
+     * specified with the constructor.
+     * @return the value of the parameter
      */
     double computeThValue();
 
 private:
-    const std::string name;
+    const std::string name;///< The name of the parmeter to be retrieved.
 
 };
 
--- StandardModel/src/StandardModelParams.h
+++ StandardModel/src/StandardModelParams.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013 SusyFit Collaboration
+ * Copyright (C) 2013-2014 SusyFit Collaboration
  * All rights reserved.
  *
  * For the licensing terms see doc/COPYING.
@@ -15,20 +15,29 @@
 /**
  * @class StandardModelParams
  * @ingroup StandardModel
- * @brief A class for retrieving parameters in StandardModel.
+ * @brief A class for retrieving parameters in QCD and StandardModel.
  * @author SusyFit Collaboration
  * @copyright GNU General Public License
- * @details
+ * @details This class is responsible for retrieving the values of model
+ * parameters in QCD and StandardModel classes. The available parameters are
+ * listed in the description of the constructor StandardModelParams().
  */
 class StandardModelParams : public ThObservable {
 public:
 
     /**
-     * @brief StandardModelParams constructor. 
-     * @param[in] ObsType An object of ModelObservable class.
-     * @param[in] name_i The name of a parameter: "AlsMz", "dAle5Mz", "Mz", "mtop",
-     * "mHl", "delMw", "delSin2th_l", "delGammaZ", "delRhoZ_nu", "delRhoZ_e",
-     * "delRhoZ_u", "delRhoZ_d", "delRhoZ_b".
+     * @brief Constructor.
+     * @param[in] ObsType a reference to an object of type ThObsType
+     * @param[in] name_i the name of the parameter to be retrieved:
+     * @li "AlsMz":&nbsp; the fine-structure constant @f$\alpha@f$,
+     * @li "dAle5Mz":&nbsp; the five-flavour hadronic contribution to the electromagnetic coupling,
+     * @f$\Delta\alpha_{\mathrm{had}}^{(5)}(M_Z^2)@f$, 
+     * @li "Mz":&nbsp; the Fermi constant @f$G_\mu@f$ in @f$\mathrm{GeV}^{-2}@f$,
+     * @li "mtop":&nbsp; the top-quark mass @f$m_t@f$ in GeV,
+     * @li "mHl":&nbsp; the Higgs mass @f$m_h@f$ in GeV,
+     * @li "delMw":&nbsp; the theoretical uncertainty in @f$M_W@f$ in GeV,
+     * @li "delSin2th_l":&nbsp; the theoretical uncertainty in @f$\sin^2\theta_{\rm eff}^{\rm lept}@f$,
+     * @li "delGammaZ":&nbsp; the theoretical uncertainty in @f$\Gamma_Z@f$ in GeV.
      */
     StandardModelParams(const ThObsType& ObsType, const std::string name_i)
     : ThObservable(ObsType), name(name_i)
@@ -36,13 +45,14 @@ public:
     };
 
     /**
-     * @brief Retrieves the value of the parameter specified in the constructor. 
-     * @return The value of the parameter. 
+     * @brief A method to retrieve the value of the desired parameter specified
+     * with the constructor. 
+     * @return the value of the parameter
      */
     double computeThValue();
 
 private:
-    const std::string name;
+    const std::string name;///< The name of the parmeter to be retrieved.
 
 };
 
