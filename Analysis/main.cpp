2ac78f1af4a4032fbaaab2c03e26e7cdb20e6b35
Ayan Paul <apaul2@alumni.nd.edu>
2014-02-12 18:01:01 +0100

    Argument error handling fixed. Argument --nI changed to --it for unique single character recognition.

--- Analysis/main.cpp
+++ Analysis/main.cpp
@@ -35,99 +35,111 @@ int main(int argc, char** argv)
     int rank = 0;
 #endif
 
-    string ModelConf, MCMCConf, FileOut, JobTag;
+    string ModelConf, JobTag;
     string FolderOut;
     int nIterations = 0;
     bool noMC;
     bool checkTheoryRange = false;
 
-    if (rank == 0)
-        cout << "\n *** SusyFit Markov Chain Montecarlo ***\n" << endl;
-
     try {
-        options_description desc("Allowed (positional) options");
+        options_description desc("\nAllowed (positional) options");
         desc.add_options()
-                ("modconf", value<string > (), "model config filename (1st)")
-                ("mcconf", value<string > (), "montecarlo config filename (2nd)")
+                ("modconf", value<string > (), "MANDATORY model config filename (1st)")
+                ("mcconf", value<string > (), "MANDATORY Monte Carlo config filename (2nd) for MCMC run")
+                ("noMC", "run in generate event mode with --noMC (no MCMC run)")
+                ("it", value<int > ()->default_value(0),
+                 "no. of iterations in generate event mode, specify with --it=# or --it #")
                 ("rootfile", value<string > ()->default_value("MCout"),
                 "output root filename (without extension)")
                 ("output_folder", value<string > ()->default_value(""),
-                "output folder for Generate Event mode to be specified for printing to file")
+                "output folder for Generate Event mode to be specified for printing to file, please specify with --output_folder <name>")
                 ("job_tag", value<string > ()->default_value(""),
-                "job tag")
-                ("noMC", "run in generate event mode with --noMC")
-                ("nI", value<int > ()->default_value(0),
-                "no. of iterations in generate event mode, specify with --nI=#")
-                ("thRange", "output the min and max of theory values to HistoLog.txt")
+                "job tag, please specify with --job_tag <tag>")
+                ("thRange", "output the min and max of theory values to HistoLog.txt, please turn on with --thRange")
                 ("help", "help message")
                 ;
+        string coderun = "\n *** SusyFit Routines ***\n"
+                         "\nMonte Carlo mode: analysis Model.conf MonteCarlo.conf [--rootfile <name>] [--job_tag <tag>] [--thRange]"
+                         "\nGenerate Event mode: analysis Model.conf --noMC [--nI #] [--output_folder <name> [--job_tag <tag>]]\n";
         positional_options_description pd;
         variables_map vm;
-        if (!vm.count("noMC")) {
-            pd.add("modconf", 1);
-            pd.add("mcconf", 1);
-            //pd.add("rootfile", 1);
-            //pd.add("job_tag", 1);
-        } else {
-            pd.add("modconf", 1);
-            //pd.add("output_folder", 1);
-            //pd.add("job_tag", 1);
-        }
+        pd.add("modconf", 1);
+        pd.add("mcconf", 1);
+
         
         try {
             store(command_line_parser(argc, argv).options(desc).positional(pd).run(), vm);
             notify(vm);
 
             if (vm.count("help")) {
-                cout << desc << endl;
+                if (rank == 0){
+                    cout << coderun << endl;
+                    cout << desc << endl;
+                }
                 return EXIT_SUCCESS;
-            }
-
+            }       
+            
             if (vm.count("modconf"))
                 ModelConf = vm["modconf"].as<string > ();
             else
-                throw runtime_error("Please specify mandatory model config filename");
+                if (rank == 0){
+                cout << coderun << endl;
+                cout << desc << endl;
+                throw runtime_error("\nERROR: Please specify mandatory model config filename.\n");
+                }
+
+            if (vm.count("noMC") && vm["output_folder"].as<string > () == "" && !(vm["job_tag"].as<string > () == "")){
+                if (rank == 0) cout << "\nWARNING: --job_tag not being used as --output_folder is not specified.\nOutput being sent to terminal only.\n";
+            } else {
+            JobTag = vm["job_tag"].as<string > ();
+            }
             
             if (vm.count("noMC") && vm.count("mcconf"))
             {
-                throw std::runtime_error("\nArgument Error:\nPlease specify EITHER --noMC OR Monte Carlo configuration file as the second argument.\n");
+                if (rank == 0){
+                    cout << coderun << endl;
+                    cout << desc << endl;
+                    throw std::runtime_error("\nERROR: Please specify EITHER --noMC OR Monte Carlo configuration file as the second free argument.\nAll other arguments must be specified with option specifiers (--nI=#, --job_tag <tag>, etc.)\n");
+                }
             }
             else if (vm.count("noMC") && !vm.count("mcconf"))
             {
+                if (rank == 0)
+                    cout << "\n *** SusyFit Event Generation ***\n" << endl;
                 noMC = true;
-                MCMCConf = "";
                 FolderOut = vm["output_folder"].as<string > ();
-                nIterations = vm["nI"].as<int > ();
+                nIterations = vm["it"].as<int > ();
+                GenerateEvent GE(ModelConf, FolderOut, JobTag, noMC);
+                GE.generate(rank, nIterations);
             }
             else if (!vm.count("noMC") && vm.count("mcconf"))
             {
+                if (rank == 0)
+                    cout << "\n *** SusyFit Markov Chain Monte Carlo ***\n" << endl;
                 noMC = false;
-                MCMCConf = vm["mcconf"].as<string > ();
-                FileOut = vm["rootfile"].as<string > ();
-                    
+                string MCMCConf = vm["mcconf"].as<string > ();
+                string FileOut = vm["rootfile"].as<string > ();
+                MonteCarlo MC(ModelConf, MCMCConf, FileOut, JobTag, checkTheoryRange);
+                MC.Run(rank);
+                if (vm.count("thRange")) checkTheoryRange = true;
+                
             } else {
-                throw runtime_error("Please specify mandatory Monte Carlo config filename\nor specify --noMC for Generate Event Mode (no Monte Carlo run)");
+                if (rank == 0){
+                    cout << coderun << endl;
+                    cout << desc << endl;
+                throw runtime_error("\nERROR: Please specify mandatory Monte Carlo config filename\nOR specify --noMC for Generate Event Mode (no Monte Carlo run)\n");
+                }
             }
             
-            JobTag = vm["job_tag"].as<string > ();
-
-            if (vm.count("thRange"))
-                checkTheoryRange = true;
 
         } catch(error& e) {
-            cerr << "ERROR: " << e.what() << std::endl << std::endl
+            if (rank == 0)
+                cout << coderun << endl;
+            cerr << "\nERROR: " << e.what() << std::endl << std::endl
                  << desc << std::endl;
             return EXIT_FAILURE;
         }
         
-        if (!noMC) {
-            MonteCarlo MC(ModelConf, MCMCConf, FileOut, JobTag, checkTheoryRange);
-            MC.Run(rank);
-        } else {
-            GenerateEvent GE(ModelConf, FolderOut, JobTag, noMC);
-            GE.generate(rank, nIterations);
-        }
-
 #ifdef _MPI
         MPI::Finalize();
 #endif
