4e09e51ea1b9da91281b3017adcd8829c77191d7
Ayan Paul <apaul2@alumni.nd.edu>
2014-03-09 22:02:30 +0100

    Minor bug removed.

--- Analysis/main_libmode_example.cpp
+++ Analysis/main_libmode_example.cpp
@@ -5,8 +5,11 @@ int main(int argc, char** argv)
 {
     try {
         
-        /* Define the model configuration file. */
-        string ModelConf = argv[1];
+        /* Define the model configuration file.                        */
+        /* Here it is passed as the first argument to the executable.  */
+        /* The model configuration file provides the default values of */
+        /* the mandatory model parameters.                             */
+        std::string ModelConf = argv[1];
         
         /* Define a map for the observables. */
         std::map<std::string, double> DObs;
@@ -23,9 +26,11 @@ int main(int argc, char** argv)
         ComputeObservables CO(ModelConf, DObs);
         
         for (int i = 0; i < 10; i++) {
+            
         /* Vary the parameters that need to be varied in the analysis. */
             DPars["Mz"] = 91.1875 + 0.0001 * i;
             DPars["AlsMz"] = 0.1184 + 0.000001 * i;
+            
             /* Get the map of observables with the parameter values defined above. */
             DObs = CO.compute(DPars);
         
@@ -39,8 +44,8 @@ int main(int argc, char** argv)
             }
         }      
         return EXIT_SUCCESS;
-    } catch (const runtime_error& e) {
-        cerr << e.what() << endl;
+    } catch (const std::runtime_error& e) {
+        std::cerr << e.what() << std::endl;
         return EXIT_FAILURE;
     }
 }
