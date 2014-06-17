d8747cd87b2fbcff7f0b54381e605078ffe215ef
Luca Silvestrini <Luca.Silvestrini@roma1.infn.it>
2014-06-17 14:42:41 +0200

    Reverted BParameter.h to previous version upon Ayan's kind request

--- StandardModel/src/BParameter.h
+++ StandardModel/src/BParameter.h
@@ -35,38 +35,23 @@ public:
      * @param[in] n dimension of the vector of bag parameters
      */
     BParameter(int n)
-    : bpars(new double[n])
+    : bpars(n,0.)
     {};
 
-    ~BParameter()
-    {
-       if(bpars != NULL) delete(bpars);
-    }
-    
     /**
      * @brief A get method for the vector of the bag parameters.
      * @return the vector of the bag parameters
      */
-    const double* getBpars() const
+    vector<double> getBpars() const
     {
         return bpars;
     }
 
     /**
-     * @brief A get method for the reference to a bag parameter.
-     * @param[in] i the index for the component of the vector of bag parameters
-     * @return the reference to the i-th bag parameter
-     */
-    const double& getBpar(int i) const
-    {
-        return bpars[i];
-    }
-    
-    /**
      * @brief A set method for a vector of the bag parameters.
      * @param[in] bpars a vector of the bag parameters
      */
-    void setBpars(double *bpars) 
+    void setBpars(vector<double> bpars) 
     {
         this->bpars = bpars;
     }
@@ -78,14 +63,14 @@ public:
      */
     void setBpars(int i, double value) 
     {
-        this->bpars[i] = value;
+        this->bpars(i) = value;
     }
 
     /**
      * @brief A get method for the scale of the bag parameters.
      * @return the scale at which the bag parameters are defined
      */
-    const double& getMu() const
+    double getMu() const
     {
         return mu;
     }
@@ -117,7 +102,7 @@ public:
     }
 
 private:
-    double *bpars;///< A vector of bag parameters.
+    vector<double> bpars;///< A vector of bag parameters.
     double mu;///< The scale at which the bag parameters are defined. 
     schemes scheme;///< The scheme in which the bag parameters are defined.
     
