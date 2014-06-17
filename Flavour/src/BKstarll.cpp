a1541c0042a527fd316568df6e566d932eb030c0
marco <marco@192.168.43.25>
2014-06-17 17:44:52 +0200

    B->K*ll added

---
+++ Flavour/src/BKstarll.cpp
@@ -0,0 +1,255 @@
+/* 
+ * File:   BKstarll.cpp
+ * Author: marco
+ * 
+ * Created on 16 giugno 2014, 15.30
+ */
+
+#include "BKstarll.h"
+#include <gslpp.h>
+#include <gslpp_complex.h>
+
+
+
+
+
+BKstarll::BKstarll(const StandardModel& SM_i) : ThObservable(SM_i), mySM(dynamic_cast<const StandardModel&> (SM_i)) {
+
+}
+
+
+BKstarll::~BKstarll() {
+}
+
+
+
+/*******************************************************************************
+ * Helicity amplitudes                                                         *
+ * ****************************************************************************/
+
+
+
+double BKstarll::N(){
+    return -(4*GF*MB*e*e*lambda_t)/(sqrt(2)*16*M_PI*M_PI);
+}
+
+
+/**
+ * polarization   i
+ *      0         0
+ *      +         1
+ *      -         2
+ */
+
+
+gslpp::complex BKstarll::H_V(int i) {
+    return -gslpp::complex::i()*N()*( C9*V_L(i) + C9p*V_R(i) + MB*MB/q2*( 2*Mb_MSB/MB*( C7*T_L(i) 
+            + C7p*T_R(i) ) - 16*M_PI*M_PI*h_lambda ) );
+}
+
+
+
+gslpp::complex BKstarll::H_A(int i) {
+    return -gslpp::complex::i()*N()*( C10*V_L(i) + C10p*V_R(i) );
+}
+
+
+
+gslpp::complex BKstarll::H_S() {
+    return gslpp::complex::i()*N()*Mb_MSB/MW*( CS*S_L + CSp*S_R );
+}
+
+
+
+gslpp::complex BKstarll::H_P() {
+    return gslpp::complex::i()*N()*( Mb_MSB/MW*( CS*S_L + CSp*S_R ) + 2*Mm*Mb_MSB/q2*( C10*( S_L - Ms/Mb*S_R ) + C10p*( S_R - Ms/Mb*S_L ) ) );
+}
+
+
+
+/*******************************************************************************
+ * Anguar coefficients                                                         *
+ * ****************************************************************************/
+
+
+
+double BKstarll::beta(double q2) {
+    return sqrt(1-4*Mm*Mm/q2);
+}
+
+
+
+double BKstarll::lambda(double k2) {
+    return 4*MB*MB*k2;
+}
+
+
+
+double BKstarll::F(double q2, double b) {
+    return sqrt(lambda(k2))*beta(q2)*q2*b/(96*M_PI*M_PI*M_PI*MB*MB*MB);   
+}
+
+
+
+double BKstarll::I(int i) {
+
+    double Mm2 = Mm*Mm;
+    double beta2 = beta(q2)*beta(q2);
+    
+    switch (i){
+        case '0':
+            return F(q2,b)*( ( H_V(0).abs2() + H_A(0).abs2() )/2  +  H_P().abs2()  +  2*Mm2/q2*( H_V(0).abs2() - H_A(0).abs2() )  +  
+                beta2*H_S().abs2() ); 
+        case '1':
+            return F(q2,b)*( (beta2 + 2)/8*( H_V(1).abs2() + H_V(2).abs2() + H_A(1).abs2() + H_A(2).abs2() )  +  
+                Mm2/q2*( H_V(1).abs2() + H_V(2).abs2() - H_A(1).abs2() - H_A(2).abs2() ) );
+        case '2':
+            return -F(q2,b)*beta2/2*( ( H_V(0).abs2() + H_A(0).abs2() )/2 );
+        case '3':
+            return F(q2,b)*beta2/8*( ( H_V(1).abs2() + H_V(2).abs2() )/2  +  ( H_A(1).abs2() + H_A(2).abs2() )/2 );
+        case '4':
+            return -F(q2,b)/2*( ( H_V(1)*H_V(2).conjugate() ).real()  +  ( H_A(1)*H_A(2).conjugate() ).real() );
+        case '5':
+            return F(q2,b)*beta2/4*( ( (H_V(2) + H_V(1))*H_V(0).conjugate() ).real()  +  ( (H_A(2) + H_A(1))*H_A(0).conjugate() ).real() );
+        case '6':
+            return F(q2,b)*( beta(q2)/2*( ( (H_V(2) - H_V(1))*H_A(0).conjugate() ).real()  +  ( (H_A(2) - H_A(1))*H_V(0).conjugate() ).real() )  -
+                beta(q2)*Mm/sqrt(q2)*( H_S().conjugate()*(H_V(1) + H_V(2)) ).real() );
+        case '7':
+            return F(q2,b)*beta(q2)*( H_V(2)*H_A(2).conjugate() - H_V(1)*H_A(1).conjugate() ).real();
+        case '8':
+            return 2*F(q2,b)*beta(q2)*Mm/sqrt(q2)*( H_S().conjugate()*H_V(0) ).real();
+        case '9':
+            return F(q2,b)*( beta(q2)/2*( ( (H_V(2) + H_V(1))*H_A(0).conjugate() ).imag()  +  ( (H_A(2) + H_A(1))*H_V(0).conjugate() ).imag() )  -
+                beta(q2)*Mm/sqrt(q2)*( H_S().conjugate()*(H_V(2) - H_V(1)) ).imag() );
+        case '10':
+            return F(q2,b)*beta2/4*( ( (H_V(2) - H_V(1))*H_V(0).conjugate() ).imag()  +  ( (H_A(2) - H_A(1))*H_A(0).conjugate() ).imag() );
+        case '11':
+            return F(q2,b)*beta2/2*( ( H_V(1)*H_V(2).conjugate() ).imag()  +  ( H_A(1)*H_A(2).conjugate() ).imag() );
+        default:
+            std::stringstream out;
+            out << i;
+            throw std::runtime_error("I: index " + out.str() + "not implemented");
+    }
+}
+
+
+
+double BKstarll::I_bar(int i) {
+    
+    if ( i==0 | i==1 | i==2 | i==3 | i==4 | i==5 | i==9)
+    {
+        return I(i);
+    }
+    else if ( i==6 | i==7 | i==8 | i==10 | i==11)
+    {
+        return -I(i);
+    }
+    else{
+        std::stringstream out;
+        out << i;
+        throw std::runtime_error("I: index " + out.str() + "not implemented");
+    }   
+}
+
+
+
+
+double BKstarll::Sigma(int i) {
+    return (I(i) + I_bar(i))/2;
+}
+
+
+
+/*******************************************************************************
+ * Observables                                                                 *
+ * ****************************************************************************/
+
+
+
+P_1::P_1(const StandardModel& SM_i) : BKstarll(SM_i) {  
+}
+
+double P_1::computeThValue() {
+    return Sigma(4)/(2.*Sigma(3));
+    
+}
+
+
+P_2::P_2(const StandardModel& SM_i) : BKstarll(SM_i) {  
+}
+
+double P_2::computeThValue() {
+    return Sigma(7)/(8.*Sigma(3));
+}
+
+
+P_3::P_3(const StandardModel& SM_i) : BKstarll(SM_i) {  
+}
+
+double P_3::computeThValue() {
+    return -Sigma(11)/(4.*Sigma(3));
+
+}
+
+
+P_4Prime::P_4Prime(const StandardModel& SM_i) : BKstarll(SM_i) {  
+}
+
+double P_4Prime::computeThValue() {
+    return Sigma(5)/sqrt(-Sigma(2)*Sigma(3));
+   
+}
+
+
+P_5Prime::P_5Prime(const StandardModel& SM_i) : BKstarll(SM_i) {  
+}
+
+double P_5Prime::computeThValue() {
+    return Sigma(6)/(2.*sqrt(-Sigma(2)*Sigma(3)));
+
+}
+
+
+P_6Prime::P_6Prime(const StandardModel& SM_i) : BKstarll(SM_i) {  
+}
+
+double P_6Prime::computeThValue() {
+    return -Sigma(9)/(2.*sqrt(-Sigma(2)*Sigma(3)));
+ 
+}
+
+
+GammaPrime::GammaPrime(const StandardModel& SM_i) : BKstarll(SM_i) {  
+}
+
+double GammaPrime::computeThValue() {
+    return ((3.*Sigma(0) - Sigma(2)) + 2.*(3*Sigma(1) - Sigma(3)))/4.;
+
+}
+
+
+F_L::F_L(const StandardModel& SM_i) : BKstarll(SM_i), mySM(SM_i) {  
+}
+
+double F_L::computeThValue() {
+    GammaPrime myGammaPrime(mySM);
+    return (3.*Sigma(0) - Sigma(2))/(4.*myGammaPrime.computeThValue());
+
+}
+
+
+M_1Prime::M_1Prime(const StandardModel& SM_i) : BKstarll(SM_i) {  
+}
+
+double M_1Prime::computeThValue() {
+    return ( H_V(1).abs2() + H_V(2).abs2() - H_A(1).abs2() - H_A(2).abs2() )/( 2*( H_V(1).abs2() + H_V(2).abs2() + H_A(1).abs2() + H_A(2).abs2() ) );
+  
+}
+
+
+M_2Prime::M_2Prime(const StandardModel& SM_i) : BKstarll(SM_i) {  
+}
+
+double M_2Prime::computeThValue() {
+    return ( q2/(2*Mm*Mm)*( H_P().abs2() + beta(q2)*beta(q2)*H_S().abs2() ) + H_V(0).abs2() - H_A(0).abs2() )/( H_V(0).abs2() + H_A(0).abs2() );  
+}
---
+++ Flavour/src/BKstarll.h
@@ -0,0 +1,444 @@
+/* 
+ * File:   BKstarll.h
+ * Author: marco
+ *
+ * Created on 16 giugno 2014, 15.30
+ */
+
+#ifndef BKSTARLL_H
+#define	BKSTARLL_H
+
+#include <math.h>
+#include "StandardModelMatching.h"
+#include <StandardModel.h>
+#include <ThObservable.h>
+
+
+/**
+ * @class BKstarll
+ * @ingroup flavour
+ * @brief A class for the decay B -> K^*ll. 
+ * @author SusyFit Collaboration
+ * @copyright GNU General Public License
+ * @details 
+ */
+class BKstarll : public ThObservable {
+public:
+    BKstarll(const StandardModel& SM_i);
+    virtual ~BKstarll();
+    virtual double computeThValue()=0;
+        /*dummy variables*/
+    double GF;            //Fermi constant
+    double e;             //electron charge
+    double Mm;            //muon mass
+    double MB;            //B meson mass
+    double Mb_MSB;        //b quark mass defined in the MSbar scheme
+    double Mb;            //b quark mass
+    double Ms;            //s quark mass
+    double MW;            //W boson mass
+    double lambda_t;      //Vckm factor
+    double h_lambda;      //parameter that contains the contribution from the hadronic hamiltonian  
+    double q2;            //q^2 of the decay
+    double k2;            //square of the 3-momentum k
+    double b;             //BF of the decay K^* -> K pi
+    double C7,C9,C7p,C9p; //WC
+    double C10,C10p;      //WC
+    double CS,CSp;        //WC
+    
+    
+    
+    
+    
+    /**
+    * @brief \f$ V_L \f$ ; NOT CODED, SHOULD BE ADDED AS INPUT FROM LATTICE
+    * @param[in] i polarization
+    * @return return the matrix element V_L(lambda)
+    */
+    double V_L(int i);
+
+    
+    /**
+    * @brief \f$ V_L \f$ ; NOT CODED, SHOULD BE ADDED AS INPUT FROM LATTICE
+    * @param[in] i polarization
+    * @return return the matrix element V_R(lambda)
+    */
+    double V_R(int i);
+
+
+    /**
+    * @brief \f$ V_L \f$ ; NOT CODED, SHOULD BE ADDED AS INPUT FROM LATTICE
+    * @param[in] i polarization
+    * @return return the matrix element T_L(lambda)
+    */
+    double T_L(int i);
+
+
+    /**
+    * @brief \f$ V_L \f$ ; NOT CODED, SHOULD BE ADDED AS INPUT FROM LATTICE 
+    * @param[in] i polarization
+    * @return return the matrix element T_R(lambda)
+    */
+    double T_R(int i);
+
+
+    /**
+    * 
+    * @brief \f$ V_L \f$ ; NOT CODED, SHOULD BE ADDED AS INPUT FROM LATTICE 
+    * @return return the matrix element S_L
+    */
+    double S_L;
+
+
+    /**
+    * 
+    * @brief \f$ V_L \f$ ; NOT CODED, SHOULD BE ADDED AS INPUT FROM LATTICE 
+    * @return return the matrix element S_R
+    */
+    double S_R;
+    
+    
+    /**
+    * @brief \f$ N \f$ 
+    * @return return the helicity amplitude normalization factor N
+    */
+    double N();
+    
+    
+    /**
+    * @brief \f$ H_V \f$ 
+    * @param[in] i polarization lambda
+    * @return return the helicity amplitude H_V(lambda)
+    */
+    gslpp::complex H_V(int i);
+
+
+    /**
+    * @brief \f$ H_A \f$ 
+    * @param[in] i polarization lambda
+    * @return return the helicity amplitude H_A(lambda)
+    */
+    gslpp::complex H_A(int i);
+
+
+    /**
+    * @brief \f$ H_S \f$ 
+    * @return return the helicity amplitude H_S
+    */
+    gslpp::complex H_S();
+
+
+    /**
+    * @brief \f$ H_P \f$ 
+    * @return return the helicity amplitude H_P
+    */
+    gslpp::complex H_P();
+    
+    
+    /**
+    * @brief \f$ beta \f$ 
+    * @param[in] q2 q^2 of the decay
+    * @return return the factor beta used in the angular coefficients I_i
+    */
+    double beta (double q2);
+    
+    
+    /**
+    * @brief \f$ lambda \f$ 
+    * @param[in] k2 square of the 3-momentum k of the recoiling meson in the B rest frame
+    * @return return the factor lambda used in the angular coefficients I_i
+    */
+    double lambda(double k2);
+
+    
+    /**
+    * @brief \f$ F \f$ 
+    * @param[in] q2 q^2 of the decay
+    * @param[in] b BF of the decay K* -> K pi
+    * @return return the factor F used in the angular coefficients I_i
+    */
+    double F(double q2, double b);
+    
+    
+    /**
+    * i values:
+    * 0 = 1c
+    * 1 = 1s
+    * 2 = 2c
+    * 3 = 2s
+    * 4 = 3
+    * 5 = 4
+    * 6 = 5
+    * 7 = 6s
+    * 8 = 6c
+    * 9 = 7
+    * 10 = 8
+    * 11 = 9
+    */
+    
+    
+    /**
+    * @brief \f$ I_{i} \f$ 
+    * @param[in] i index of the angular coefficient
+    * @return return the angular coefficient I_i
+    */
+    double  I(int i);
+    
+    
+    /**
+    * @brief \f$ I_{i} \f$ 
+    * @param[in] i index of the angular coefficient
+    * @return return the angular coefficient \bar{I}_i
+    */
+    double I_bar(int i);
+    
+    
+    /**
+    * @brief \f$ Sigma_{i} \f$ 
+    * @param[in] i index of the angular coefficient I_i
+    * @return return the CP avarage Sigma_i
+    */
+    double Sigma(int i);
+
+    
+
+private:
+    const StandardModel& mySM;
+
+};
+
+
+/**
+ * @class BKstarll
+ * @ingroup flavour
+ * @brief A class for the clean observable P_1. 
+ * @author SusyFit Collaboration
+ * @copyright GNU General Public License
+ * @details 
+ */
+class P_1 : public BKstarll{
+public:
+    
+    /**
+    * @brief \f$ P_{1} \f$ 
+    */
+    P_1(const StandardModel& SM_i);
+    
+    /**
+    * @return return the clean observable P_1
+    */
+    double computeThValue ();
+};
+
+
+/**
+ * @class BKstarll
+ * @ingroup flavour
+ * @brief A class for the clean observable P_2. 
+ * @author SusyFit Collaboration
+ * @copyright GNU General Public License
+ * @details 
+ */
+class P_2 : public BKstarll{
+public:
+    
+    /**
+    * @brief \f$ P_{2} \f$ 
+    */
+    P_2(const StandardModel& SM_i);
+    
+    /**
+    * @return return the clean observable P_2
+    */
+    double computeThValue ();
+};
+
+
+/**
+ * @class BKstarll
+ * @ingroup flavour
+ * @brief A class for the clean observable P_3. 
+ * @author SusyFit Collaboration
+ * @copyright GNU General Public License
+ * @details 
+ */
+class P_3 : public BKstarll{
+public:
+    
+    /**
+    * @brief \f$ P_{3} \f$ 
+    */
+    P_3(const StandardModel& SM_i);
+    
+    /**
+    * @return return the clean observable P_3
+    */
+    double computeThValue ();
+};
+
+
+/**
+ * @class BKstarll
+ * @ingroup flavour
+ * @brief A class for the clean observable P'_4. 
+ * @author SusyFit Collaboration
+ * @copyright GNU General Public License
+ * @details 
+ */
+class P_4Prime : public BKstarll{
+public:
+    
+    /**
+    * @brief \f$ P'_{4} \f$ 
+    */
+    P_4Prime(const StandardModel& SM_i);
+    
+    /**
+    * @return return the clean observable P'_4
+    */
+    double computeThValue ();
+};
+
+
+/**
+ * @class BKstarll
+ * @ingroup flavour
+ * @brief A class for the clean observable P'_5. 
+ * @author SusyFit Collaboration
+ * @copyright GNU General Public License
+ * @details 
+ */
+class P_5Prime : public BKstarll{
+public:
+    
+    /**
+    * @brief \f$ P'_{5} \f$ 
+    */
+    P_5Prime(const StandardModel& SM_i);
+    
+    /**
+    * @return return the clean observable P'_5
+    */
+    double computeThValue ();
+};
+
+
+/**
+ * @class BKstarll
+ * @ingroup flavour
+ * @brief A class for the clean observable P'_6. 
+ * @author SusyFit Collaboration
+ * @copyright GNU General Public License
+ * @details 
+ */
+class P_6Prime : public BKstarll{
+public:
+    
+    /**
+    * @brief \f$ P'_{6} \f$ 
+    */
+    P_6Prime(const StandardModel& SM_i);
+    
+    /**
+    * @return return the clean observable P'_6
+    */
+    double computeThValue ();
+};
+
+
+/**
+ * @class BKstarll
+ * @ingroup flavour
+ * @brief A class for the clean observable Gamma'. 
+ * @author SusyFit Collaboration
+ * @copyright GNU General Public License
+ * @details 
+ */
+class GammaPrime : public BKstarll{
+public:
+    
+    /**
+    * @brief \f$ Gamma' \f$ 
+    */
+    GammaPrime(const StandardModel& SM_i);
+    
+    /**
+    * @return return the clean observable Gamma'
+    */
+    double computeThValue ();
+};
+
+
+/**
+ * @class BKstarll
+ * @ingroup flavour
+ * @brief A class for the clean observable F_L. 
+ * @author SusyFit Collaboration
+ * @copyright GNU General Public License
+ * @details 
+ */
+class F_L : public BKstarll{
+public:
+    
+    /**
+    * @brief \f$ F_L \f$ 
+    */
+    F_L(const StandardModel& SM_i);
+    
+    /**
+    * @return return the clean observable F_L
+    */
+    double computeThValue ();
+    
+    const StandardModel& mySM;
+
+};
+
+
+/**
+ * @class BKstarll
+ * @ingroup flavour
+ * @brief A class for the clean observable M'_1. 
+ * @author SusyFit Collaboration
+ * @copyright GNU General Public License
+ * @details 
+ */
+class M_1Prime : public BKstarll{
+public:
+    
+    /**
+    * @brief \f$ M'_1 \f$ 
+    */
+    M_1Prime(const StandardModel& SM_i);
+    
+    /**
+    * @return return the clean observable M'_1
+    */
+    double computeThValue ();
+};
+
+
+/**
+ * @class BKstarll
+ * @ingroup flavour
+ * @brief A class for the clean observable M'_2. 
+ * @author SusyFit Collaboration
+ * @copyright GNU General Public License
+ * @details 
+ */
+class M_2Prime : public BKstarll{
+public:
+    
+    /**
+    * @brief \f$ M'_2 \f$ 
+    */
+    M_2Prime(const StandardModel& SM_i);
+    
+    /**
+    * @return return the clean observable M'_2
+    */
+    double computeThValue ();
+};
+
+
+#endif	/* BKSTARLL_H */
+
