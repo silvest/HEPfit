d7e59af92ddbca3d1b91e9a5402ef03e2c87a622
SatoshiMishima <mishima@kias.re.kr>
2015-04-23 01:28:16 +0200

    Doxygen files are copied into the package.

--- Packaging/makePackage.sh
+++ Packaging/makePackage.sh
@@ -60,8 +60,7 @@ if [ ! -d "$OUTDIR" ]; then
     echo "mkdir $OUTDIR"
     mkdir $OUTDIR
 fi
-SRCDIR="${OUTDIR}"
-if [ "$ORGDIR" = "$SRCDIR" ]; then
+if [ "$ORGDIR" = "$OUTDIR" ]; then
     echo "The target directory has to differ from the original directory!"
     exit 1
 fi
@@ -76,38 +75,38 @@ fi
 
 for PROJECT in ${PROJECTARRAY[@]}
 do
-    if [ ! -d "${SRCDIR}/${PROJECT}" ]; then
-        echo "midir ${SRCDIR}/${PROJECT}"
-        mkdir ${SRCDIR}/${PROJECT}
+    if [ ! -d "${OUTDIR}/${PROJECT}" ]; then
+        echo "midir ${OUTDIR}/${PROJECT}"
+        mkdir ${OUTDIR}/${PROJECT}
     fi
 
     if [ "$PROJECT" = "Analysis" ]; then
-        cp -af ${ORGDIR}/${PROJECT}/main.cpp ${SRCDIR}/${PROJECT}/
+        cp -af ${ORGDIR}/${PROJECT}/main.cpp ${OUTDIR}/${PROJECT}/
     else
-        cp -af ${ORGDIR}/${PROJECT}/src ${SRCDIR}/${PROJECT}/
+        cp -af ${ORGDIR}/${PROJECT}/src ${OUTDIR}/${PROJECT}/
     fi
 done
 
-if [ ! -d "${SRCDIR}/doc" ]; then
-    echo "mkdir ${SRCDIR}/doc"
-    mkdir ${SRCDIR}/doc
+if [ ! -d "${OUTDIR}/doc" ]; then
+    echo "mkdir ${OUTDIR}/doc"
+    mkdir ${OUTDIR}/doc
 fi
-cp -af ${ORGDIR}/doc/COPYING ${SRCDIR}/doc/
-cp -af ${ORGDIR}/doc/LICENSE ${SRCDIR}/doc/
+cp -af ${ORGDIR}/doc/COPYING ${OUTDIR}/doc/
+cp -af ${ORGDIR}/doc/LICENSE ${OUTDIR}/doc/
 
 ###########################################################
 # remove unnecessary files
 
-rm -f ${SRCDIR}/StandardModel/src/EWSMTwoFermionsLEP2.h
-rm -f ${SRCDIR}/StandardModel/src/EWSMTwoFermionsLEP2.cpp
-rm -f ${SRCDIR}/StandardModel/src/EWSM_Output.h
-rm -f ${SRCDIR}/StandardModel/src/EWSM_Output.cpp
-rm -f ${SRCDIR}/EW/src/LEP2*
+rm -f ${OUTDIR}/StandardModel/src/EWSMTwoFermionsLEP2.h
+rm -f ${OUTDIR}/StandardModel/src/EWSMTwoFermionsLEP2.cpp
+rm -f ${OUTDIR}/StandardModel/src/EWSM_Output.h
+rm -f ${OUTDIR}/StandardModel/src/EWSM_Output.cpp
+rm -f ${OUTDIR}/EW/src/LEP2*
 
 #FLAVORFILES="AmpDD2.cpp AmpDD2.h AmpDS1.cpp AmpDS1.h ArgD.h BR_Bdnunu.cpp BR_Bdnunu.h BR_Bsnunu.cpp BR_Bsnunu.h BR_Kmumu.cpp BR_Kmumu.h BR_Kp0nunu.cpp BR_Kp0nunu.h BR_Kppnunu.cpp BR_Kppnunu.h CPenguinBox.cpp CPenguinBox.h CPenguinBoxMu.cpp CPenguinBoxMu.h Charm_Kpnunu.cpp Charm_Kpnunu.h EpsilonP_O_Epsilon.cpp EpsilonP_O_Epsilon.h EvolDC1.cpp EvolDC1.h EvolDC1Buras.cpp EvolDC1Buras.h EvolDF1nlep.cpp EvolDF1nlep.h HeffDC1.cpp HeffDC1.h HeffDF1bnlep.cpp HeffDF1bnlep.h HeffDS1.cpp HeffDS1.h M12D.h"
 #for FLAVOR in $FLAVORFILES
 #do
-#    rm -f ${SRCDIR}/Flavour/src/${FLAVOR}
+#    rm -f ${OUTDIR}/Flavour/src/${FLAVOR}
 #done
 
 ###########################################################
@@ -122,7 +121,7 @@ do
     fi
 
     echo "generating ${PROJECT}/CMakeLists.txt"
-    python3 $PYSCRIPT $PROJECT $CONF > ${SRCDIR}/${PROJECT}/CMakeLists.txt
+    python3 $PYSCRIPT $PROJECT $CONF > ${OUTDIR}/${PROJECT}/CMakeLists.txt
 done
 
 ###########################################################
@@ -133,20 +132,39 @@ SED_ARG="-e 's/VERSIONNUMBER/${VERSION}/g'"
 eval sed "$SED_ARG" ${SCRIPTPATH}/etc/INSTALL > ${OUTDIR}/INSTALL
 eval sed "$SED_ARG" ${SCRIPTPATH}/etc/README.md > ${OUTDIR}/README.md
 
-cp ${SCRIPTPATH}/etc/CMakeLists.txt ${SRCDIR}/
-cp ${SCRIPTPATH}/etc/SusyFit_noMCMC.h.in ${SRCDIR}/
-cp ${SCRIPTPATH}/etc/BAT_make_wrapper.sh.in ${SRCDIR}/
-cp ${SCRIPTPATH}/etc/cmake_uninstall.cmake.in ${SRCDIR}/
-cp ${SCRIPTPATH}/etc/susyfit-config.in ${SRCDIR}/
-eval sed "$SED_ARG" ${SCRIPTPATH}/etc/SusyFit.pc.in > ${SRCDIR}/SusyFit.pc.in
-cp ${SCRIPTPATH}/etc/SusyFit.h.in ${SRCDIR}/
-cp ${SCRIPTPATH}/etc/BAT_mpi_patch.txt ${SRCDIR}/
+cp ${SCRIPTPATH}/etc/CMakeLists.txt ${OUTDIR}/
+cp ${SCRIPTPATH}/etc/SusyFit_noMCMC.h.in ${OUTDIR}/
+cp ${SCRIPTPATH}/etc/BAT_make_wrapper.sh.in ${OUTDIR}/
+cp ${SCRIPTPATH}/etc/cmake_uninstall.cmake.in ${OUTDIR}/
+cp ${SCRIPTPATH}/etc/susyfit-config.in ${OUTDIR}/
+eval sed "$SED_ARG" ${SCRIPTPATH}/etc/SusyFit.pc.in > ${OUTDIR}/SusyFit.pc.in
+cp ${SCRIPTPATH}/etc/SusyFit.h.in ${OUTDIR}/
+cp ${SCRIPTPATH}/etc/BAT_mpi_patch.txt ${OUTDIR}/
 
 cp -r ${SCRIPTPATH}/etc/examples-src/LibMode_config ${OUTDIR}/examples-src/
 cp -r ${SCRIPTPATH}/etc/examples-src/LibMode_header ${OUTDIR}/examples-src/
 cp -r ${SCRIPTPATH}/etc/examples-src/MonteCarloMode ${OUTDIR}/examples-src/
 cp -r ${SCRIPTPATH}/etc/examples-src/config ${OUTDIR}/examples-src/
 cp -r ${SCRIPTPATH}/etc/examples-src/config/input ${OUTDIR}/examples-src/config/
+
+###########################################################
+# copy Doxygen files
+
+DOXYGENDIR=${OUTDIR}/Doxygen
+if [ ! -d "${DOXYGENDIR}" ]; then
+    echo "mkdir ${DOXYGENDIR}"
+    mkdir ${DOXYGENDIR}
+fi
+if [ ! -d "${DOXYGENDIR}/images" ]; then
+    echo "mkdir ${DOXYGENDIR}/images"
+    mkdir ${DOXYGENDIR}/images
+fi
+DOXYFILELIST="Doxyfile DoxygenLayout.xml customdoxygen.css footer.html header.html Installation.md MainPage.md Models.md Usage.md EW.bib QCD.bib bibconversion.pl"
+for DOXYFILE in $DOXYFILELIST
+do
+    cp -af ${ORGDIR}/Doxygen/${DOXYFILE} ${DOXYGENDIR}/
+done
+cp -af ${ORGDIR}/Doxygen/images/Model_inherit_graph.svg ${DOXYGENDIR}/images/
 
 ###########################################################
 # Example main files
@@ -163,7 +181,7 @@ eval sed "$SED_ARG" ${ORGFILE} > ${OUTDIR}/examples-src/MonteCarloMode/MCMC.cpp
 ###########################################################
 # Patch
 
-sh ${SCRIPTPATH}/mod.sh ${SRCDIR}/InputParser/src/ModelFactory.cpp ${SRCDIR}/EW/src/EWObservables.h ${SRCDIR}/InputParser/src/ThObsFactory.cpp ${SRCDIR}/StandardModel/src/StandardModel.cpp ${SRCDIR}/StandardModel/src/StandardModel.h
+sh ${SCRIPTPATH}/mod.sh ${OUTDIR}/InputParser/src/ModelFactory.cpp ${OUTDIR}/EW/src/EWObservables.h ${OUTDIR}/InputParser/src/ThObsFactory.cpp ${OUTDIR}/StandardModel/src/StandardModel.cpp ${OUTDIR}/StandardModel/src/StandardModel.h
 echo "applying patches to souce codes..."
 
 ###########################################################
