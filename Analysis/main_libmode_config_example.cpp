81244e2a5a788180f71fa821d26c2d5e8b6720e7
SatoshiMishima <mishima@kias.re.kr>
2015-04-25 01:40:08 +0200

    Added the copyright header to main_libmode_config_example.cpp.

--- Analysis/main_libmode_config_example.cpp
+++ Analysis/main_libmode_config_example.cpp
@@ -1,3 +1,10 @@
+/*
+ * Copyright (C) 2014 SusyFit Collaboration
+ * All rights reserved.
+ *
+ * For the licensing terms see doc/COPYING.
+ */
+
 #include <iostream>
 #include <ComputeObservables.h>
 
