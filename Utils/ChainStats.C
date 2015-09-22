f29416be2c1d620e058e1eceb601f474a8b4b7e3
Luca Silvestrini <Luca.Silvestrini@roma1.infn.it>
2015-09-22 18:31:12 +0200

    Added root macro to plot the single chain results from a root file

---
+++ Utils/ChainStats.C
@@ -0,0 +1,48 @@
+#include "TMath.h"
+
+void ChainStats(TString ifile = "MCout", int nchains)
+{
+  gROOT->Reset();
+  gROOT->SetStyle("Plain");
+
+  TCanvas * c = new TCanvas("c","ChainStats",1000,1000);
+  //  c->SetFillColor(18);
+  //  c->Divide(2,3);
+  gStyle->SetOptStat("");
+
+  TFile file(ifile+".root","read");
+  TNtupleD *nt0 = file.Get("AnalysisTree");
+  TH1D *tmp = new TH1D("tmp","tmp",50, 0., 1000.);
+
+  nt0->Project("tmp","fNParameters");
+  long int npars = (long int) tmp->GetMean();
+  double ss = TMath::Sqrt(tmp->GetMean());
+  int nx = TMath::FloorNint(ss);
+  int ny = TMath::CeilNint((double) npars/nx);
+
+  c->Divide(nx,ny);
+
+  TNtupleD * nt;
+  TGraphErrors tge[npars];
+  
+  for (Int_t i = 0; i < npars; i++)
+    {
+      for (Int_t j = 0; j < nchains; j++)
+	{
+	  tmp->Reset("M");
+	  TString ntp = Form("MarkovChainTree_%d",j);
+	  nt = (TNtupleD * ) file.Get(ntp);
+	  nt->Project("tmp",Form("Parameter%d",i));
+	  tge[i].SetPoint(j,j+1,tmp->GetMean());
+	  tge[i].SetPointError(j,0.,tmp->GetRMS());
+	}
+      tge[i].SetTitle(Form("Parameter%d",i));
+      c->cd(i+1);
+      tge[i].Draw();
+    }
+
+  c->cd();
+  c->SaveAs(ifile+"_chains.pdf");
+
+  file.Close();
+}
