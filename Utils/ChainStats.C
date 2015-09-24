c9a773b8bbbf344075347c84aba51d84c34f5fa0
Luca Silvestrini <Luca.Silvestrini@roma1.infn.it>
2015-09-24 13:00:38 +0200

    Minor improvements

--- Utils/ChainStats.C
+++ Utils/ChainStats.C
@@ -21,12 +21,19 @@ void ChainStats(TString ifile = "MCout", int nchains)
   int ny = TMath::CeilNint((double) npars/nx);
 
   c->Divide(nx,ny);
-
+  
   TNtupleD * nt;
   TGraphErrors tge[npars];
-  
+  TGraph tg[npars*nchains];
+  Double_t x;
+
+  TCanvas * c2 = new TCanvas("c2","TracePlot",1000,1000);
+  c2->Divide(TMath::FloorNint(TMath::Sqrt(nchains)),TMath::CeilNint((double) nchains/TMath::FloorNint(TMath::Sqrt(nchains))));
+
+
   for (Int_t i = 0; i < npars; i++)
     {
+      c2->SetTitle(Form("TracePlot%d",i));
       for (Int_t j = 0; j < nchains; j++)
 	{
 	  TString ntp = Form("MarkovChainTree_%d",j);
@@ -37,7 +44,26 @@ void ChainStats(TString ifile = "MCout", int nchains)
 	  tge[i].SetPoint(j,j+1,histo->GetMean());
 	  tge[i].SetPointError(j,0.,histo->GetRMS());
 	  histo->Delete();
+	  nt->SetBranchAddress(npar,&x);
+	  for(Int_t k = 0; k < nt->GetEntries(); k++){
+	    nt->GetEntry(k);
+	    tg[i*nchains+j].SetPoint(k,k,x);
+	  }
+	  tg[i*nchains+j].SetTitle(Form("Chain%d",j));
+	  c2->cd(j+1);
+	  tg[i*nchains+j].Draw("al");
 	}
+      c2->cd();
+      switch(i){
+      case 0:
+	c2->Print(ifile+"_traceplots.pdf(","pdf");
+	break;
+      case (npars-1):
+	c2->Print(ifile+"_traceplots.pdf)","pdf");
+	break;
+      default:
+	c2->Print(ifile+"_traceplots.pdf","pdf");
+      }
       tge[i].SetTitle(Form("Parameter%d",i));
       c->cd(i+1);
       tge[i].Draw("ap");
