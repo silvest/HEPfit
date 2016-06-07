105a984b87402c0131356e0eac72e38daf553097
Ayan Paul <apaul2@alumni.nd.edu>
2016-06-07 13:09:16 +0200

    added a compiler independent way of initializing a list of elements

--- InputParser/src/ThObsFactory.h
+++ InputParser/src/ThObsFactory.h
@@ -13,6 +13,23 @@
 #include <boost/function.hpp>
 #include <map>
 
+template <typename T>
+class make_vector {
+public:
+    typedef make_vector<T> my_type;
+
+    my_type& operator<<(const T& val) {
+        data_.push_back(val);
+        return *this;
+    }
+
+    operator std::vector<T>() const {
+        return data_;
+    }
+private:
+    std::vector<T> data_;
+};
+
 class ThObservable;
 
 /**
