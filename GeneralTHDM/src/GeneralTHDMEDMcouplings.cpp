733922ad51edae678c9d6789ec717fe952d090ae
Ayan Paul <apaul2@alumni.nd.edu>
2017-02-06 12:42:44 +0100

    EDM for GeneralTHDM

---
+++ GeneralTHDM/src/GeneralTHDMEDMcouplings.cpp
@@ -0,0 +1,26 @@
+/* 
+ * Copyright (C) 2017 HEPfit Collaboration
+ *
+ *
+ * For the licensing terms see doc/COPYING.
+ */
+
+#include "GeneralTHDMEDMcouplings.h"
+#include "GeneralTHDM.h"
+
+GeneralTHDMEDMcouplings::GeneralTHDMEDMcouplings(const GeneralTHDM& GeneralTHDM_i) 
+: EffectiveEDMcouplings(GeneralTHDM_i), myGeneralTHDM(GeneralTHDM_i)
+{}
+
+GeneralTHDMEDMcouplings::~GeneralTHDMEDMcouplings() 
+{}
+
+double GeneralTHDMEDMcouplings::kappa_t() 
+{
+    return 0.;
+}
+
+double GeneralTHDMEDMcouplings::kappa_b() 
+{
+    return 0.;
+}
