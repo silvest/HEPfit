bdd7b3820488a463800fc5eceaec40118aec22ea
Ayan Paul <apaul2@alumni.nd.edu>
2017-05-25 00:52:42 +0200

    minor correction to header inclusion

--- StandardModel/src/QCD.cpp
+++ StandardModel/src/QCD.cpp
@@ -14,7 +14,7 @@
 #include <gsl/gsl_errno.h>
 #include <gsl/gsl_math.h>
 #include <gsl/gsl_roots.h>
-#include <gsl/gsl_sf.h>
+#include <gsl/gsl_sf_zeta.h>
 #include <TF1.h>
 #include <Math/WrappedTF1.h>
 #include <Math/BrentRootFinder.h>
