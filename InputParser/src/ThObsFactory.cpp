8a2cfcb342db16155d9763c001ec92d1ce32abc8
MarcoFedele <marco.fedele@uniroma1.it>
2018-03-09 15:14:32 +0100

    Observables generalized for Bsll

--- InputParser/src/ThObsFactory.cpp
+++ InputParser/src/ThObsFactory.cpp
@@ -289,10 +289,14 @@ ThObsFactory::ThObsFactory()
     obsThFactory["BRbar_Bdmumu"] = boost::bind(boost::factory<Bdmumu*>(), _1, 2);
     obsThFactory["Amumu_Bd"] = boost::bind(boost::factory<Bdmumu*>(), _1, 3);
     obsThFactory["Smumu_Bd"] = boost::bind(boost::factory<Bdmumu*>(), _1, 4);
-    obsThFactory["BR_Bsmumu"] = boost::bind(boost::factory<Bsmumu*>(), _1, 1);
-    obsThFactory["BRbar_Bsmumu"] = boost::bind(boost::factory<Bsmumu*>(), _1, 2);
-    obsThFactory["Amumu_Bs"] = boost::bind(boost::factory<Bsmumu*>(), _1, 3);
-    obsThFactory["Smumu_Bs"] = boost::bind(boost::factory<Bsmumu*>(), _1, 4);
+    obsThFactory["BR_Bsmumu"] = boost::bind(boost::factory<Bsmumu*>(), _1, 1, StandardModel::MU);
+    obsThFactory["BRbar_Bsmumu"] = boost::bind(boost::factory<Bsmumu*>(), _1, 2, StandardModel::MU);
+    obsThFactory["Amumu_Bs"] = boost::bind(boost::factory<Bsmumu*>(), _1, 3, StandardModel::MU);
+    obsThFactory["Smumu_Bs"] = boost::bind(boost::factory<Bsmumu*>(), _1, 4, StandardModel::MU);
+    obsThFactory["BR_Bsee"] = boost::bind(boost::factory<Bsmumu*>(), _1, 1, StandardModel::ELECTRON);
+    obsThFactory["BRbar_Bsee"] = boost::bind(boost::factory<Bsmumu*>(), _1, 2, StandardModel::ELECTRON);
+    obsThFactory["Aee_Bs"] = boost::bind(boost::factory<Bsmumu*>(), _1, 3, StandardModel::ELECTRON);
+    obsThFactory["See_Bs"] = boost::bind(boost::factory<Bsmumu*>(), _1, 4, StandardModel::ELECTRON);
     obsThFactory["BR_BdmumuOBR_Bsmumu"] = boost::factory<BdmumuOBsmumu*>();
    //----- b to q gamma  -----
     obsThFactory["BR_bsgamma"] = boost::bind(boost::factory<Bsgamma*>(), _1, StandardModel::STRANGE, 1);
