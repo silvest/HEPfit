7b5a67146dc5c03d937c28d467e1ad3b2fde9e08
Ayan Paul <apaul2@alumni.nd.edu>
2018-11-23 13:11:08 +0100

    added correlations to Higgs Observables.

--- Observables/src/HiggsObservable.cpp
+++ Observables/src/HiggsObservable.cpp
@@ -17,6 +17,27 @@
 #include <string>
 #include <sstream>
 
+HiggsObservable::HiggsObservable(const Observable& Obs) :
+Observable(Obs)
+{
+    isnew = true;
+    isCorrelated = false;
+    covarianceFromConfig = false;
+};
+
+HiggsObservable::HiggsObservable(const HiggsObservable& orig)
+{
+    channels = orig.channels;
+    thObsV = orig.thObsV;
+    thobsvsize = orig.thobsvsize;
+    channelsize = orig.channelsize;
+    isnew = orig.isnew;
+    isCorrelated = orig.isCorrelated;
+    covarianceFromConfig = orig.covarianceFromConfig;
+    InvCov = orig.InvCov;
+    rank = orig.rank;
+}
+
 //HiggsObservable::~HiggsObservable() {}
 
 void HiggsObservable::setParametricLikelihood(std::string filename, std::vector<ThObservable*> thObsV)
@@ -28,6 +49,7 @@ void HiggsObservable::setParametricLikelihood(std::string filename, std::vector<
         throw std::runtime_error("\nERROR: " + filename + " does not exist. Make sure to specify a valid Higgs parameters configuration file.\n");
     std::string line;
     bool IsEOF;
+    bool readCorrelations = false;
     int i = 0, nrows = 0;
     do {
         IsEOF = getline(ifile, line).eof();
@@ -42,26 +64,80 @@ void HiggsObservable::setParametricLikelihood(std::string filename, std::vector<
     channels.ResizeTo(nrows, thObsV.size() + 3 + implicit_tth);
     ifile.clear();
     ifile.seekg(0, ifile.beg);
+    int lineNo = 0;
     do {
         IsEOF = getline(ifile, line).eof();
+        lineNo++;
         if (*line.rbegin() == '\r') line.erase(line.length() - 1); // for CR+LF
-        if (line.compare(0, 11, "MEASUREMENT") != 0)
+        if (line.compare(0, 11, "MEASUREMENT") != 0 && !readCorrelations) {
             continue;
-        boost::char_separator<char> sep(" |\t");
-        boost::tokenizer<boost::char_separator<char> > tok(line, sep);
-        boost::tokenizer<boost::char_separator<char> >::iterator beg = tok.begin();
-        // Read the necessary information from the config file. Each row contains:
-        // ggH fraction
-        // VBF fraction
-        // VH fraction (ttH is computed as 1-ggH-VBF-VH)
-        // average value of mu
-        // left-side error
-        // right-side error
-        beg++; // skip label
-        for (int j = 0; j < channels.GetNcols(); j++) 
-            channels(i, j) = atof((*(++beg)).c_str());
-        i++;
-
+        } else {
+            boost::char_separator<char> sep(" |\t");
+            boost::tokenizer<boost::char_separator<char> > tok(line, sep);
+            boost::tokenizer<boost::char_separator<char> >::iterator beg = tok.begin();
+            if (isCorrelated) readCorrelations = true;
+            // Read the necessary information from the config file. Each row contains:
+            // ggH fraction
+            // VBF fraction
+            // VH fraction (ttH is computed as 1-ggH-VBF-VH)
+            // average value of mu
+            // left-side error
+            // right-side error
+            beg++; // skip label
+            for (int j = 0; j < channels.GetNcols(); j++)
+                channels(i, j) = atof((*(++beg)).c_str());
+            if (isCorrelated && (std::fabs(channels(i, thObsV.size() + 1)) != std::fabs(channels(i, thObsV.size() + 2)))) {
+                if (rank == 0) throw std::runtime_error("ERROR: Gaussian errors must be symmetric for CorrelatedHiggsObservables in " + filename + " at line number:" + boost::lexical_cast<std::string>(lineNo) + ".\n");
+                else sleep(2);
+            }
+            i++;
+            if (readCorrelations && (i == nrows)) {
+                gslpp::matrix<double> myCorr(gslpp::matrix<double>::Id(nrows));
+                int ni = 0;
+                for (int irow = 0; irow < nrows; irow++) {
+                    IsEOF = getline(ifile, line).eof();
+                    if (line.empty() || line.at(0) == '#') {
+                        if (rank == 0) throw std::runtime_error("ERROR: no comments or empty lines in CorrelatedHiggsObservables please! In file " + filename + " at line number:" + boost::lexical_cast<std::string>(lineNo) + ".\n");
+                        else sleep(2);
+                    }
+                    lineNo++;
+                    boost::tokenizer<boost::char_separator<char> > mytok(line, sep);
+                    beg = mytok.begin();
+                    int nj = 0;
+                    for (int jcol = 0; jcol < nrows; jcol++) {
+                        if ((*beg).compare(0, 1, "0") == 0
+                                || (*beg).compare(0, 1, "1") == 0
+                                || (*beg).compare(0, 1, "-") == 0 || (covarianceFromConfig == true)) {
+                            if (std::distance(mytok.begin(), mytok.end()) < nrows) {
+                                if (rank == 0) throw std::runtime_error(("ERROR: Correlation matrix is of wrong size in CorrelatedHiggsObservables: " + name + +" at line number:" + boost::lexical_cast<std::string>(lineNo) + ".\n").c_str());
+                                else sleep(2);
+                            }
+                            myCorr(ni, nj) = atof((*beg).c_str());
+                            nj++;
+                            beg++;
+                        } else {
+                            if (rank == 0) throw std::runtime_error("ERROR: invalid correlation matrix for " + name + ". Check element (" + boost::lexical_cast<std::string>(ni + 1) + "," + boost::lexical_cast<std::string>(nj + 1) + ") in line number " + boost::lexical_cast<std::string>(lineNo) + " in file " + filename + ".\n");
+                            else sleep(2);
+                        }
+                    }
+                    ni++;
+                }
+                if (myCorr.size_i() != nrows || myCorr.size_j() != nrows)
+                    throw std::runtime_error("The size of the correlated observables in " + name + " does not match the size of the correlation matrix!");
+                InvCov = new gslpp::matrix<double>(nrows, nrows, 0.);
+                if (covarianceFromConfig) {
+                    for (unsigned int i = 0; i < nrows; i++)
+                        for (unsigned int j = 0; j < nrows; j++)
+                            (*InvCov)(i, j) = myCorr(i, j);
+                } else {
+                    for (unsigned int i = 0; i < nrows; i++)
+                        for (unsigned int j = 0; j < nrows; j++)
+                            (*InvCov)(i, j) = channels(i, thObsV.size() + 2) * myCorr(i, j) * channels(i, thObsV.size() + 2); // left = right imposed above
+                    *InvCov = InvCov->inverse();
+                }
+                readCorrelations = false;
+            }
+        }
     } while (!IsEOF);
     if (i != nrows) {
         std::stringstream ss;
@@ -85,14 +161,20 @@ double HiggsObservable::computeWeight()
                 theoryValues.at(j) = thObsV.at(j)->computeThValue();
                 mu += channels(i, j) * theoryValues.at(j);
             }
-                theoryValues.at(thobsvsize) = tho->computeThValue();
-                if(thObsV.at(0)->getModel().isModelLinearized())
-                    theoryValues.at(thobsvsize + i + 1) = -1. + mu + theoryValues.at(thobsvsize);
-                else
-                    theoryValues.at(thobsvsize + i + 1) = mu * theoryValues.at(thobsvsize);
-            logprob += LogSplitGaussian(theoryValues.at(thobsvsize + i + 1), channels(i, thobsvsize), channels(i, thobsvsize + 1), channels(i, thobsvsize + 2));
+            theoryValues.at(thobsvsize) = tho->computeThValue();
+            if (thObsV.at(0)->getModel().isModelLinearized())
+                theoryValues.at(thobsvsize + i + 1) = -1. + mu + theoryValues.at(thobsvsize);
+            else
+                theoryValues.at(thobsvsize + i + 1) = mu * theoryValues.at(thobsvsize);
+            if (!isCorrelated) logprob += LogSplitGaussian(theoryValues.at(thobsvsize + i + 1), channels(i, thobsvsize), channels(i, thobsvsize + 1), channels(i, thobsvsize + 2));
         }
-      
+        if (isCorrelated) {
+            gslpp::vector<double> theObsVal(channels.GetNrows(), 0.);
+            for (int i = 0; i < channels.GetNrows(); i++) {
+                theObsVal(i) = theoryValues.at(thobsvsize + i + 1);
+            }
+            logprob =  (-0.5 * theObsVal * ((*InvCov) * theObsVal));
+        }
     } else {
         for (int i = 0; i < channels.GetNrows(); i++) {
             double mu = 0, sum = 0.;
@@ -107,16 +189,17 @@ double HiggsObservable::computeWeight()
             logprob += LogSplitGaussian(theoryValues.at(thobsvsize + i + 1), channels(i, 3), channels(i, 4), channels(i, 5));
         }
     }
-    
+
     return (logprob);
 }
 
-
-boost::tokenizer<boost::char_separator<char> >::iterator & HiggsObservable::ParseHiggsObservable(boost::tokenizer<boost::char_separator<char> >::iterator & beg,
-                                                                                                 ThObsFactory& myObsFactory,
-                                                                                                 StandardModel * myModel,
-                                                                                                 int rank)
+boost::tokenizer<boost::char_separator<char> >::iterator & HiggsObservable::ParseHiggsObservable(
+        boost::tokenizer<boost::char_separator<char> >::iterator & beg,
+        ThObsFactory& myObsFactory,
+        StandardModel * myModel,
+        int rank)
 {
+    this->rank = rank;
     std::string type = "HiggsObservable";
     setObsType(type);
     std::vector<ThObservable*> hthobs;
@@ -149,7 +232,7 @@ boost::tokenizer<boost::char_separator<char> >::iterator & HiggsObservable::Pars
         std::ifstream ifile(filename_h.c_str());
         if (!ifile.is_open()) {
             if (rank == 0) throw std::runtime_error("\nERROR: " + filename_h + " does not exist. Make sure to specify a valid Higgs parameters configuration file.\n");
-            else sleep (2);
+            else sleep(2);
         }
         std::string line_h;
         bool IsEOF_h;
@@ -177,7 +260,7 @@ boost::tokenizer<boost::char_separator<char> >::iterator & HiggsObservable::Pars
         if (rank == 0) throw std::runtime_error("ERROR: wrong distribution flag " + distr + " in " + getName());
         else sleep(2);
     }
-    
+
     return beg;
 }
 
