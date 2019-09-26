5fea5c9cd9657505c319ecae08824a059498268d
Luca Silvestrini <Luca.Silvestrini@roma1.infn.it>
2019-09-26 16:13:49 +0200

    Added new class for bounds on coefficients of Delta F=2 operators in the WET

---
+++ FlavourWilsonCoefficient/src/FlavourWilsonCoefficient_DF2Matching.h
@@ -0,0 +1,59 @@
+/* 
+ * Copyright (C) 2019 HEPfit Collaboration
+ *
+ *
+ * For the licensing terms see doc/COPYING.
+ */
+
+#ifndef FLAVOURWILSONCOEFFICIENT_DF2MATCHING_H
+#define FLAVOURWILSONCOEFFICIENT_DF2MATCHING_H
+
+#include "gslpp.h"
+#include "StandardModelMatching.h"
+
+class FlavourWilsonCoefficient_DF2;
+
+class FlavourWilsonCoefficient_DF2Matching : public StandardModelMatching {
+public:
+    FlavourWilsonCoefficient_DF2Matching(const FlavourWilsonCoefficient_DF2& FWC_i);
+    
+        /**
+     * 
+     * @brief \f$ \Delta B = 2 \f$, \f$ B_{d} \f$ 
+     * @return return the vector of SM Wilson coefficients (SM + NP)
+     */
+    virtual  std::vector<WilsonCoefficient>& CMdbd2() ;
+    
+    /**
+     * 
+     * @brief \f$ \Delta B = 2 \f$, \f$ B_{s} \f$ 
+     * @return return the vector of SM Wilson coefficients (SM + NP)
+     */
+    virtual   std::vector<WilsonCoefficient>& CMdbs2() ;
+
+    /**
+     * 
+     * @brief \f$ \Delta C = 2 \f$,
+     * @return return the vector of SM Wilson coefficients (SM + NP)
+     */
+    virtual   std::vector<WilsonCoefficient>& CMdd2() ;
+    
+    /**
+     * 
+     * @brief \f$ \Delta S = 2 \f$ 
+     * @return return the vector of SM Wilson coefficients (SM + NP)
+     */
+    virtual   std::vector<WilsonCoefficient>& CMdk2() ;
+       
+protected:
+    std::vector<WilsonCoefficient> vmcdbd2, vmcdbs2, vmcdc2, vmcds2;
+
+
+private:
+    const FlavourWilsonCoefficient_DF2& FWC;
+    WilsonCoefficient mcdbd2, mcdbs2, mcdc2, mcds2;
+
+};
+
+#endif /* FLAVOURWILSONCOEFFICIENT_DF2MATCHING_H */
+
