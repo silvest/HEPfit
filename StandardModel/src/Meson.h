363cd4b8590f142751f80a930238ecfb6c1ce97b
MarcoFedele <marco.fedele@uniroma1.it>
2019-10-22 17:32:50 +0200

    Documentatio: parameter definitions moved from QCD to Meson

--- StandardModel/src/Meson.h
+++ StandardModel/src/Meson.h
@@ -24,8 +24,298 @@ class QCD;
  * @author HEPfit Collaboration
  * @copyright GNU General Public License
  * @details This class is used to define a meson and three of its
- * characteristics: mass, lifetime and decay constant. 
+ * characteristics: mass, lifetime and decay constant. For selected mesons,
+ * also the Gegenbauer moments are defined.
  * This class inherits the public access members of the Particle class.
+ * 
+ *
+ * @anchor QCDParameters
+ * <h3>%Model parameters</h3>
+ *
+ * The model parameters of %QCD are summarized below:
+ * <table class="model">
+ * <tr>
+ *   <td class="mod_name">%MP0</td>
+ *   <td class="mod_symb">@f$M_{\pi^0}@f$</td>
+ *   <td class="mod_desc">The mass of the \f$ \pi^0 \f$ meson in GeV.</td>
+ * </tr>
+ * <tr>
+ *   <td class="mod_name">%tP0</td>
+ *   <td class="mod_symb">@f$\tau_{\pi^0}@f$</td>
+ *   <td class="mod_desc">The lifetime of the \f$ \pi^0 \f$ meson in \f$\mathrm{ps}^{-1} \f$.</td>
+ * </tr>
+ * <tr>
+ *   <td class="mod_name">%FP0</td>
+ *   <td class="mod_symb">@f$F_{\pi^0}@f$</td>
+ *   <td class="mod_desc">The decay constant of the \f$ \pi^0 \f$ meson in GeV.</td>
+ * </tr>
+ * <tr>
+ *   <td class="mod_name">%MPp</td>
+ *   <td class="mod_symb">@f$M_{\pi^\pm}@f$</td>
+ *   <td class="mod_desc">The mass of the \f$ \pi^\pm \f$ meson in GeV.</td>
+ * </tr>
+ * <tr>
+ *   <td class="mod_name">%tPp</td>
+ *   <td class="mod_symb">@f$\tau_{\pi^\pm}@f$</td>
+ *   <td class="mod_desc">The lifetime of the \f$ \pi^\pm \f$ meson in \f$\mathrm{ps}^{-1} \f$.</td>
+ * </tr>
+ * <tr>
+ *   <td class="mod_name">%FPp</td>
+ *   <td class="mod_symb">@f$F_{\pi^\pm}@f$</td>
+ *   <td class="mod_desc">The decay constant of the \f$ \pi^{\pm} \f$ meson in GeV.</td>
+ * </tr>
+ * <tr>
+ *   <td class="mod_name">%MK0</td>
+ *   <td class="mod_symb">@f$M_{K^0}@f$</td>
+ *   <td class="mod_desc">The mass of the \f$ K^0 \f$ meson in GeV.</td>
+ * </tr>
+ * <tr>
+ *   <td class="mod_name">%tKl</td>
+ *   <td class="mod_symb">@f$\tau_{K_L}@f$</td>
+ *   <td class="mod_desc">The lifetime of the \f$ K_L \f$ meson in \f$\mathrm{ps}^{-1} \f$.</td>
+ * </tr>
+ * <tr>
+ *   <td class="mod_name">%MKp</td>
+ *   <td class="mod_symb">@f$M_{K^\pm}@f$</td>
+ *   <td class="mod_desc">The mass of the \f$ K^\pm \f$ meson in GeV.</td>
+ * </tr>
+ * <tr>
+ *   <td class="mod_name">%tKp</td>
+ *   <td class="mod_symb">@f$\tau_{K^\pm}@f$</td>
+ *   <td class="mod_desc">The lifetime of the \f$ K^\pm \f$ meson in \f$\mathrm{ps}^{-1} \f$.</td>
+ * </tr>
+ * <tr>
+ *   <td class="mod_name">%FK</td>
+ *   <td class="mod_symb">@f$F_{K}@f$</td>
+ *   <td class="mod_desc">The decay constant of the \f$ K^{0,\pm} \f$ meson in GeV.</td>
+ * </tr>
+ * <tr>
+ *   <td class="mod_name">%alpha1kp</td>
+ *   <td class="mod_symb">@f$\alpha_1^{K^\pm}@f$</td>
+ *   <td class="mod_desc">The first Gegenbauer moment of the \f$ K^\pm \f$ meson.</td>
+ * </tr>
+ * <tr>
+ *   <td class="mod_name">%alpha2kp</td>
+ *   <td class="mod_symb">@f$\alpha_2^{K^\pm}@f$</td>
+ *   <td class="mod_desc">The second Gegenbauer moment of the \f$ K^\pm \f$ meson.</td>
+ * </tr>
+ * <tr>
+ *   <td class="mod_name">%MD</td>
+ *   <td class="mod_symb">@f$M_{D^0}@f$</td>
+ *   <td class="mod_desc">The mass of the \f$ D^0 \f$ meson in GeV.</td>
+ * </tr>
+ * <tr>
+ *   <td class="mod_name">%tD</td>
+ *   <td class="mod_symb">@f$\tau_{D^0}@f$</td>
+ *   <td class="mod_desc">The lifetime of the \f$ D^0 \f$ meson in \f$\mathrm{ps}^{-1} \f$.</td>
+ * </tr>
+ * <tr>
+ *   <td class="mod_name">%FD</td>
+ *   <td class="mod_symb">@f$F_{D^0}@f$</td>
+ *   <td class="mod_desc">The decay constant of the \f$ D^0 \f$ meson in GeV.</td>
+ * </tr>
+ * <tr>
+ *   <td class="mod_name">%MDp</td>
+ *   <td class="mod_symb">@f$M_{D^\pm}@f$</td>
+ *   <td class="mod_desc">The mass of the \f$ D^\pm \f$ meson in GeV.</td>
+ * </tr>
+ * <tr>
+ *   <td class="mod_name">%tDp</td>
+ *   <td class="mod_symb">@f$\tau_{D^\pm}@f$</td>
+ *   <td class="mod_desc">The lifetime of the \f$ D^\pm \f$ meson in \f$\mathrm{ps}^{-1} \f$.</td>
+ * </tr>
+ * <tr>
+ *   <td class="mod_name">%FDp</td>
+ *   <td class="mod_symb">@f$F_{D^\pm}@f$</td>
+ *   <td class="mod_desc">The decay constant of the \f$ D^\pm \f$ meson in GeV.</td>
+ * </tr>
+ * <tr>
+ *   <td class="mod_name">%MBd</td>
+ *   <td class="mod_symb">@f$M_{B_d}@f$</td>
+ *   <td class="mod_desc">The mass of the \f$ B_d \f$ meson in GeV.</td>
+ * </tr>
+ * <tr>
+ *   <td class="mod_name">%tBd</td>
+ *   <td class="mod_symb">@f$\tau_{B_d}@f$</td>
+ *   <td class="mod_desc">The lifetime of the \f$ B_d \f$ meson in \f$\mathrm{ps}^{-1} \f$.</td>
+ * </tr>
+ * <tr>
+ *   <td class="mod_name">%MBp</td>
+ *   <td class="mod_symb">@f$M_{B^\pm}@f$</td>
+ *   <td class="mod_desc">The mass of the \f$ B^\pm \f$ meson in GeV.</td>
+ * </tr>
+ * <tr>
+ *   <td class="mod_name">%tBp</td>
+ *   <td class="mod_symb">@f$\tau_{B^\pm}@f$</td>
+ *   <td class="mod_desc">The lifetime of the \f$ B^\pm \f$ meson in \f$\mathrm{ps}^{-1} \f$.</td>
+ * </tr>
+ * <tr>
+ *   <td class="mod_name">%FBsoFBd</td>
+ *   <td class="mod_symb">@f$F_{B_s}/F_{B_d}@f$</td>
+ *   <td class="mod_desc">The ratio of decay constants of the \f$ B_s \f$ and \f$ B_d \f$ mesons.</td>
+ * </tr>
+ * <tr>
+ *   <td class="mod_name">%MBs</td>
+ *   <td class="mod_symb">@f$M_{B_s}@f$</td>
+ *   <td class="mod_desc">The mass of the \f$ B_s \f$ meson in GeV.</td>
+ * </tr>
+ * <tr>
+ *   <td class="mod_name">%tBs</td>
+ *   <td class="mod_symb">@f$\tau_{B_s}@f$</td>
+ *   <td class="mod_desc">The lifetime of the \f$ B_s \f$ meson in \f$\mathrm{ps}^{-1} \f$.</td>
+ * </tr>
+ * <tr>
+ *   <td class="mod_name">%FBs</td>
+ *   <td class="mod_symb">@f$F_{B_s}@f$</td>
+ *   <td class="mod_desc">The decay constant of the \f$ B_s \f$ meson in GeV.</td>
+ * </tr>
+ * <tr>
+ *   <td class="mod_name">%lambdaB</td>
+ *   <td class="mod_symb">@f$ \lambda_B @f$</td>
+ *   <td class="mod_desc">The moment of the \f$B\f$ meson light-cone distribution amplitude.</td>
+ * </tr>
+ * <tr>
+ *   <td class="mod_name">%DGs_Gs</td>
+ *   <td class="mod_symb">@f$ \Delta \Gamma_s /\Gamma_s @f$</td>
+ *   <td class="mod_desc">The oscillation parameter for the \f$B_s\f$ meson.</td>
+ * </tr>
+ * <tr>
+ *   <td class="mod_name">%MBc</td>
+ *   <td class="mod_symb">@f$M_{B_c}@f$</td>
+ *   <td class="mod_desc">The mass of the \f$ B_c \f$ meson in GeV.</td>
+ * </tr>
+ * <tr>
+ *   <td class="mod_name">%tBc</td>
+ *   <td class="mod_symb">@f$\tau_{B_c}@f$</td>
+ *   <td class="mod_desc">The lifetime of the \f$ B_c \f$ meson in \f$\mathrm{ps}^{-1} \f$.</td>
+ * </tr>
+ * <tr>
+ *   <td class="mod_name">%FBc</td>
+ *   <td class="mod_symb">@f$F_{B_c}@f$</td>
+ *   <td class="mod_desc">The decay constant of the \f$ B_c \f$ meson in GeV.</td>
+ * </tr>
+ * <tr>
+ *   <td class="mod_name">%Mphi</td>
+ *   <td class="mod_symb">@f$M_{\phi}@f$</td>
+ *   <td class="mod_desc">The mass of the \f$ \phi \f$ meson in GeV.</td>
+ * </tr>
+ * <tr>
+ *   <td class="mod_name">%tphi</td>
+ *   <td class="mod_symb">@f$\tau_{\phi}@f$</td>
+ *   <td class="mod_desc">The lifetime of the \f$ \phi \f$ meson in \f$\mathrm{ps}^{-1} \f$.</td>
+ * </tr>
+ * <tr>
+ *   <td class="mod_name">%Fphi</td>
+ *   <td class="mod_symb">@f$F_{\phi}@f$</td>
+ *   <td class="mod_desc">The decay constant of the \f$ \phi \f$ meson in GeV.</td>
+ * </tr>
+ * <tr>
+ *   <td class="mod_name">%Fphip</td>
+ *   <td class="mod_symb">@f$F_{\phi}^{\perp}@f$</td>
+ *   <td class="mod_desc">The decay constant of a transversely polarized \f$ \phi \f$ meson in GeV.</td>
+ * </tr>
+ * <tr>
+ *   <td class="mod_name">%alpha2phi</td>
+ *   <td class="mod_symb">@f$\alpha_2^{\phi}@f$</td>
+ *   <td class="mod_desc">The second Gegenbauer moment of the \f$ \phi \f$ meson.</td>
+ * </tr>
+ * <tr>
+ *   <td class="mod_name">%MKstar</td>
+ *   <td class="mod_symb">@f$M_{K^{*0}}@f$</td>
+ *   <td class="mod_desc">The mass of the \f$ K^{*0} \f$ meson in GeV.</td>
+ * </tr>
+ * <tr>
+ *   <td class="mod_name">%MKstarP</td>
+ *   <td class="mod_symb">@f$M_{K^{*\pm}}@f$</td>
+ *   <td class="mod_desc">The mass of the \f$ K^{*\pm} \f$ meson in GeV.</td>
+ * </tr>
+ * <tr>
+ *   <td class="mod_name">%tKstar</td>
+ *   <td class="mod_symb">@f$\tau_{K^*}@f$</td>
+ *   <td class="mod_desc">The lifetime of the \f$ K^* \f$ meson in \f$\mathrm{ps}^{-1} \f$.</td>
+ * </tr>
+ * <tr>
+ *   <td class="mod_name">%FKstar</td>
+ *   <td class="mod_symb">@f$F_{K^*}@f$</td>
+ *   <td class="mod_desc">The decay constant of the \f$ K^* \f$ meson in GeV.</td>
+ * </tr>
+ * <tr>
+ *   <td class="mod_name">%FKstarp</td>
+ *   <td class="mod_symb">@f$F_{K^*}^{\perp}@f$</td>
+ *   <td class="mod_desc">The decay constant of a transversely polarized \f$ K^* \f$ meson in GeV.</td>
+ * </tr>
+ * <tr>
+ *   <td class="mod_name">%alpha1kst</td>
+ *   <td class="mod_symb">@f$\alpha_1^{K^*}@f$</td>
+ *   <td class="mod_desc">The first Gegenbauer moment of the \f$ K^* \f$ meson.</td>
+ * </tr>
+ * <tr>
+ *   <td class="mod_name">%alpha2kst</td>
+ *   <td class="mod_symb">@f$\alpha_2^{K^*}@f$</td>
+ *   <td class="mod_desc">The second Gegenbauer moment of the \f$ K^* \f$ meson.</td>
+ * </tr>
+ * <tr>
+ *   <td class="mod_name">%MDstarP</td>
+ *   <td class="mod_symb">@f$M_{D^{*\pm}}@f$</td>
+ *   <td class="mod_desc">The mass of the \f$ K^{*\pm} \f$ meson in GeV.</td>
+ * </tr>
+ * <tr>
+ *   <td class="mod_name">%tDstarP</td>
+ *   <td class="mod_symb">@f$\tau_{D^{*\pm}}@f$</td>
+ *   <td class="mod_desc">The lifetime of the \f$ D^{*\pm} \f$ meson in \f$\mathrm{ps}^{-1} \f$.</td>
+ * </tr>
+ * <tr>
+ *   <td class="mod_name">%FDstarP</td>
+ *   <td class="mod_symb">@f$F_{D^{*\pm}}@f$</td>
+ *   <td class="mod_desc">The decay constant of the \f$ D^{*\pm} \f$ meson in GeV.</td>
+ * </tr>
+ * <tr>
+ *   <td class="mod_name">%Mrho</td>
+ *   <td class="mod_symb">@f$M_{\rho}@f$</td>
+ *   <td class="mod_desc">The mass of the \f$ \rho \f$ meson in GeV.</td>
+ * </tr>
+ * <tr>
+ *   <td class="mod_name">%trho</td>
+ *   <td class="mod_symb">@f$\tau_{\rho}@f$</td>
+ *   <td class="mod_desc">The lifetime of the \f$ \rho \f$ meson in \f$\mathrm{ps}^{-1} \f$.</td>
+ * </tr>
+ * <tr>
+ *   <td class="mod_name">%Frho</td>
+ *   <td class="mod_symb">@f$F_{\rho}@f$</td>
+ *   <td class="mod_desc">The decay constant of the \f$ \rho \f$ meson in GeV.</td>
+ * </tr>
+ * <tr>
+ *   <td class="mod_name">%MrhoP</td>
+ *   <td class="mod_symb">@f$M_{\rho^{\pm}}@f$</td>
+ *   <td class="mod_desc">The mass of the \f$ \rho^{\pm} \f$ meson in GeV.</td>
+ * </tr>
+ * <tr>
+ *   <td class="mod_name">%trhoP</td>
+ *   <td class="mod_symb">@f$\tau_{\rho^{\pm}}@f$</td>
+ *   <td class="mod_desc">The lifetime of the \f$ \rho^{\pm} \f$ meson in \f$\mathrm{ps}^{-1} \f$.</td>
+ * </tr>
+ * <tr>
+ *   <td class="mod_name">%FrhoP</td>
+ *   <td class="mod_symb">@f$F_{\rho^{\pm}}@f$</td>
+ *   <td class="mod_desc">The decay constant of the \f$ \rho^{\pm} \f$ meson in GeV.</td>
+ * </tr>
+ * <tr>
+ *   <td class="mod_name">%Momega</td>
+ *   <td class="mod_symb">@f$M_{\omega}@f$</td>
+ *   <td class="mod_desc">The mass of the \f$ \omega \f$ meson in GeV.</td>
+ * </tr>
+ * <tr>
+ *   <td class="mod_name">%tomega</td>
+ *   <td class="mod_symb">@f$\tau_{\omega}@f$</td>
+ *   <td class="mod_desc">The lifetime of the \f$ \omega \f$ meson in \f$\mathrm{ps}^{-1} \f$.</td>
+ * </tr>
+ * <tr>
+ *   <td class="mod_name">%Fomega</td>
+ *   <td class="mod_symb">@f$F_{\omega}@f$</td>
+ *   <td class="mod_desc">The decay constant of the \f$ \omega \f$ meson in GeV.</td>
+ * </tr>
+ * </table>
+ * 
  */
 class Meson : public Particle {
 public:
