ff896c010ecb2cae22ddeacfd8ea5f3229bee800
jdeblasm <jdeblasm@gmail.com>
2019-10-29 15:19:35 +0100

    NewPhysics: updated documentation for HiggsChiral (Typos)

--- NewPhysics/src/HiggsChiral.h
+++ NewPhysics/src/HiggsChiral.h
@@ -159,7 +159,7 @@ public:
     }
     
     /**
-     * @brief a getter for the EFT coeff @f$c_c@f$4
+     * @brief a getter for the EFT coeff @f$c_c@f$
      * @return @f$c_c@f$
      */
     double getcc() const
